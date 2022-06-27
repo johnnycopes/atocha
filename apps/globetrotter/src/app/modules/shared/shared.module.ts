4b185a7d447fb72680a1f637a87142c16a3523d7
John Coppola <jdcoppola@gmail.com>
2022-06-27 08:41:45 -0400

    deletes modal.component

--- apps/globetrotter/src/app/modules/shared/shared.module.ts
+++ apps/globetrotter/src/app/modules/shared/shared.module.ts
@@ -6,7 +6,6 @@ import { UiGlobetrotterModule } from '@atocha/ui-globetrotter';
 import { ListDetailsComponent } from './components/list-details/list-details.component';
 import { LoaderComponent } from './components/loader/loader.component';
 import { MeasurementPipe } from './pipes/measurement.pipe';
-import { ModalComponent } from './components/modal/modal.component';
 import { RadioButtonsComponent } from './components/radio-buttons/radio-buttons.component';
 import { TabComponent } from './components/tabset/tab/tab.component';
 import { TabsetComponent } from './components/tabset/tabset.component';
@@ -22,7 +21,6 @@ import { TabsetComponent } from './components/tabset/tabset.component';
     ListDetailsComponent,
     LoaderComponent,
     MeasurementPipe,
-    ModalComponent,
     RadioButtonsComponent,
     TabComponent,
     TabsetComponent,
@@ -31,7 +29,6 @@ import { TabsetComponent } from './components/tabset/tabset.component';
     ListDetailsComponent,
     LoaderComponent,
     MeasurementPipe,
-    ModalComponent,
     RadioButtonsComponent,
     TabComponent,
     TabsetComponent,
