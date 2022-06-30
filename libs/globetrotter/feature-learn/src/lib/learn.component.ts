fa86d470af0e738ca83e49d785beef877e7872ca
John Coppola <jdcoppola@gmail.com>
2022-06-30 09:45:59 -0400

    deletes original learn.module

--- libs/globetrotter/feature-learn/src/lib/learn.component.ts
+++ libs/globetrotter/feature-learn/src/lib/learn.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-learn',
   templateUrl: './learn.component.html',
   styleUrls: ['./learn.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LearnComponent {}
