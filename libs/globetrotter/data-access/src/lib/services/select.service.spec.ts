6f1162a7371cacd88ad4e5bcad0328f3e98c5add
John Coppola <jdcoppola@gmail.com>
2022-07-03 15:06:42 -0400

    gets select.service tests running:

--- libs/globetrotter/data-access/src/lib/services/select.service.spec.ts
+++ libs/globetrotter/data-access/src/lib/services/select.service.spec.ts
@@ -1,21 +1,53 @@
-import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { SelectService } from './select.service';
+import { QuizType } from '@atocha/globetrotter/types';
 
 describe('SelectService', () => {
   let service: SelectService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        { provide: HttpClient, useClass: {} },
-      ],
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(SelectService);
   });
 
-  it('should be created', () => {
+  it('renders', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('maps a selection to selection query params', () => {
+    expect(service.mapSelectionToQueryParams({
+      type: QuizType.flagsCountries,
+      quantity: 3,
+      places: {
+        Asia: 'indeterminate',
+        'Southern Asia': 'checked',
+        'Western Asia': 'checked',
+      },
+    })).toEqual({
+      type: "1",
+      quantity: "3",
+      places: "Asia_i,Southern Asia_c,Western Asia_c",
+    });
+  });
+
+  it('maps selection query params to a selection', () => {
+    expect(service.mapQueryParamsToSelection({
+      type: '2',
+      quantity: '7',
+      places: 'Oceania_i,Melanesia_c,Micronesia_c,Polynesia_c',
+    })).toEqual({
+      type: QuizType.capitalsCountries,
+      quantity: 7,
+      places: {
+        Oceania: 'indeterminate',
+        Melanesia: 'checked',
+        Micronesia: 'checked',
+        Polynesia: 'checked',
+      },
+    });
   });
 });
