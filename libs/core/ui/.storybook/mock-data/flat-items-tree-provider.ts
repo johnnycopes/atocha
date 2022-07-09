42c5f66e7f059b28530bb97544d88358802e9cf6
John Coppola <jdcoppola@gmail.com>
2022-07-09 11:22:21 -0400

    gets flatItems examples working

--- libs/core/ui/.storybook/mock-data/flat-items-tree-provider.ts
+++ libs/core/ui/.storybook/mock-data/flat-items-tree-provider.ts
@@ -3,28 +3,16 @@ import { groupBy, keyBy } from "lodash-es";
 import { TreeProvider } from "../../src/lib/nested-checkboxes/nested-checkboxes.component";
 import { Item } from "./item.interface";
 
-export class FlatItemTreeProvider implements TreeProvider<Item> {
-  private _itemsKeyedById: Record<string, Item>;
-  private _itemsGroupedByParentId: Record<string, Item[]>;
-
+export class FlatItemsTreeProvider implements TreeProvider<Item> {
   constructor(items: Item[]) {
-    this._itemsKeyedById = keyBy(items, 'id');
-    this._itemsGroupedByParentId = groupBy(items, 'parentId');
+    this.getParent = ({ parentId }: Item) => parentId ? keyBy(items, 'id')[parentId] : undefined;
+    this.getChildren = ({ id }: Item) => groupBy(items, 'parentId')[id] || [];
   }
+
+  getParent: (item: Item) => Item | undefined;
+  getChildren: (item: Item) => Item[];
 
   getId(item: Item): string {
     return item.id;
-  }
-
-  getParent(item: Item): Item | undefined {
-    const parentId = item.parentId;
-    if (parentId) {
-      return this._itemsKeyedById[parentId];
-    }
-    return undefined;
-  }
-
-  getChildren(item: Item): Item[] {
-    return this._itemsGroupedByParentId[item.id] || [];
   }
 }
