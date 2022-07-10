3fb2e9fc4689e6319691504426ad125e6311397d
John Coppola <jdcoppola@gmail.com>
2022-07-10 15:48:45 -0400

    progress

--- libs/core/ui/src/lib/nested-checkboxes/nested-checkboxes.component.ts
+++ libs/core/ui/src/lib/nested-checkboxes/nested-checkboxes.component.ts
@@ -2,14 +2,15 @@
 import {
   Component,
   Input,
-  OnInit,
   TemplateRef,
   forwardRef,
   ChangeDetectionStrategy,
   ChangeDetectorRef,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
-import { dedupe, getItemsRecursively } from '@atocha/core/util';
+import { dedupe } from '@atocha/core/util';
 
 export type CheckboxState = 'checked' | 'unchecked' | 'indeterminate';
 
@@ -21,11 +22,12 @@ export interface TreeProvider<T> {
   getParent?(item: T): T | undefined;
 }
 
-// interface NestedCheckboxItem {
-//   id: string;
-//   parentId?: string;
-//   children?: NestedCheckboxItem[];
-// }
+interface ItemsRecord<T> {
+  [id: string]: {
+    parent: T | undefined;
+    item: T;
+  }
+}
 
 @Component({
   selector: 'core-nested-checkboxes',
@@ -40,28 +42,24 @@ export interface TreeProvider<T> {
     },
   ],
 })
-export class NestedCheckboxesComponent<T> implements ControlValueAccessor {
-  @Input()
-  set item(value: T | undefined) {
-    if (!value) {
-      throw new Error('Item must be defined');
-    }
-    console.log(value);
-    this._item = value;
-  };
-  get item(): T {
-    return this._item;
-  }
-  private _item!: T;
+export class NestedCheckboxesComponent<T> implements OnChanges, ControlValueAccessor {
+  @Input() item!: T;
   @Input() getId: (item: T) => string = () => '';
   @Input() getChildren: (item: T) => T[] = () => [];
   @Input() treeProvider!: TreeProvider<T>;
   @Input() itemTemplate: TemplateRef<unknown> | undefined;
   @Input() indentation = 24;
   model: string[] = [];
+  private _itemsKeyedById: ItemsRecord<T> = {};
   private _onChangeFn: (value: string[]) => void = () => ({});
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  ngOnChanges({ item }: SimpleChanges): void {
+    if (item) {
+      this._itemsKeyedById = this._createParentIdsRecord(item.currentValue);
+    }
+  }
 
   writeValue(value: string[]): void {
     if (value) {
@@ -77,53 +75,74 @@ export class NestedCheckboxesComponent<T> implements ControlValueAccessor {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
   registerOnTouched(_fn: (value: string[]) => void): void {}
 
-  isIndeterminate(item: T, model: string[]): boolean {
-    const children = this.getChildren(item);
-
+  isIndeterminate(item: T): boolean {
+		const children = this.getChildren(item);
 		if (children?.length) {
 			const validChildrenIds = this._getIds(children);
 			const validChildrenSelected = validChildrenIds.reduce((accum, childId) => {
-				if (model.includes(childId)) {
+				if (this.model.includes(childId)) {
 					return accum + 1;
 				}
 				return accum;
 			}, 0);
 			return validChildrenSelected > 0 && validChildrenSelected < validChildrenIds.length;
 		}
-
     return false;
+  }
+
+  onChange(checked: boolean, item: T): void {
+    const parent = this._itemsKeyedById[this.getId(item)].parent;
+    let model = [...this.model];
+
+    if (!parent) {
+      model = checked ? Object.keys(this._itemsKeyedById) : [];
+    } else {
+      const id = this.getId(item);
+      const ids = this._getIds(this.getChildren(parent));
+      const parentId = this.getId(parent);
+
+      model = checked ? [...model, id] : model.filter(modelId => modelId !== id);
+
+      if (ids.every(id => model.includes(id))) {
+        model = [...model, parentId];
+      } else {
+        model = model.filter(modelValue => modelValue !== parentId);
+      }
+    }
+
+    this.model = dedupe(model);
+    this._onChangeFn(this.model);
   }
 
   private _getIds(items: T[]): string[] {
     return items.map(item => this.getId(item));
   }
 
-  onChange(checked: boolean, item: T): void {
-    const items = getItemsRecursively(item, this.getChildren);
-		const ids = this._getIds(items);
-		let updatedModel: string[];
+  private _createParentIdsRecord(item: T): ItemsRecord<T> {
+    const output: ItemsRecord<T> = {
+      [this.getId(item)]: {
+        parent: undefined,
+        item,
+      }
+    };
+    const items = [item];
 
-		if (checked) {
-			updatedModel = [...this.model, ...ids];
-		} else {
-			updatedModel = this.model.filter(modelValue => !ids.includes(modelValue));
-		}
+    while (items.length) {
+      const current = items.shift();
+      if (current) {
+        const children = this.getChildren(current);
+        if (children.length) {
+          children.forEach(child => {
+            output[this.getId(child)] = {
+              parent: current,
+              item: child,
+            };
+            items.push(child);
+          });
+        }
+      }
+    }
 
-    this._onChangeFn(updatedModel);
+    return output;
   }
-
-  // private _onChildrenChange(model: string[], parent: T): void {
-	// 	const children = this.getChildren(parent);
-	// 	const ids = this._getIds(children);
-	// 	const parentId = this.getId(parent);
-	// 	let updatedModel: string[];
-
-	// 	if (ids.every(id => model.includes(id))) {
-	// 		updatedModel = [...model, parentId];
-	// 	} else {
-	// 		updatedModel = model.filter(modelValue => modelValue !== parentId);
-	// 	}
-
-  //   this._onChangeFn(dedupe(updatedModel))
-	// }
 }
