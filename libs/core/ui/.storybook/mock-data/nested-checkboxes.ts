0118ed0d982edbf47b9a612acb40375e7e5b078a
John Coppola <jdcoppola@gmail.com>
2022-07-14 06:50:19 -0700

    adds new data to nested-checkboxes.ts to work with counts component examples

--- libs/core/ui/.storybook/mock-data/nested-checkboxes.ts
+++ libs/core/ui/.storybook/mock-data/nested-checkboxes.ts
@@ -1,8 +1,10 @@
-import { CheckboxStates } from '../../src/lib/nested-checkboxes/nested-checkboxes.component';
+import { CheckboxStates, TreeProvider } from '../../src/lib/nested-checkboxes/nested-checkboxes.component';
 
 export interface NestedItem {
   id: string;
+  parentId?: string;
   children?: NestedItem[];
+  targets?: number;
 }
 
 export const NESTED_ITEM: NestedItem = {
@@ -10,15 +12,15 @@ export const NESTED_ITEM: NestedItem = {
   children: [
     {
       id: 'Southern Africa',
-      children: [{ id: 'Swaziland' }, { id: 'Namibia' }],
+      children: [{ id: 'Swaziland', targets: 28 }, { id: 'Namibia', targets: 17 }],
     },
-    { id: 'Central Africa' },
+    { id: 'Central Africa', targets: 65 },
     {
       id: 'Northern Africa',
       children: [
         {
           id: 'Morocco',
-          children: [{ id: 'Marrakesh' }, { id: 'Fes' }],
+          children: [{ id: 'Marrakesh', targets: 9 }, { id: 'Fes', targets: 11 }],
         },
       ],
     },
@@ -48,3 +50,42 @@ export const ALL_SELECTED: CheckboxStates = {
 
 export const getId = ({ id }: NestedItem) => id;
 export const getChildren = ({ children }: NestedItem) => children ?? [];
+export const getCounts = ({ targets }: NestedItem) => targets ?? 0;
+
+export class NestedItemTreeProvider implements TreeProvider<NestedItem> {
+  private _itemsKeyedById: Record<string, NestedItem> = {};
+
+  constructor(item: NestedItem) {
+    // set itemsKeyedById recursively
+    const items = [item];
+    while (items.length) {
+      const currentItem = items.shift();
+      if (currentItem) {
+        const currentItemId = this.getId(currentItem);
+        const currentItemChildren = this.getChildren(currentItem);
+        this._itemsKeyedById[currentItemId] = currentItem;
+        if (currentItemChildren.length) {
+          currentItemChildren.forEach((child) => {
+            items.push(child);
+          });
+        }
+      }
+    }
+  }
+
+  getId(item: NestedItem): string {
+    return item.id;
+  }
+
+  getParent(item: NestedItem): NestedItem | undefined {
+    const parentId = item.parentId;
+    if (parentId) {
+      return this._itemsKeyedById[parentId];
+    }
+    return undefined;
+  }
+
+  getChildren(item: NestedItem): NestedItem[] {
+    return item.children || [];
+  }
+}
