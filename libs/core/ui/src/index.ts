e88df16640bdb6bcd5817ed9c60f3c669056c2cf
John Coppola <jdcoppola@gmail.com>
2022-07-14 08:08:30 -0700

    exports new component, deletes globetrotter-ui version of component, and subs in new component

--- libs/core/ui/src/index.ts
+++ libs/core/ui/src/index.ts
@@ -5,4 +5,5 @@ export * from './lib/autofocus/autofocus.directive';
 export * from './lib/checkbox/checkbox.component';
 export * from './lib/measurement/measurement.pipe';
 export * from './lib/nested-checkboxes/nested-checkboxes.component';
+export * from './lib/nested-checkboxes-with-counts/nested-checkboxes-with-counts.component';
 export * from './lib/tree/tree.component';
