ed7e3a45e01dd5811660e51c3f8a939d8a77f6d2
John Coppola <jdcoppola@gmail.com>
2022-07-17 10:21:26 -0700

    deletes obsolete data overrides

--- libs/globetrotter/data-access/src/lib/data/country-modifications.ts
+++ libs/globetrotter/data-access/src/lib/data/country-modifications.ts
@@ -1,12 +1,4 @@
-// Modifications to REST Countries API names for use in Globetrotter
-
-export const COUNTRY_APP_NAMES: Record<string, string> = {
-  "Côte d'Ivoire": 'Ivory Coast',
-  "Korea (Democratic People's Republic of)": 'North Korea',
-  'Korea (Republic of)': 'South Korea',
-  "Lao People's Democratic Republic": 'Laos',
-  'Macedonia (the former Yugoslav Republic of)': 'North Macedonia',
-};
+// Overrides to Wikipedia API data
 
 export const COUNTRY_SUMMARY_NAMES: Record<string, string> = {
   Georgia: 'Georgia country',
