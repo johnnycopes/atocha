a0ed9e8f4b073c7f6dcc80c2ed271a2787c25cd0
John Coppola <jdcoppola@gmail.com>
2022-07-17 05:50:25 -0700

    alphabetizes Country interface properties

--- libs/globetrotter/types/src/lib/domain/country.interface.ts
+++ libs/globetrotter/types/src/lib/domain/country.interface.ts
@@ -1,25 +1,25 @@
 export interface Country {
-  name: string;
-  topLevelDomain: string[];
-  capital: string;
-  region: string;
-  subregion: string;
-  population: number;
-  demonym: string;
   area: number;
-  gini: number;
-  numericCode: string;
+  capital: string;
+  cioc: string;
   currencies: {
     code: string;
     name: string;
     symbol: string;
   }[];
+  demonym: string;
+  flag: string;
+  gini: number;
   languages: {
     iso639_1: string;
     iso639_2: string;
     name: string;
     nativeName: string;
   }[];
-  flag: string;
-  cioc: string;
+  name: string;
+  numericCode: string;
+  population: number;
+  region: string;
+  subregion: string;
+  topLevelDomain: string[];
 }
