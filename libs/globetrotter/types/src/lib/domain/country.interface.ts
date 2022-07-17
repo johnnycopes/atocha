e6c7205c86fcfcfdf895d7920cab11eb7e8ed756
John Coppola <jdcoppola@gmail.com>
2022-07-17 05:49:42 -0700

    removes unneeded Country properties from interface

--- libs/globetrotter/types/src/lib/domain/country.interface.ts
+++ libs/globetrotter/types/src/lib/domain/country.interface.ts
@@ -1,23 +1,13 @@
-// Source: REST Countries API -- https://restcountries.com/#api-endpoints-v2-response-example
-
 export interface Country {
   name: string;
   topLevelDomain: string[];
-  alpha2Code: string;
-  alpha3Code: string;
-  callingCodes: string[];
   capital: string;
-  altSpellings: string[];
   region: string;
   subregion: string;
   population: number;
-  latlng: number[];
   demonym: string;
   area: number;
   gini: number;
-  timezones: string[];
-  borders: string[];
-  nativeName: string;
   numericCode: string;
   currencies: {
     code: string;
@@ -30,24 +20,6 @@ export interface Country {
     name: string;
     nativeName: string;
   }[];
-  translations: {
-    de: string;
-    es: string;
-    fr: string;
-    ja: string;
-    it: string;
-    br: string;
-    pt: string;
-    nl: string;
-    hr: string;
-    fa: string;
-  };
   flag: string;
-  regionalBlocs: {
-    acronym: string;
-    name: string;
-    otherAcronyms: string[];
-    otherNames: string[];
-  }[];
   cioc: string;
 }
