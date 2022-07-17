9d76c657a066a30d12025f945cc0242c36642c2e
John Coppola <jdcoppola@gmail.com>
2022-07-17 05:58:44 -0700

    adds comments to unusual properties

--- libs/globetrotter/types/src/lib/domain/country.interface.ts
+++ libs/globetrotter/types/src/lib/domain/country.interface.ts
@@ -1,15 +1,21 @@
 export interface Country {
+  /** Area in km² */
   area: number;
   capital: string;
+  /**
+   * A unique three-letter country code used by the
+   * International Olympics Committee (IOC)
+   * https://en.wikipedia.org/wiki/List_of_IOC_country_codes
+   */
   cioc: string;
   currencies: {
     code: string;
     name: string;
     symbol: string;
   }[];
+  /** The name for an inhabitant of a specific place */
   demonym: string;
   flag: string;
-  gini: number;
   languages: {
     iso639_1: string;
     iso639_2: string;
