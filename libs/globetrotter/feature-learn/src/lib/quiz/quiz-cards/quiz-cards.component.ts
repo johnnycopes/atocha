43c5fbc8a391302540069cd28ec84e9258db3e71
John Coppola <jdcoppola@gmail.com>
2022-07-24 07:29:12 -0700

    refactors countries to be an input with setter

--- libs/globetrotter/feature-learn/src/lib/quiz/quiz-cards/quiz-cards.component.ts
+++ libs/globetrotter/feature-learn/src/lib/quiz/quiz-cards/quiz-cards.component.ts
@@ -1,10 +1,7 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
-import { combineLatest } from 'rxjs';
-import { map, first } from 'rxjs/operators';
 import { shuffle } from 'lodash-es';
 
 import { staggerAnimation, fadeInAnimation } from '@atocha/globetrotter/ui';
-import { QuizService } from '@atocha/globetrotter/data-access';
 import { Country, QuizType } from '@atocha/globetrotter/types';
 
 @Component({
@@ -17,20 +14,12 @@ import { Country, QuizType } from '@atocha/globetrotter/types';
 export class QuizCardsComponent {
   @Input() type: QuizType | undefined;
   @Input() currentCountry: Country | undefined;
-  private _countries$ = this._quizService.quiz$.pipe(
-    first(),
-    map((quiz) => shuffle(quiz?.countries ?? []))
-  );
-  vm$ = combineLatest([
-    this._countries$,
-  ]).pipe(
-    map(([countries]) => ({
-      countries,
-    }))
-  );
+  @Input()
+  set countries(value: Country[]) {
+    this.shuffledCountries = shuffle(value);
+  }
+  shuffledCountries: Country[] = [];
   canFlipCards = true;
-
-  constructor(private _quizService: QuizService) {}
 
   onFlip(cardFlipped: boolean): void {
     this.canFlipCards = !cardFlipped;
