42ee36a55f248e99781a447db231c3d4897b59e2
John Coppola <jdcoppola@gmail.com>
2022-07-24 20:56:07 -0700

    reduces vm$ to two observables

--- libs/globetrotter/feature-learn/src/lib/select/select.component.ts
+++ libs/globetrotter/feature-learn/src/lib/select/select.component.ts
@@ -1,7 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { combineLatest } from 'rxjs';
-import { first, map, distinctUntilChanged } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
 
 import { fadeInAnimation } from '@atocha/globetrotter/ui';
 import { Route, QuizType, PlaceSelection } from '@atocha/globetrotter/types';
@@ -18,62 +18,36 @@ import {
   animations: [fadeInAnimation],
 })
 export class SelectComponent {
-  private _selection$ = this._selectService.selection$;
-  private _regions$ = this._countryService.countries$.pipe(
-    map(({ regions }) => regions)
-  );
-  private _numberOfSelectedCountries$ = combineLatest([
-    this._countryService.countries$.pipe(
-      map(({ countriesBySubregion }) => countriesBySubregion)
-    ),
-    this._selection$,
+  vm$ = combineLatest([
+    this._countryService.countries$,
+    this._selectService.selection$,
   ]).pipe(
-    map(([countriesBySubregion, { places }]) => (
-      Object.keys(places).reduce(
+    map(
+      ([
+        { regions, countriesBySubregion },
+        { places, type, quantity },
+      ]) => {
+        if (!regions.length) {
+          return undefined;
+        }
+        const numberOfSelectedCountries = Object.keys(places).reduce(
         (total, name) =>
             countriesBySubregion[name]
               ? total + countriesBySubregion[name].length
               : total,
           0
-        )
-      )),
-    distinctUntilChanged()
-  );
-  private _invalidQuantity$ = combineLatest([
-    this._numberOfSelectedCountries$,
-    this._selection$,
-  ]).pipe(
-    map(([numberOfSelectedCountries, { quantity }]) => {
-      return (
-        numberOfSelectedCountries <= 1 ||
-        quantity < 2 ||
-        quantity > numberOfSelectedCountries
-      );
-    }),
-    distinctUntilChanged()
-  );
-  vm$ = combineLatest([
-    this._numberOfSelectedCountries$,
-    this._regions$,
-    this._selection$,
-    this._invalidQuantity$,
-  ]).pipe(
-    map(
-      ([
-        numberOfSelectedCountries,
-        regions,
-        { places, type, quantity },
-        invalidQuantity,
-      ]) => {
-        if (!regions.length) {
-          return undefined;
-        }
+        );
+        const invalidQuantity = (
+          numberOfSelectedCountries <= 1 ||
+          quantity < 2 ||
+          quantity > numberOfSelectedCountries
+        );
         return {
-          numberOfSelectedCountries,
           regions,
           places,
           type,
           quantity,
+          numberOfSelectedCountries,
           invalidQuantity,
         };
       }
@@ -99,7 +73,7 @@ export class SelectComponent {
   }
 
   async onLaunch(): Promise<void> {
-    this._selection$.pipe(first()).subscribe(async (selection) => {
+    this._selectService.selection$.pipe(first()).subscribe(async (selection) => {
       if (!selection) {
         return;
       }
