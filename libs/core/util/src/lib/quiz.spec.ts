38ade3f4b14be6fd04b6c93053a6b77ea0f9e510
John Coppola <jdcoppola@gmail.com>
2022-07-25 08:09:22 -0700

    adds more tests

--- libs/core/util/src/lib/quiz.spec.ts
+++ libs/core/util/src/lib/quiz.spec.ts
@@ -1,14 +1,42 @@
 import { Quiz } from './quiz';
 
 describe('Quiz', () => {
-  it('initializes when passed no data', () => {
-    expect(new Quiz().quiz).toEqual({
+  it('initializes state when passed no data', () => {
+    expect(new Quiz().state).toStrictEqual({
       items: [],
+      currentItem: undefined,
       totalItems: 0,
       guess: 1,
       correctGuesses: 0,
       accuracy: 100,
       isComplete: false,
     });
+  });
+
+  it('initalizes state when passed data', () => {
+    expect(new Quiz({items: ['English', 'Spanish', 'French', 'Italian', 'Portuguese'] }).state).toStrictEqual({
+      items: ['English', 'Spanish', 'French', 'Italian', 'Portuguese'],
+      currentItem: 'English',
+      totalItems: 5,
+      guess: 1,
+      correctGuesses: 0,
+      accuracy: 100,
+      isComplete: false,
+    });
+  });
+
+  it('returns state after correct guess', () => {
+    const quiz = new Quiz({items: ['English', 'Spanish', 'French', 'Italian', 'Portuguese'] });
+    quiz.makeGuess(true);
+
+    expect(quiz.state).toStrictEqual({
+      items: ['Spanish', 'French', 'Italian', 'Portuguese'],
+      currentItem: 'Spanish',
+      totalItems: 5,
+      guess: 2,
+      correctGuesses: 1,
+      accuracy: 100,
+      isComplete: false,
+    })
   });
 });
--- libs/core/util/src/lib/quiz.ts
+++ libs/core/util/src/lib/quiz.ts
@@ -1,14 +1,26 @@
+interface QuizState<T> {
+  items: T[];
+  currentItem: T | undefined;
+  totalItems: number;
+  guess: number;
+  correctGuesses: number;
+  accuracy: number;
+  isComplete: boolean;
+}
+
 export class Quiz<T> {
+  private _originalItems: T[] = [];
   private _items: T[] = [];
   private _guess = 1;
   private _correctGuesses = 0;
   private _accuracy = 100;
   private _isComplete = false;
 
-  get quiz() {
+  get state(): QuizState<T> {
     return {
       items: this._items.slice(),
-      totalItems: this._items.length,
+      currentItem: this._items.slice()[0],
+      totalItems: this._originalItems.length,
       guess: this._guess,
       correctGuesses: this._correctGuesses,
       accuracy: this._accuracy,
@@ -21,6 +33,41 @@ export class Quiz<T> {
   }: {
     items?: T[],
   } = {}) {
-    this._items = items;
+    this._originalItems = items.slice();
+    this._items = items.slice();
+  }
+
+  makeGuess(isCorrect: boolean): void {
+    if (this._isComplete) {
+      return;
+    }
+
+    if (isCorrect) {
+      this._items.shift();
+      this._correctGuesses++;
+      // End the game if there are no remaining items left to guess
+      if (!this._items.length) {
+        this._accuracy = this._calculateAccuracy(this._guess, this._items.length);
+        this._isComplete = true;
+      }
+    } else {
+      this._items = this._movedGuessedItemToEnd(this._items);
+    }
+
+    // Increment the guess counter if the game isn't over, regardless of whether the guess was right or wrong
+    if (!this._isComplete) {
+      this._guess++;
+    }
+  }
+
+  private _movedGuessedItemToEnd(items: T[]): T[] {
+    const guessedItem = items[0];
+    const updatedItems = items.slice(1);
+    updatedItems.push(guessedItem);
+    return updatedItems;
+  }
+
+  private _calculateAccuracy(guesses: number, total: number): number {
+    return Math.round((total / guesses) * 100);
   }
 }
