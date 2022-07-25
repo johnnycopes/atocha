addb2f3e692d5c75e426f4e6b328219e8c8fbdcc
John Coppola <jdcoppola@gmail.com>
2022-07-25 11:02:31 -0700

    updates quiz.service to use quiz class internally

--- libs/globetrotter/data-access/src/lib/services/quiz.service.ts
+++ libs/globetrotter/data-access/src/lib/services/quiz.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { filter, first, map, shareReplay } from 'rxjs/operators';
 
-import { Route, Country, Selection, Quiz } from '@atocha/globetrotter/types';
+import { Quiz } from '@atocha/core/util';
+import { Route, Country, Selection } from '@atocha/globetrotter/types';
 import { PlaceService } from './place.service';
 import { RouterService } from './router.service';
 import { shuffle } from 'lodash-es';
@@ -12,10 +13,11 @@ import { shuffle } from 'lodash-es';
   providedIn: 'root',
 })
 export class QuizService {
-  private readonly _quizSubject = new BehaviorSubject<Quiz | undefined>(
+  private readonly _quizSubject = new BehaviorSubject<Quiz<Country> | undefined>(
     undefined
   );
   quiz$ = this._quizSubject.pipe(
+    map((quiz) => quiz?.state),
     shareReplay({ bufferSize: 1, refCount: true })
   );
 
@@ -28,7 +30,7 @@ export class QuizService {
       .subscribe(() => this._quizSubject.next(undefined));
   }
 
-  initializeQuiz({ type, quantity, places }: Selection): void {
+  initializeQuiz({ quantity, places }: Selection): void {
     this._placeService.places$
       .pipe(
         map(({ countriesBySubregion }) => {
@@ -44,60 +46,18 @@ export class QuizService {
         })
       )
       .subscribe((countries) => {
-        this._quizSubject.next({
-          guess: 1,
-          correctGuesses: 0,
-          countries,
-          totalCountries: countries.length,
-          accuracy: 100,
-          isComplete: false,
-        });
+        this._quizSubject.next(new Quiz(countries));
       });
   }
 
   updateQuiz(correctGuess: boolean): void {
-    this._quizSubject
-      .pipe(
-        first(),
-        map((quiz) => {
-          if (!quiz) {
-            return undefined;
-          }
-
-          const updatedQuiz = { ...quiz };
-          if (correctGuess) {
-            updatedQuiz.countries.shift();
-            updatedQuiz.correctGuesses++;
-            // End the game if there are no remaining countries left to guess
-            if (!updatedQuiz.countries.length) {
-              updatedQuiz.accuracy = this._calculateAccuracy(updatedQuiz);
-              updatedQuiz.isComplete = true;
-            }
-          } else {
-            updatedQuiz.countries = this._moveGuessedCountryToEnd(
-              updatedQuiz.countries
-            );
-          }
-
-          // Increment the guess counter if the game isn't over, regardless of whether the guess was right or wrong
-          if (!updatedQuiz.isComplete) {
-            updatedQuiz.guess++;
-          }
-
-          return updatedQuiz;
-        })
-      )
-      .subscribe((quiz) => this._quizSubject.next(quiz));
-  }
-
-  private _moveGuessedCountryToEnd(countries: Country[]): Country[] {
-    const guessedCountry = countries[0];
-    const updatedCountries = countries.slice(1);
-    updatedCountries.push(guessedCountry);
-    return updatedCountries;
-  }
-
-  private _calculateAccuracy(quiz: Quiz): number {
-    return Math.round((quiz.totalCountries / quiz.guess) * 100);
+    this._quizSubject.pipe(
+      first()
+    ).subscribe(quiz => {
+      if (quiz) {
+        quiz.guess(correctGuess);
+        this._quizSubject.next(quiz);
+      }
+    });
   }
 }
