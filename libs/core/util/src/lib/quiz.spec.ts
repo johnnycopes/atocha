2e230b1ba3640edb8bf6234d159bd177fc80cc8b
John Coppola <jdcoppola@gmail.com>
2022-07-25 09:34:17 -0700

    adds new test case

--- libs/core/util/src/lib/quiz.spec.ts
+++ libs/core/util/src/lib/quiz.spec.ts
@@ -51,25 +51,26 @@ describe('Quiz', () => {
     });
   });
 
-  it('returns state after partially completing quiz', () => {
+  it('returns state after partially finishing quiz', () => {
     const quiz = new Quiz(['French', 'Italian', 'Portuguese']);
     quiz.guess(true);
     quiz.guess(false);
     quiz.guess(false);
     quiz.guess(true);
     quiz.guess(false);
+    quiz.guess(false);
 
     expect(quiz.state).toStrictEqual({
       currentItem: 'Portuguese',
       totalItems: 3,
-      currentGuess: 6,
+      currentGuess: 7,
       correctGuesses: 2,
-      accuracy: 40,
+      accuracy: 33,
       isComplete: false,
     });
   });
 
-  it('returns state after fully completing quiz', () => {
+  it('returns state after completing quiz', () => {
     const quiz = new Quiz(['French', 'Italian', 'Portuguese']);
     quiz.guess(true);
     quiz.guess(false);
@@ -82,6 +83,24 @@ describe('Quiz', () => {
       currentGuess: 5,
       correctGuesses: 3,
       accuracy: 75,
+      isComplete: true,
+    });
+  });
+
+  it('prevents additional guesses after completing quiz', () => {
+    const quiz = new Quiz(['French', 'Italian', 'Portuguese']);
+    quiz.guess(true);
+    quiz.guess(true);
+    quiz.guess(true);
+    quiz.guess(true);
+    quiz.guess(true);
+
+    expect(quiz.state).toStrictEqual({
+      currentItem: undefined,
+      totalItems: 3,
+      currentGuess: 4,
+      correctGuesses: 3,
+      accuracy: 100,
       isComplete: true,
     });
   });
