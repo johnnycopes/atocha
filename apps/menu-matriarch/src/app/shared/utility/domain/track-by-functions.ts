9607b3afe72670c277e6c2c0bcded5932e80ec2a
John Coppola <jdcoppola@gmail.com>
2022-07-30 09:45:21 -0400

    trackBySelf passes in string generic

--- apps/menu-matriarch/src/app/shared/utility/domain/track-by-functions.ts
+++ apps/menu-matriarch/src/app/shared/utility/domain/track-by-functions.ts
@@ -2,7 +2,7 @@ import { Day } from "@models/day.type";
 import { DishType } from "@models/dish-type.type";
 import { trackByFactory } from "../generic/track-by-factory";
 
-export const trackBySelf = trackByFactory(item => item);
+export const trackBySelf = trackByFactory<string>(item => item);
 export const trackById = trackByFactory<{ id: string }>(item => item.id);
 export const trackByDay = trackByFactory<{ day: Day }>(item => item.day);
 export const trackByDishType = trackByFactory<{ type: DishType }>(item => item.type);
