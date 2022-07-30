25ca4459841a1f6d70fbf46bd6ff5ee79d6abf45
John Coppola <jdcoppola@gmail.com>
2022-07-30 09:52:09 -0400

    updates trackByDishType

--- apps/menu-matriarch/src/app/shared/utility/domain/track-by-functions.ts
+++ apps/menu-matriarch/src/app/shared/utility/domain/track-by-functions.ts
@@ -1,8 +1,8 @@
 import { Day } from "@models/day.type";
-import { DishType } from "@models/dish-type.type";
+import { FilteredDishesGroup } from "@models/filtered-dishes.interface";
 import { trackByFactory } from "../generic/track-by-factory";
 
 export const trackBySelf = trackByFactory<string>(item => item);
 export const trackById = trackByFactory<{ id: string }>(item => item.id);
 export const trackByDay = trackByFactory<{ day: Day }>(item => item.day);
-export const trackByDishType = trackByFactory<{ type: DishType }>(item => item.type);
+export const trackByDishType = trackByFactory<FilteredDishesGroup>(({ type }) => type);
