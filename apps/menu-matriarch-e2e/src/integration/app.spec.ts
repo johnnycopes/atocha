3331a66fee105292fb98e43f4b00bc57cd49a484
John Coppola <jdcoppola@gmail.com>
2022-07-30 11:48:29 -0400

    updates mm e2e test to pass

--- apps/menu-matriarch-e2e/src/integration/app.spec.ts
+++ apps/menu-matriarch-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
-describe('menu-matriarch', () => {
+describe('menu matriarch', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome menu-matriarch');
+    cy.get('h1').should('contain.text', 'Menu Matriarh');
   });
 });
