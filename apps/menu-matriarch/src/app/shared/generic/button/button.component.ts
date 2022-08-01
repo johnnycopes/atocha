be50c07e736ba80a241013bc4c91860f109b8263
John Coppola <jdcoppola@gmail.com>
2022-08-01 18:23:15 -0400

    replaces app-button usage with ui-button

--- apps/menu-matriarch/src/app/shared/generic/button/button.component.ts
+++ apps/menu-matriarch/src/app/shared/generic/button/button.component.ts
@@ -11,7 +11,7 @@ type ButtonStyle = 'primary' | 'secondary' | 'ternary' | 'danger';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
-  selector: '[app-button]',
+  selector: '[ui-button]',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,8 +28,8 @@ export class ButtonComponent {
   @HostBinding('class')
   public get hostClasses(): { [key: string]: boolean } {
     return {
-      'app-button': true,
-      [`app-button--${this.buttonStyle}`]: true,
+      'ui-button': true,
+      [`ui-button--${this.buttonStyle}`]: true,
     };
   }
 }
