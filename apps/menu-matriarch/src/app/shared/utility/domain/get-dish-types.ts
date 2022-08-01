fb41ef5353fb21940f2e96db49bfff2a9761aa7b
John Coppola <jdcoppola@gmail.com>
2022-08-01 07:18:10 -0400

    missed import; removes @models declaration in tsconfig

--- apps/menu-matriarch/src/app/shared/utility/domain/get-dish-types.ts
+++ apps/menu-matriarch/src/app/shared/utility/domain/get-dish-types.ts
@@ -1,4 +1,4 @@
-import { DishType } from '../../models/dish-type.type';
+import { DishType } from "@atocha/menu-matriarch/types";
 
 export function getDishTypes(): readonly DishType[] {
   return ['main', 'side', 'dessert'];
