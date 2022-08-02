596b6a5a83a1bddd4faf5b9ced3f34d5041dde6d
John Coppola <jdcoppola@gmail.com>
2022-08-02 07:57:14 -0400

    surfaces domain folder contents to top of shared folder

--- apps/menu-matriarch/src/app/app.module.ts
+++ apps/menu-matriarch/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { environment } from '@env/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { CoreUiModule } from '@atocha/core/ui';
-import { DomainModule } from '@shared/domain/domain.module';
+import { DomainModule } from '@shared/domain.module';
 import { MenuMatriarchUiModule } from '@atocha/menu-matriarch/ui';
 
 import { AppComponent } from './app.component';
