4ccd5b4ca8f4372b23e676ed64ae1dae0e642e9f
John Coppola <jdcoppola@gmail.com>
2022-08-08 23:44:22 -0400

    removes unneeded import of FontAwesomeModule

--- apps/menu-matriarch/src/app/app.module.ts
+++ apps/menu-matriarch/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
@@ -27,7 +26,6 @@ import { MenuMatriarchUiModule } from '@atocha/menu-matriarch/ui';
     AngularFirestoreModule,
     BrowserAnimationsModule,
     BrowserModule,
-    FontAwesomeModule,
     FormsModule,
     AppRoutingModule,
     CoreUiModule,
