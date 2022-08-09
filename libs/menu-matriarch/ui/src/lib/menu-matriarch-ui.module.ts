824a926859a22ac2517b7862791c89060976bae8
John Coppola <jdcoppola@gmail.com>
2022-08-08 23:27:34 -0400

    removes RouterModule import from ui-module

--- libs/menu-matriarch/ui/src/lib/menu-matriarch-ui.module.ts
+++ libs/menu-matriarch/ui/src/lib/menu-matriarch-ui.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { CoreUiModule, PluralPipe, SafePipe } from '@atocha/core/ui';
@@ -37,7 +36,6 @@ import { TagsListComponent } from './tags/tags-list/tags-list.component';
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule,
     FontAwesomeModule,
     CoreUiModule,
   ],
