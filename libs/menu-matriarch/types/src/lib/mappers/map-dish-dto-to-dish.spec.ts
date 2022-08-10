91ea75df446b801e8774a01d717233df09745a39
John Coppola <jdcoppola@gmail.com>
2022-08-10 07:44:24 -0400

    writes mapDishDtoToDish spec

---
+++ libs/menu-matriarch/types/src/lib/mappers/map-dish-dto-to-dish.spec.ts
@@ -0,0 +1,50 @@
+import { createDishDto, createTagDto } from '../functions/create-dtos';
+import { mapDishDtoToDish } from './map-dish-dto-to-dish';
+
+describe('mapDishDtoToDish', () => {
+  const dishDto = createDishDto({
+    id: 'dish-1',
+    uid: 'abc',
+    name: 'Pizza',
+    description: 'Delicious round vessel from Italy',
+    link: 'https://cooking.nytimes.com/guides/1-how-to-make-pizza',
+    menuIds: [],
+    tagIds: ['tag-1'],
+    usages: 2,
+  });
+  const tags = [
+    createTagDto({
+      id: 'tag-1',
+      uid: 'abc',
+      dishIds: ['dish-1'],
+      name: 'Vegetarian',
+    }),
+  ];
+
+  it('returns a dish when passed a dishDto and tags', () => {
+    expect(mapDishDtoToDish(dishDto, tags)).toEqual({
+      description: 'Delicious round vessel from Italy',
+      favorited: false,
+      id: 'dish-1',
+      ingredients: [],
+      link: 'https://cooking.nytimes.com/guides/1-how-to-make-pizza',
+      mealIds: [],
+      menuIds: [],
+      name: 'Pizza',
+      notes: '',
+      tags: [
+        {
+          color: '',
+          dishIds: ['dish-1'],
+          id: 'tag-1',
+          mealIds: [],
+          name: 'Vegetarian',
+          uid: 'abc',
+        },
+      ],
+      type: 'main',
+      uid: 'abc',
+      usages: 2,
+    });
+  });
+});
