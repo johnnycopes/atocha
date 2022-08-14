6bb531b57ffd0326538c54343178c0328548570f
John Coppola <jdcoppola@gmail.com>
2022-08-14 10:40:07 -0400

    MM Firebase config (#50)
    * moves functions folder
    
    * moves app alias to front of commands
    
    * cleans up environment files
    
    * moves firebase files up to root and fixes deploy command
    
    * updates prod deployment command
    
    * fixes scripts after moving config files
    
    * moves scripts to root and updates commands
    
    * moves functions file to root
    
    * moves most firebase config into root-level firebase folder
    
    * nx format:write

--- apps/menu-matriarch/src/environments/environment.ts
+++ apps/menu-matriarch/src/environments/environment.ts
@@ -1,11 +1,3 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
 export const environment = {
   production: false,
   firebaseConfig: {
