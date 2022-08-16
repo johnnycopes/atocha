f73a8aeb86e34e0a095499f3d0e6d405c398bf1f
John Coppola <jdcoppola@gmail.com>
2022-08-16 09:10:12 -0400

    exports components individually from index.js

--- libs/globetrotter/ui/src/index.ts
+++ libs/globetrotter/ui/src/index.ts
@@ -1,6 +1,14 @@
 export * from './lib/globetrotter-ui.module';
 
 export * from './lib/animations';
+export * from './lib/alert/alert.component';
+export * from './lib/button/button.component';
+export * from './lib/container/container.component';
 export * from './lib/fixed-slideable-panel/fixed-slideable-panel.component';
 export * from './lib/flip-card/flip-card.component';
+export * from './lib/icon/icon.component';
+export * from './lib/input/input.component';
+export * from './lib/list-details/list-details.component';
+export * from './lib/loader/loader.component';
 export * from './lib/radio-buttons/radio-buttons.component';
+export * from './lib/small-caps/small-caps.component';
