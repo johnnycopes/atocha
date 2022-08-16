e42ef8a57e53697f5330e9cc6845e0531fc3bee6
John Coppola <jdcoppola@gmail.com>
2022-08-16 07:50:48 -0400

    changes preloading strategy; removes feature module imports from app.module

--- apps/menu-matriarch/src/app/app-routing.module.ts
+++ apps/menu-matriarch/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import {
   AuthGuard,
@@ -95,7 +95,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      preloadingStrategy: PreloadAllModules,
       scrollPositionRestoration: 'enabled',
     }),
   ],
--- apps/menu-matriarch/src/app/app.module.ts
+++ apps/menu-matriarch/src/app/app.module.ts
@@ -7,13 +7,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
-import { MenuMatriarchFeatureDishesModule } from '@atocha/menu-matriarch/feature-dishes';
-import { MenuMatriarchFeatureMealsModule } from '@atocha/menu-matriarch/feature-meals';
-import { MenuMatriarchFeatureMenusModule } from '@atocha/menu-matriarch/feature-menus';
-import { MenuMatriarchFeaturePlannerModule } from '@atocha/menu-matriarch/feature-planner';
-import { MenuMatriarchFeatureSettingsModule } from '@atocha/menu-matriarch/feature-settings';
-import { MenuMatriarchFeatureShellModule } from '@atocha/menu-matriarch/feature-shell';
-import { MenuMatriarchFeatureTagsModule } from '@atocha/menu-matriarch/feature-tags';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,13 +16,6 @@ import { MenuMatriarchFeatureTagsModule } from '@atocha/menu-matriarch/feature-t
     BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    MenuMatriarchFeatureDishesModule,
-    MenuMatriarchFeatureMealsModule,
-    MenuMatriarchFeatureMenusModule,
-    MenuMatriarchFeaturePlannerModule,
-    MenuMatriarchFeatureSettingsModule,
-    MenuMatriarchFeatureShellModule,
-    MenuMatriarchFeatureTagsModule,
   ],
   bootstrap: [AppComponent],
 })
