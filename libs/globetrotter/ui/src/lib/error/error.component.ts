88255a7bef68632f6000dd56f95487576e647712
John Coppola <jdcoppola@gmail.com>
2022-08-16 09:57:41 -0400

    relative import for error animation

--- libs/globetrotter/ui/src/lib/error/error.component.ts
+++ libs/globetrotter/ui/src/lib/error/error.component.ts
@@ -1,6 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
-import { fadeInAnimation } from '@atocha/globetrotter/ui';
+import { fadeInAnimation } from '../animations';
 
 @Component({
   standalone: true,
