1eb96cefd4561705b0b43591cecff5630aba1bbb
John Coppola <jdcoppola@gmail.com>
2022-08-16 09:10:24 -0400

    updates feature-shell file

--- libs/globetrotter/feature-shell/src/lib/globetrotter-feature-shell.module.ts
+++ libs/globetrotter/feature-shell/src/lib/globetrotter-feature-shell.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { GlobetrotterUiModule } from '@atocha/globetrotter/ui';
+import { ButtonComponent, IconComponent } from '@atocha/globetrotter/ui';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -10,7 +10,12 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { ShellComponent } from './shell/shell.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, GlobetrotterUiModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    IconComponent,
+    RouterModule
+  ],
   declarations: [
     ErrorComponent,
     HomeComponent,
