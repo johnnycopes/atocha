4fd4d6ba3ae4fc6cb4521216687e97f8f325aa26
John Coppola <jdcoppola@gmail.com>
2022-08-15 23:01:02 -0400

    updates feature-tags module file

--- libs/menu-matriarch/feature-tags/src/lib/menu-matriarch-feature-tags.module.ts
+++ libs/menu-matriarch/feature-tags/src/lib/menu-matriarch-feature-tags.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { MenuMatriarchUiModule } from '@atocha/menu-matriarch/ui';
+import { CardComponent, CountComponent, InlineNameEditComponent, SectionComponent, TagComponent } from '@atocha/menu-matriarch/ui';
 import { TagCardComponent } from './tag-card/tag-card.component';
 import { TagsComponent } from './tags.component';
 
 @NgModule({
   declarations: [TagCardComponent, TagsComponent],
   imports: [
-    MenuMatriarchUiModule,
+    CardComponent,
     CommonModule,
+    CountComponent,
+    InlineNameEditComponent,
     RouterModule.forChild([{ path: '', component: TagsComponent }]),
+    SectionComponent,
+    TagComponent,
   ],
 })
 export class MenuMatriarchFeatureTagsModule {}
