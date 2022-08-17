8ecfc264ff8773e7a7083afa08e2c5de963f276a
John Coppola <jdcoppola@gmail.com>
2022-08-17 10:28:01 -0400

    deletes app.component stylesheet

--- apps/globetrotter/src/app/app.component.ts
+++ apps/globetrotter/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { ErrorService, LoaderService } from '@atocha/globetrotter/data-access';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
