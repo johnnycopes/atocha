c2ab7c76092b9731ff8e9d66a3e274d336534d57
John Coppola <jdcoppola@gmail.com>
2022-08-17 07:34:43 -0400

    refactors ErrorComponent to be resuable

--- libs/globetrotter/ui/src/lib/error/error.component.ts
+++ libs/globetrotter/ui/src/lib/error/error.component.ts
@@ -1,13 +1,21 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 
 import { fadeInAnimation } from '../animations';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   standalone: true,
   selector: 'ui-error',
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [fadeInAnimation],
 })
-export class ErrorComponent {}
+export class ErrorComponent {
+  @Input() header = '';
+  @Input() message = '';
+  @Input() action = '';
+  @Output() actionClick = new EventEmitter<void>();
+}
