e7ea9147503de2708f492c875e6f4209b3a582f5
John Coppola <jdcoppola@gmail.com>
2022-08-22 10:08:58 -0400

    writes track-by.spec.ts file

---
+++ libs/core/ui/src/lib/performance/track-by.spec.ts
@@ -0,0 +1,39 @@
+import { TrackByFunction } from '@angular/core';
+import { trackByFactory, trackBySelf } from "./track-by";
+
+interface ComplexVeggie { id: string, name: string };
+type SimpleVeggie = string;
+
+
+describe('trackBy', () => {
+  describe('trackByFactory', () => {
+
+    it('returns the item itself (a string value) as the key to track by', () => {
+      const veggies: ComplexVeggie[] = [
+        { id: '1', name: 'broccoli' },
+        { id: '2', name: 'spinach' },
+        { id: '3', name: 'cucumber' },
+      ];
+      const defaultTrackByFn: TrackByFunction<ComplexVeggie> = (index, { id }) => id;
+      const idTrackByFn = trackByFactory<ComplexVeggie>(({ id }) => id);
+
+      const trackByFnVeggies = veggies.map((vegetable, index) => defaultTrackByFn(index, vegetable));
+      const trackBySelfVeggies = veggies.map((vegetable, index) => idTrackByFn(index, vegetable));
+
+      expect(trackByFnVeggies).toEqual(trackBySelfVeggies);
+    });
+  });
+
+  describe('trackBySelf', () => {
+
+    it('returns the item itself (a string value) as the key to track by', () => {
+      const veggies = ['broccoli', 'spinach', 'cucumber'];
+      const defaultTrackByFn: TrackByFunction<SimpleVeggie> = (index: number, item: string) => item;
+
+      const trackByFnVeggies = veggies.map((vegetable, index) => defaultTrackByFn(index, vegetable));
+      const trackBySelfVeggies = veggies.map((vegetable, index) => trackBySelf(index, vegetable));
+
+      expect(trackByFnVeggies).toEqual(trackBySelfVeggies);
+    });
+  });
+});
