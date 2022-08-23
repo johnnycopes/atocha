fb4c2a87d7cea437d263ad48404feddb2a7a494a
John Coppola <jdcoppola@gmail.com>
2022-08-23 08:31:11 -0400

    writes additional test for object accumulation

--- libs/core/util/src/lib/act-recursively.spec.ts
+++ libs/core/util/src/lib/act-recursively.spec.ts
@@ -2,6 +2,7 @@ import { actRecursively } from './act-recursively';
 
 interface Item {
   name: string;
+  description?: string;
   children?: Item[];
 }
 
@@ -87,5 +88,37 @@ describe('actRecursively', () => {
       'Item 1C',
       'Item 1C.1',
     ]);
+  });
+
+  it('returns an object of all items keyed by item description', () => {
+    const item: Item = {
+      name: 'Item 1',
+      description: 'First item',
+      children: [
+        { name: 'Item 1A', description: 'First item A' },
+        { name: 'Item 1B', description: 'First item B' },
+        {
+          name: 'Item 1C',
+          children: [{ name: 'Item 1C.1', description: 'First item C.1' }]
+        },
+      ],
+    };
+    const recorder = (
+      accumulator: Record<string, string>,
+      item: Item
+    ) => ({ ...accumulator, [item.name]: item.description ?? 'No description' });
+
+    expect(actRecursively({
+      item,
+      getChildren,
+      reducer: recorder,
+      accumulator: {},
+    })).toEqual({
+      "Item 1": "First item",
+      "Item 1A": "First item A",
+      "Item 1B": "First item B",
+      "Item 1C": "No description",
+      "Item 1C.1": "First item C.1",
+    });
   });
 });
