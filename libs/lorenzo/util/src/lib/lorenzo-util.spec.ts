fbb791414886be698b7d233ebd30b22c1a137f54
John Coppola <jdcoppola@gmail.com>
2022-08-24 09:10:21 -0400

    deletes boilerplate files

--- libs/lorenzo/util/src/lib/lorenzo-util.spec.ts
+++
@@ -1,7 +0,0 @@
-import { lorenzoUtil } from './lorenzo-util';
-
-describe('lorenzoUtil', () => {
-  it('should work', () => {
-    expect(lorenzoUtil()).toEqual('lorenzo-util');
-  });
-});
--- libs/lorenzo/util/src/lib/lorenzo-util.ts
+++
@@ -1,3 +0,0 @@
-export function lorenzoUtil(): string {
-  return 'lorenzo-util';
-}
