8611e3fbbde390754f21bf98e6acd76853692c69
John Coppola <jdcoppola@gmail.com>
2022-08-25 09:45:41 -0400

    casing

--- libs/lorenzo/util/src/lib/leaders.ts
+++ libs/lorenzo/util/src/lib/leaders.ts
@@ -4,30 +4,30 @@ export const LEADERS: readonly Leader[] = [
   {
     name: "Alfonso V d'Aragona",
     requirement: '6 Military Points and 0 Faith Points',
-    ability: 'Each time you take a Territory card, gain 1 Faith Point',
+    ability: 'Each time you take a Territory Card, gain 1 Faith Point',
     type: 'permanent',
   },
   {
     name: "Alfonso I d'Este",
     requirement: '10 coins and max 1 servant',
-    ability: 'Each time you take a Character card, gain 2 servants',
+    ability: 'Each time you take a Character Card, gain 2 servants',
     type: 'permanent',
   },
   {
     name: 'Bartolomeo Colleoni',
-    requirement: '2 Venture cards and 4 Territory cards',
+    requirement: '2 Venture Cards and 4 Territory Cards',
     ability: 'Gain 4 Victory Points',
     type: 'once per round',
   },
   {
     name: "Beatrice d'Este",
-    requirement: '5 Development Cards with max 1 Character card and max 1 coin',
+    requirement: '5 Development Cards with max 1 Character Card and max 1 coin',
     ability: 'Discard 1 Special Token to gain 5 Victory Points OR 5 coins',
     type: 'once per round',
   },
   {
     name: 'Bramante',
-    requirement: '4 Venture cards, 4 servants, 4 wood, and 4 coins',
+    requirement: '4 Venture Cards, 4 servants, 4 wood, and 4 coins',
     ability:
       'When you place one of your Family Members here, perform a Harvest action OR a Production action at the value of the placed Family Member. You can increase this action value',
     type: 'action',
@@ -40,20 +40,20 @@ export const LEADERS: readonly Leader[] = [
   },
   {
     name: 'Cesare Borgia',
-    requirement: '3 Building cards, 12 coins, and 2 Faith Points',
+    requirement: '3 Building Cards, 12 coins, and 2 Faith Points',
     ability:
-      'You may waive the Military Points requirement when you take Territory cards',
+      'You may waive the Military Points requirement when you take Territory Cards',
     type: 'permanent',
   },
   {
     name: "Cosimo de' Medici",
-    requirement: '2 Character cards and 4 Building cards',
+    requirement: '2 Character Cards and 4 Building Cards',
     ability: 'Gain 3 servants and 1 Victory Point',
     type: 'once per round',
   },
   {
     name: 'Erasmo da Rotterdam',
-    requirement: '4 Venture cards and 3 Character cards',
+    requirement: '4 Venture Cards and 3 Character Cards',
     ability:
       'When you place one of your Family Members here, gain 3 wood, 3 stone, and 3 Victory Points',
     type: 'action',
@@ -61,12 +61,12 @@ export const LEADERS: readonly Leader[] = [
   {
     name: "Ercole I d'Este",
     requirement: '4 Faith Points, max 1 Military Point, and max 1 coin',
-    ability: 'Each time you take a Building card, gain 2 Military Points',
+    ability: 'Each time you take a Building Card, gain 2 Military Points',
     type: 'permanent',
   },
   {
     name: 'Federico da Montefeltro',
-    requirement: '5 Territory cards',
+    requirement: '5 Territory Cards',
     ability:
       'One of your colored Family Members has value 6, regardless of its related die',
     type: 'once per round',
@@ -74,19 +74,19 @@ export const LEADERS: readonly Leader[] = [
   {
     name: "Ferdinando d'Aragona",
     requirement: '8 coins, 8 servants, and max 1 combined wood and stone',
-    ability: 'Each time you take a Venture card, gain 1 wood and 1 stone',
+    ability: 'Each time you take a Venture Card, gain 1 wood and 1 stone',
     type: 'permanent',
   },
   {
     name: 'Filippo Brunelleschi',
-    requirement: '5 Building cards',
+    requirement: '5 Building Cards',
     ability:
       "You don't have to spend 3 coins when you place your Family Members in an already occupied tower",
     type: 'permanent',
   },
   {
     name: 'Francesco Sforza',
-    requirement: '5 Venture cards',
+    requirement: '5 Venture Cards',
     ability: 'Perform a Harvest action at value 1. You may increase this value',
     type: 'once per round',
   },
@@ -110,33 +110,33 @@ export const LEADERS: readonly Leader[] = [
   },
   {
     name: "Guglielmo de' Pazzi",
-    requirement: '4 Character cards and 8 coins',
+    requirement: '4 Character Cards and 8 coins',
     ability:
       'When you place one of your Family Members here, gain 2 Military Points and 3 Faith Points',
     type: 'action',
   },
   {
     name: 'Jacopo Salivate',
-    requirement: '3 Building cards and 8 Military Points',
+    requirement: '3 Building Cards and 8 Military Points',
     ability:
       'Whenever you take 1 or more Council Privileges, take 1 additional Council Privilege of any type',
     type: 'permanent',
   },
   {
     name: 'Ladislao VII Jagellone',
-    requirement: '10 Victory Points and max 1 Building card',
-    ability: 'You can play a Leader card regardless of its requirements',
+    requirement: '10 Victory Points and max 1 Building Card',
+    ability: 'You can play a Leader Card regardless of its requirements',
     type: 'immediate',
   },
   {
     name: 'Leon Battista Alberti',
-    requirement: '3 Venture cards and 10 servants',
-    ability: 'Perform a Production action at value 3 on 1 Building card',
+    requirement: '3 Venture Cards and 10 servants',
+    ability: 'Perform a Production action at value 3 on 1 Building Card',
     type: 'once per round',
   },
   {
     name: 'Leonardo da Vinci',
-    requirement: '4 Character cards and 2 Territory cards',
+    requirement: '4 Character Cards and 2 Territory Cards',
     ability:
       'Perform a Production action at value 0. You may increase this value',
     type: 'once per round',
@@ -145,13 +145,13 @@ export const LEADERS: readonly Leader[] = [
     name: "Lorenzo de' Medici",
     requirement: '35 Victory Points',
     ability:
-      "Copy the ability of another Leader card already played by another player. Once you've decided which ability to copy, it cannot be changed",
-    type: 'immediate',
+      "Copy the ability of another Leader Card already played by another player. Once you've decided which ability to copy, it cannot be changed",
+    type: 'permanent',
   },
   {
     name: 'Lucia da Narni',
     requirement:
-      '6 Development Cards, max 1 Territory card, and max 2 Special Tokens',
+      '6 Development Cards, max 1 Territory Card, and max 2 Special Tokens',
     ability: 'Gain 1 Faith Point and 2 Victory Points',
     type: 'once per round',
   },
@@ -164,7 +164,7 @@ export const LEADERS: readonly Leader[] = [
   },
   {
     name: 'Ludovico Ariostso',
-    requirement: '5 Character cards',
+    requirement: '5 Character Cards',
     ability: 'You can place your Family Members in occupied action spaces',
     type: 'permanent',
   },
@@ -177,27 +177,27 @@ export const LEADERS: readonly Leader[] = [
   {
     name: 'Ludovico il Moro',
     requirement:
-      '2 Territory cards, 2 Character cards, 2 Building cards, and 2 Venture cards',
+      '2 Territory Cards, 2 Character Cards, 2 Building Cards, and 2 Venture Cards',
     ability:
       'Your colored Family Members have a value of 5, regardless of their related dice. You may increase this value',
     type: 'permanent',
   },
   {
     name: 'Marsilio Ficino',
-    requirement: '4 Faith Points, 5 stone, and a max of 1 Venture card',
+    requirement: '4 Faith Points, 5 stone, and a max of 1 Venture Card',
     ability: 'Draw 2 Special Tokens. Choose 1 to keep and discard the other',
     type: 'once per round',
   },
   {
     name: 'Martin Lutero',
     requirement:
-      '7 Military Points, 1 Territory card, 1 Character card, 1 Building card, and 1 Venture card',
+      '7 Military Points, 1 Territory Card, 1 Character Card, 1 Building Card, and 1 Venture Card',
     ability: 'You cannot be excommunicated',
     type: 'permanent',
   },
   {
     name: "Massimilano I d'Asburgo",
-    requirement: '4 Territory cards and 4 Building cards',
+    requirement: '4 Territory Cards and 4 Building Cards',
     ability:
       'When you place one of your Family Members here, gain 3 Special Tokens and 5 Victory Points',
     type: 'action',
@@ -210,28 +210,28 @@ export const LEADERS: readonly Leader[] = [
   },
   {
     name: 'Niccoló Machiavelli',
-    requirement: '3 Territory cards, 20 Victory Points, and 1 Leader',
+    requirement: '3 Territory Cards, 20 Victory Points, and 1 Leader',
     ability:
       'Whenever you gain Victory Points, gain 1 additional Victory Point',
     type: 'permanent',
   },
   {
     name: 'Palla Strozzi',
-    requirement: '4 Territory cards and 5 Faith Points',
-    ability: 'Perform a Harvest action at value 3 on 1 Territory card',
+    requirement: '4 Territory Cards and 5 Faith Points',
+    ability: 'Perform a Harvest action at value 3 on 1 Territory Card',
     type: 'once per round',
   },
   {
     name: 'Paracelso',
-    requirement: '3 Territory cards and 3 stone',
+    requirement: '3 Territory Cards and 3 stone',
     ability: 'You can spend servants instead of wood, stone, or coins',
     type: 'permanent',
   },
   {
     name: 'Pico della Mirandola',
-    requirement: '4 Venture cards and 2 Building cards',
+    requirement: '4 Venture Cards and 2 Building Cards',
     ability:
-      'When you take Development Cards, you get a discount of 3 coins for the cards, but not for the occupied tower cost',
+      'When you take Development Cards, you get a discount of 3 coins for the Cards, but not for the occupied tower cost',
     type: 'permanent',
   },
   {
@@ -243,14 +243,14 @@ export const LEADERS: readonly Leader[] = [
   },
   {
     name: 'Pietro Bembo',
-    requirement: '3 Character cards and 3 Faith Points',
+    requirement: '3 Character Cards and 3 Faith Points',
     ability:
       'When you place one of your Family Members here, gain 3 coins and 3 servants',
     type: 'action',
   },
   {
     name: 'Raffaello Sanzio',
-    requirement: '3 Character cards, 6 wood, and 4 servants',
+    requirement: '3 Character Cards, 6 wood, and 4 servants',
     ability:
       'You may lose 1 Victory Point to gain 3 coins OR discard 1 coin to gain 3 Victory Points',
     type: 'once per round',
@@ -284,8 +284,8 @@ export const LEADERS: readonly Leader[] = [
   },
   {
     name: 'Tomas de Torquemada',
-    requirement: '3 Building cards and 5 wood',
-    ability: 'Discard a Character card to gain 3 Faith Points',
+    requirement: '3 Building Cards and 5 wood',
+    ability: 'Discard a Character Card to gain 3 Faith Points',
     type: 'once per round',
   },
 ];
