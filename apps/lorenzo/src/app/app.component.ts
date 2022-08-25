fcf2088adf0af1171f8085b6b976e927ea73a4c9
John Coppola <jdcoppola@gmail.com>
2022-08-25 11:09:29 -0400

    begins on MVP

--- apps/lorenzo/src/app/app.component.ts
+++ apps/lorenzo/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { trackByFactory } from '@atocha/core/ui';
+
+import { Development, DEVELOPMENTS, Leader, LEADERS } from '@atocha/lorenzo/util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'lorenzo';
+  text = '';
+  readonly leaders = LEADERS;
+  readonly developments = DEVELOPMENTS;
+
+  leaderTrackByFn = trackByFactory<Leader>(({ name }) => name);
+  developmentTrackByFn = trackByFactory<Development>(({ id }) => id.toString());
 }
--- apps/lorenzo/src/app/app.module.ts
+++ apps/lorenzo/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
