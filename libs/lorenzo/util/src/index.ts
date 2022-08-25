511f3ecb768e44a8c8c0af6c3b82204df870a9ad
John Coppola <jdcoppola@gmail.com>
2022-08-25 10:31:26 -0400

    Lorenzo `util` lib (#92)
    * adds lorenzo scope tag to workspace.json and eslintrc.json
    
    * generates @atocha/lorenzo/util lib
    
    * deletes boilerplate files
    
    * writes leader.interface and leaders const
    
    * nx format:write
    
    * improves language in descriptions
    
    * defines Development interface
    
    * changes LEADERS array to readonly; fixing typos
    
    * improves Development interface
    
    * begins fleshing out DEVELOPMENTS array
    
    * leader casing changes
    
    * finishes periods 2 and 3 of Character cards
    
    * begins adding territory cards
    
    * defines Territory card data
    
    * starts defining Venture cards
    
    * finishes venture cards
    
    * Special Development cards period 1
    
    * Special Development cards period 2
    
    * preps remaining gaps
    
    * Special Development period 3
    
    * Building Cards period 1
    
    * Building Cards period 3
    
    * changes key 'number' to 'id'
    
    * casing
    
    * consistent grammar
    
    * more consistency
    
    * nx format:write
    
    * export interfaces and constants
    
    * adds comments to interfaces

---
+++ libs/lorenzo/util/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/development.interface';
+export * from './lib/leader.interface';
+
+export * from './lib/developments';
+export * from './lib/leaders';
---
+++ libs/lorenzo/util/src/lib/leader.interface.ts
@@ -0,0 +1,10 @@
+export interface Leader {
+  /** The leader's name, printed at the top of the card */
+  name: string;
+  /** What must be possessed (not spent) to play the card */
+  requirement: string;
+  /** What happens when the card is played */
+  ability: string;
+  /** The card's ability type */
+  type: 'permanent' | 'once per round' | 'action' | 'immediate';
+}
