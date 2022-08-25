23e2970bc427584f13ae5674b752a44c0a3fe292
John Coppola <jdcoppola@gmail.com>
2022-08-25 11:23:35 -0400

    gets search working via observables

--- apps/lorenzo/src/app/app.component.ts
+++ apps/lorenzo/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
 import { trackByFactory } from '@atocha/core/ui';
+import { includes } from '@atocha/core/util';
 
 import { Development, DEVELOPMENTS, Leader, LEADERS } from '@atocha/lorenzo/util';
+import { BehaviorSubject, combineLatest, of } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +14,34 @@ import { Development, DEVELOPMENTS, Leader, LEADERS } from '@atocha/lorenzo/util
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  text = '';
+  private _textSubject = new BehaviorSubject<string>('');
+  text$ = this._textSubject.pipe(
+    distinctUntilChanged(),
+  );
   readonly leaders = LEADERS;
   readonly developments = DEVELOPMENTS;
 
+  vm$ = combineLatest([
+    this.text$,
+    of(LEADERS),
+    of(DEVELOPMENTS),
+  ]).pipe(
+    map(([text, leaders, developments]) => ({
+      text,
+      filteredLeaders: leaders.filter(({ name }) =>
+        includes([name], text)
+      ),
+      filteredDevelopments: developments.filter(({ id }) =>
+        includes([id.toString()], text)
+      ),
+    }))
+  );
+
   leaderTrackByFn = trackByFactory<Leader>(({ name }) => name);
   developmentTrackByFn = trackByFactory<Development>(({ id }) => id.toString());
+
+  search(text: string): void {
+    this._textSubject.next(text);
+  }
 }
+
