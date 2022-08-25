4f601aa590f04b6c78998037d355db316a999057
John Coppola <jdcoppola@gmail.com>
2022-08-25 10:25:01 -0400

    adds comments to interfaces

--- libs/lorenzo/util/src/lib/development.interface.ts
+++ libs/lorenzo/util/src/lib/development.interface.ts
@@ -1,11 +1,18 @@
 type Type = 'territory' | 'building' | 'character' | 'venture';
 
 export interface Development {
+  /** The unique number printed near the top right corner of the card */
   id: number;
+  /** The large Roman numeral printed on the back of the card */
   period: 1 | 2 | 3;
+  /** The type of deck the card belongs to, indicated by the color of the back of the card */
   deck: Type | 'special';
+  /** The type of the card itself, indicated by the color on the front of the card */
   type: Type;
+  /** How much it costs to purchase the card, printed in near the top left corner of the card (Territory Cards are always free) */
   cost?: string;
+  /** What happens immediately when the card is taken */
   immediateEffect?: string;
+  /** What happens for the rest of the game when the card is taken */
   permanentEffect?: string;
 }
