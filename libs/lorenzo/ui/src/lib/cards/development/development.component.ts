26260f2c25909f18d0ff29bdc1b7be9e05d7c778
John Coppola <jdcoppola@gmail.com>
2022-08-28 09:33:10 -0400

    nx format:write

--- libs/lorenzo/ui/src/lib/cards/development/development.component.ts
+++ libs/lorenzo/ui/src/lib/cards/development/development.component.ts
@@ -1,8 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Development } from '@atocha/lorenzo/util';
 
