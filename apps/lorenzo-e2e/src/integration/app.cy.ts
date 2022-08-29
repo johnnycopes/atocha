c774680a60b74f731482bfccdb551420e2642953
John Coppola <jdcoppola@gmail.com>
2022-08-29 00:30:31 -0400

    updates cypress test

--- apps/lorenzo-e2e/src/integration/app.cy.ts
+++ apps/lorenzo-e2e/src/integration/app.cy.ts
@@ -2,6 +2,6 @@ describe('lorenzo', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    cy.get('h1').should('contain.text', 'Lorenzo il Magnifico');
+    cy.get('h1').should('contain.text', 'Lorenzo');
   });
 });
