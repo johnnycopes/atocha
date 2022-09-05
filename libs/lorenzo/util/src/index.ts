d1c66fdd7b7ee1c231c40b118f99dbb6e6c6f408
John Coppola <jdcoppola@gmail.com>
2022-09-05 12:07:56 -0400

    deletes deleted interface export

--- libs/lorenzo/util/src/index.ts
+++ libs/lorenzo/util/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/development.interface';
 export * from './lib/leader.interface';
-export * from './lib/settings.interface';
 
 export * from './lib/developments';
 export * from './lib/leaders';
