c329e58a65ac948a1cf46110ebea8ed636594ab6
John Coppola <jdcoppola@gmail.com>
2022-09-06 07:56:36 -0400

    adds static type guard to CardTemplateDirective

--- libs/lorenzo/ui/src/lib/cards/card-template.directive.ts
+++ libs/lorenzo/ui/src/lib/cards/card-template.directive.ts
@@ -1,7 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
+
+interface CardTemplateContext<T> {
+  $implicit: T;
+}
 
 @Directive({
   standalone: true,
   selector: 'ng-template[uiCard]',
 })
-export class CardTemplateDirective {}
+export class CardTemplateDirective<TCard> {
+  // eslint-disable-next-line @angular-eslint/no-input-rename
+  @Input('uiCard') card: TCard[] = [];
+
+  static ngTemplateContextGuard<TContextItem>(
+    dir: CardTemplateDirective<TContextItem>,
+    ctx: unknown
+  ): ctx is CardTemplateContext<TContextItem> {
+    return true;
+  }
+}
