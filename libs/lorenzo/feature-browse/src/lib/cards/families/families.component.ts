03de0a266229321002c4bc802c31a10e6a505d0e
John Coppola <jdcoppola@gmail.com>
2022-09-16 12:08:51 -0400

    refactors families to consume service data

--- libs/lorenzo/feature-browse/src/lib/cards/families/families.component.ts
+++ libs/lorenzo/feature-browse/src/lib/cards/families/families.component.ts
@@ -1,15 +1,10 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { trackByFactory } from '@atocha/core/ui';
+import { BrowseService } from '@atocha/lorenzo/data-access';
 import { CardsComponent, CardTemplateDirective } from '@atocha/lorenzo/ui';
-import { Family, getFamilyId, Ordinal } from '@atocha/lorenzo/util';
+import { getFamilyId } from '@atocha/lorenzo/util';
 import { FamilyComponent } from './family.component';
 
 @Component({
@@ -22,34 +17,42 @@ import { FamilyComponent } from './family.component';
     FamilyComponent,
   ],
   template: `
-    <ui-cards
+    <ui-cards *ngIf="families$ | async as data"
       type="Families"
-      [cards]="families"
-      [total]="total"
-      [ordinal]="ordinal"
-      [favorites]="favoriteIds"
+      [cards]="data.filteredCards"
+      [total]="data.totalCards"
+      [ordinal]="data.ordinal"
+      [favorites]="data.favoriteIds"
       [trackByFn]="trackByFn"
-      (moveUp)="moveUp.emit()"
-      (moveDown)="moveDown.emit()"
+      (moveUp)="moveUp()"
+      (moveDown)="moveDown()"
     >
       <app-family
-        *uiCard="families as family"
+        *uiCard="data.filteredCards as family"
         [data]="family"
-        [favorite]="favoriteIds.has(getId(family))"
-        (favoriteChange)="toggleId.emit(getId(family))"
+        [favorite]="data.favoriteIds.has(getId(family))"
+        (favoriteChange)="toggleId(getId(family))"
       ></app-family>
     </ui-cards>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FamiliesComponent {
-  @Input() families: readonly Family[] = [];
-  @Input() total = 0;
-  @Input() ordinal: Ordinal = 1;
-  @Input() favoriteIds = new Set<string>();
-  @Output() toggleId = new EventEmitter<string>();
-  @Output() moveUp = new EventEmitter<void>();
-  @Output() moveDown = new EventEmitter<void>();
   getId = getFamilyId;
   trackByFn = trackByFactory(this.getId);
+  families$ = this._browseService.families$;
+
+  constructor(private _browseService: BrowseService) {}
+
+  moveDown(): void {
+    this._browseService.moveDown('family');
+  }
+
+  moveUp(): void {
+    this._browseService.moveUp('family');
+  }
+
+  toggleId(id: string): void {
+    this._browseService.toggleFavoriteId(id, 'family');
+  }
 }
