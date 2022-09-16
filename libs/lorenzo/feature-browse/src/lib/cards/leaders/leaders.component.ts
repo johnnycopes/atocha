442bd2ac318e62ad769e2440ba25377813cd94b4
John Coppola <jdcoppola@gmail.com>
2022-09-16 12:11:33 -0400

    refactors leaders to consume service data

--- libs/lorenzo/feature-browse/src/lib/cards/leaders/leaders.component.ts
+++ libs/lorenzo/feature-browse/src/lib/cards/leaders/leaders.component.ts
@@ -1,15 +1,10 @@
 import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { trackByFactory } from '@atocha/core/ui';
+import { BrowseService } from '@atocha/lorenzo/data-access';
 import { CardsComponent, CardTemplateDirective } from '@atocha/lorenzo/ui';
-import { getLeaderId, Leader, Ordinal } from '@atocha/lorenzo/util';
+import { getLeaderId } from '@atocha/lorenzo/util';
 import { LeaderComponent } from './leader.component';
 
 @Component({
@@ -22,33 +17,41 @@ import { LeaderComponent } from './leader.component';
     LeaderComponent,
   ],
   template: `
-    <ui-cards
+    <ui-cards *ngIf="leaders$ | async as data"
       type="Leaders"
-      [cards]="leaders"
-      [total]="total"
-      [ordinal]="ordinal"
-      [favorites]="favoriteIds"
-      (moveUp)="moveUp.emit()"
-      (moveDown)="moveDown.emit()"
+      [cards]="data.filteredCards"
+      [total]="data.totalCards"
+      [ordinal]="data.ordinal"
+      [favorites]="data.favoriteIds"
+      (moveUp)="moveUp()"
+      (moveDown)="moveDown()"
     >
       <app-leader
-        *uiCard="leaders as leader"
+        *uiCard="data.filteredCards as leader"
         [data]="leader"
-        [favorite]="favoriteIds.has(getId(leader))"
-        (favoriteChange)="toggleId.emit(getId(leader))"
+        [favorite]="data.favoriteIds.has(getId(leader))"
+        (favoriteChange)="toggleId(getId(leader))"
       ></app-leader>
     </ui-cards>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeadersComponent {
-  @Input() leaders: readonly Leader[] = [];
-  @Input() total = 0;
-  @Input() ordinal: Ordinal = 1;
-  @Input() favoriteIds = new Set<string>();
-  @Output() toggleId = new EventEmitter<string>();
-  @Output() moveUp = new EventEmitter<void>();
-  @Output() moveDown = new EventEmitter<void>();
   getId = getLeaderId;
   trackByFn = trackByFactory(this.getId);
+  leaders$ = this._browseService.leaders$;
+
+  constructor(private _browseService: BrowseService) {}
+
+  moveDown(): void {
+    this._browseService.moveDown('leader');
+  }
+
+  moveUp(): void {
+    this._browseService.moveUp('leader');
+  }
+
+  toggleId(id: string): void {
+    this._browseService.toggleFavoriteId(id, 'leader');
+  }
 }
