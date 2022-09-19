03e59a8563179c29eecce9fc5c8f23de03cde381
John Coppola <jdcoppola@gmail.com>
2022-09-19 09:48:50 -0400

    removes unused items from toolbar

--- libs/lorenzo/feature-browse/src/lib/toolbar/toolbar.component.ts
+++ libs/lorenzo/feature-browse/src/lib/toolbar/toolbar.component.ts
@@ -1,20 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { AppStateService, CardStateService } from '@atocha/lorenzo/data-access';
+import { CardStateService } from '@atocha/lorenzo/data-access';
 
 @Component({
   standalone: true,
   selector: 'app-toolbar',
-  imports: [],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarComponent {
-  constructor(
-    private _appStateService: AppStateService,
-    private _cardStateService: CardStateService,
-  ) {}
+  constructor(private _cardStateService: CardStateService) {}
 
   clearFavorites(): void {
     this._cardStateService.clearFavoriteIds();
