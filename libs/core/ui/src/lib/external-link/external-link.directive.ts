de6606636f8e1d692aaeb005e3309b6b96c2b8e3
John Coppola <jdcoppola@gmail.com>
2022-09-20 07:52:58 -0400

    simplifies directive usage

--- libs/core/ui/src/lib/external-link/external-link.directive.ts
+++ libs/core/ui/src/lib/external-link/external-link.directive.ts
@@ -1,10 +1,15 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   standalone: true,
 	selector: 'a[coreExternalLink]',
 })
 export class ExternalLinkDirective {
+  // eslint-disable-next-line @angular-eslint/no-input-rename
+  @Input('coreExternalLink')
+  @HostBinding('href')
+  link = '';
+
 	@HostBinding('target')
 	linkTarget = '_blank';
 
