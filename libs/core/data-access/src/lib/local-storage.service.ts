88be245b9a8632c00759bdd125967653f6104178
John Coppola <jdcoppola@gmail.com>
2022-10-21 09:08:23 -0400

    creates APP_NAME_TOKEN in local-storage.service

--- libs/core/data-access/src/lib/local-storage.service.ts
+++ libs/core/data-access/src/lib/local-storage.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
+
+export const APP_NAME_TOKEN = new InjectionToken<string>('appName');
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
+  constructor(@Inject(APP_NAME_TOKEN) private _appName: string) {}
+
   getItem(key: string): string | null {
     return window.localStorage.getItem(key);
   }
