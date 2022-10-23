197d9818290cbf984825404364d04dc295a67245
John Coppola <jdcoppola@gmail.com>
2022-10-23 09:46:04 -0400

    TOOL: find + replace uses of oldLoggedIn$ with loggedIn$

--- libs/menu-matriarch/data-access/src/lib/auth.guard.ts
+++ libs/menu-matriarch/data-access/src/lib/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return this._authService.oldLoggedIn$.pipe(
+    return this._authService.loggedIn$.pipe(
       first(),
       map((loggedIn) => {
         if (!loggedIn) {
