80ab4e019ee93da076d3842de42d605a42673218
John Coppola <jdcoppola@gmail.com>
2022-10-27 20:03:56 -0400

    FILE: creates store.ts and exports it from lib

--- libs/core/util/src/index.ts
+++ libs/core/util/src/index.ts
@@ -13,6 +13,7 @@ export * from './lib/reduce-recursively';
 export * from './lib/select-random';
 export * from './lib/snake-case';
 export * from './lib/sort';
+export * from './lib/store';
 export * from './lib/tally';
 export * from './lib/unique-diff';
 export * from './lib/wait';
