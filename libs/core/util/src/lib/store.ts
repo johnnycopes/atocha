77a801de92e7d1a2c0fe543b7282ceb5e462fa0a
John Coppola <jdcoppola@gmail.com>
2022-10-27 20:04:42 -0400

    FEATURE: writes functionality: state$ observable and a few methods

--- libs/core/util/src/lib/store.ts
+++ libs/core/util/src/lib/store.ts
@@ -1,1 +1,38 @@
-export class Store<State> {}
+import {
+  BehaviorSubject,
+  distinctUntilChanged,
+  first,
+  map,
+  Observable,
+  shareReplay,
+} from 'rxjs';
+
+export class Store<State extends object> {
+  private _stateSubject: BehaviorSubject<State>;
+  state$: Observable<State>;
+
+  constructor(state: State) {
+    this._stateSubject = new BehaviorSubject(state);
+    this.state$ = this._stateSubject.pipe(
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  getProp<K extends keyof State>(key: K): Observable<State[K]> {
+    return this.state$.pipe(
+      map((state) => state[key]),
+      distinctUntilChanged(),
+      shareReplay({ bufferSize: 1, refCount: true })
+    );
+  }
+
+  update(value: State): void {
+    this._stateSubject.next(value);
+  }
+
+  updateProp<K extends keyof State>(key: K, value: State[K]): void {
+    this.state$.pipe(first()).subscribe((state) => {
+      this._stateSubject.next({ ...state, [key]: value });
+    });
+  }
+}
