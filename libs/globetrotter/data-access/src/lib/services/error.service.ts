043b8f472a03158fe7c903ca54b459d3065108bd
John Coppola <jdcoppola@gmail.com>
2022-10-29 19:32:23 -0400

    FEATURE: refactors error.service.ts

--- libs/globetrotter/data-access/src/lib/services/error.service.ts
+++ libs/globetrotter/data-access/src/lib/services/error.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { distinctUntilChanged, shareReplay } from 'rxjs/operators';
+
+import { State } from '@atocha/core/util';
+
+interface Errors {
+  global: boolean;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorService {
-  private readonly _globalSubject = new BehaviorSubject<boolean>(false);
-  global$ = this._globalSubject.pipe(
-    distinctUntilChanged(),
-    shareReplay({ bufferSize: 1, refCount: true })
-  );
+  private readonly _state = new State<Errors>({ global: false });
+  global$ = this._state.getProp('global');
 
   setGlobalError(error: boolean): void {
-    this._globalSubject.next(error);
+    this._state.updateProp('global', error);
   }
 }
