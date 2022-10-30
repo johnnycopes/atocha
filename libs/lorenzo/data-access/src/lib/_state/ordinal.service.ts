aab4fed17d1ae8a44861f8e757f19367d76ab588
John Coppola <jdcoppola@gmail.com>
2022-10-30 08:59:05 -0400

    FEATURE: refactors OrdinalService

--- libs/lorenzo/data-access/src/lib/_state/ordinal.service.ts
+++ libs/lorenzo/data-access/src/lib/_state/ordinal.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, first, shareReplay, tap } from 'rxjs';
+import { first, shareReplay, tap } from 'rxjs';
 
 import { LocalStorageService } from '@atocha/core/data-access';
+import { State } from '@atocha/core/util';
 import { Card, Ordinal } from '@atocha/lorenzo/util';
+
+type Ordinals = Record<Card, Ordinal>;
 
 @Injectable({
   providedIn: 'root',
@@ -13,13 +16,13 @@ export class OrdinalService {
     family: 'FAMILY_ORDINAL',
     leader: 'LEADER_ORDINAL',
   };
-  private _ordinalSubject = new BehaviorSubject<Record<Card, Ordinal>>({
+  private _ordinals = new State<Ordinals>({
     development: this._getOrdinal(this._keys.development, 3),
     family: this._getOrdinal(this._keys.family, 1),
     leader: this._getOrdinal(this._keys.leader, 2),
   });
 
-  ordinal$ = this._ordinalSubject.pipe(
+  ordinal$ = this._ordinals.get().pipe(
     tap(({ development, family, leader }) => {
       this._setOrdinal(this._keys.development, development);
       this._setOrdinal(this._keys.family, family);
@@ -29,35 +32,41 @@ export class OrdinalService {
   );
 
   incrementOrdinal(type: Card): void {
-    this._ordinalSubject.pipe(first()).subscribe((ordinals) => {
-      const currentOrdinal = ordinals[type];
-      const target = Object.entries(ordinals).find(
-        ([_, ordinal]) => ordinal === currentOrdinal + 1
-      );
-      if (target) {
-        this._ordinalSubject.next({
-          ...ordinals,
-          [target[0]]: currentOrdinal,
-          [type]: target[1],
-        });
-      }
-    });
+    this._ordinals
+      .get()
+      .pipe(first())
+      .subscribe((ordinals) => {
+        const currentOrdinal = ordinals[type];
+        const target = Object.entries(ordinals).find(
+          ([_, ordinal]) => ordinal === currentOrdinal + 1
+        );
+        if (target) {
+          this._ordinals.update({
+            ...ordinals,
+            [target[0]]: currentOrdinal,
+            [type]: target[1],
+          });
+        }
+      });
   }
 
   decrementOrdinal(type: Card): void {
-    this._ordinalSubject.pipe(first()).subscribe((ordinals) => {
-      const currentOrdinal = ordinals[type];
-      const target = Object.entries(ordinals).find(
-        ([_, ordinal]) => ordinal === currentOrdinal - 1
-      );
-      if (target) {
-        this._ordinalSubject.next({
-          ...ordinals,
-          [target[0]]: currentOrdinal,
-          [type]: target[1],
-        });
-      }
-    });
+    this._ordinals
+      .get()
+      .pipe(first())
+      .subscribe((ordinals) => {
+        const currentOrdinal = ordinals[type];
+        const target = Object.entries(ordinals).find(
+          ([_, ordinal]) => ordinal === currentOrdinal - 1
+        );
+        if (target) {
+          this._ordinals.update({
+            ...ordinals,
+            [target[0]]: currentOrdinal,
+            [type]: target[1],
+          });
+        }
+      });
   }
 
   constructor(private _localStorageService: LocalStorageService) {}
