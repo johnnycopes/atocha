97e6ea77e14b8abe125c09e4559fcc642be306a8
John Coppola <jdcoppola@gmail.com>
2022-11-11 09:30:42 -0500

    FEATURE: creates and exports ConfigService

---
+++ libs/spirit-islander/data-access/src/lib/config.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+import { State } from '@atocha/core/util';
+import {
+  Config,
+  createAdversariesModel,
+  createBoardsModel,
+  createMapsModel,
+  createScenariosModel,
+  createSpiritsModel,
+} from '@atocha/spirit-islander/util';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigService {
+  _config = new State<Config>({
+    expansions: [],
+    players: 1,
+    difficultyRange: [0, 1],
+    spiritNames: createSpiritsModel(),
+    mapNames: createMapsModel(),
+    boardNames: createBoardsModel(),
+    scenarioNames: createScenariosModel(),
+    adversaryNamesAndIds: createAdversariesModel(),
+  });
+
+  config$ = this._config.get();
+}
