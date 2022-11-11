7357de3952570b985533f79c8c000582a863ae6d
John Coppola <jdcoppola@gmail.com>
2022-11-11 07:45:46 -0500

    FEATURE: adds game models

---
+++ libs/spirit-islander/util/src/lib/game/scenarios.ts
@@ -0,0 +1,22 @@
+import type { DifficultyOption } from './difficulty';
+import type { ExpansionOption } from './expansions';
+
+export type ScenarioName =
+  | 'No Scenario'
+  | 'A Diversity of Spirits'
+  | 'Blitz'
+  | 'Dahan Insurrection'
+  | 'Despicable Theft'
+  | 'Elemental Invocation'
+  | "Guard the Isle's Heart"
+  | 'Powers Long Forgotten'
+  | 'Rituals of Destroying Flame'
+  | 'Rituals of Terror'
+  | 'Second Wave'
+  | 'The Great River'
+  | 'Varied Terrains'
+  | 'Ward the Shores';
+
+export interface Scenario
+  extends DifficultyOption<ScenarioName>,
+    ExpansionOption<ScenarioName> {}
