0f312a45f46fe6c1ebbe54660d07d3d0862b06f2
John Coppola <jdcoppola@gmail.com>
2022-11-12 09:15:59 -0500

    TOOL: generates button.component files

---
+++ libs/spirit-islander/ui/src/lib/button/button.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ui-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
