1aa10342d37da7ce5b44ce90a1dab0cb89b480fe
John Coppola <jdcoppola@gmail.com>
2022-11-12 08:21:43 -0500

    Spirit Islander app primer (#175)
    * FEATURE: configures app.component.ts to be standalone
    
    * FEATURE: deletes NxWelcomeComponent
    
    * FEATURE: sets up global styles.scss
    
    * FEATURE: implements Google Font in index.html
    
    * FEATURE: deletes default favicon.ico and replaces it with emoji
    
    * FEATURE: adds <meta> tag containing description
    
    * FEATURE: sets up app state
    
    * TOOL: runs nx format:write
    
    * OOPS: corrects app name token
    
    * FILE: deletes app.component.spec.ts
    
    * FEATURE: updates e2e test to pass

--- apps/spirit-islander/src/app/app.component.ts
+++ apps/spirit-islander/src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { State } from '@atocha/core/util';
+import {
+  Combo,
+  Config,
+  createAdversariesModel,
+  createBoardsModel,
+  createMapsModel,
+  createScenariosModel,
+  createSpiritsModel,
+  GameSetup,
+  Page,
+} from '@atocha/spirit-islander/util';
+
+interface AppState {
+  page: Page;
+  config: Config;
+  validCombos: Combo[] | undefined;
+  gameSetup: GameSetup | undefined;
+}
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'spirit-islander';
+  _state = new State<AppState>({
+    page: Page.Config,
+    config: {
+      expansions: [],
+      players: 1,
+      difficultyRange: [0, 1],
+      spiritNames: createSpiritsModel(),
+      mapNames: createMapsModel(),
+      boardNames: createBoardsModel(),
+      scenarioNames: createScenariosModel(),
+      adversaryNamesAndIds: createAdversariesModel(),
+    },
+    validCombos: undefined,
+    gameSetup: undefined,
+  });
+
+  vm$ = this._state.get();
 }
