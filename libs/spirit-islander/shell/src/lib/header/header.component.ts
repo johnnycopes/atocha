4cb2ac8fab72cc878deb9ac371cb49cb2f7125e8
John Coppola <jdcoppola@gmail.com>
2022-11-12 08:27:31 -0500

    TOOL: generates header.component

---
+++ libs/spirit-islander/shell/src/lib/header/header.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
