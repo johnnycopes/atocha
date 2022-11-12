5e0ff69bdc0112f9f2fd45468d2cce4d70d78b79
John Coppola <jdcoppola@gmail.com>
2022-11-12 08:39:42 -0500

    FEATURE: sets up header content

--- libs/spirit-islander/shell/src/lib/header/header.component.ts
+++ libs/spirit-islander/shell/src/lib/header/header.component.ts
@@ -1,16 +1,15 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { ExternalLinkDirective } from '@atocha/core/ui';
 
 @Component({
-  selector: 'app-header',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'header[app-header]',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ExternalLinkDirective],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class HeaderComponent {}
