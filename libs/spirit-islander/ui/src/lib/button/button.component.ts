8ea52d30eb8391b695e39517e54d04ee16b72d29
John Coppola <jdcoppola@gmail.com>
2022-11-12 09:22:06 -0500

    FEATURE: button.component contents

--- libs/spirit-islander/ui/src/lib/button/button.component.ts
+++ libs/spirit-islander/ui/src/lib/button/button.component.ts
@@ -1,16 +1,22 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  Input,
+} from '@angular/core';
 
 @Component({
-  selector: 'ui-button',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'button[ui-button]',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ButtonComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ButtonComponent {
+  @Input()
+  @HostBinding('disabled')
+  disabled = false;
 }
