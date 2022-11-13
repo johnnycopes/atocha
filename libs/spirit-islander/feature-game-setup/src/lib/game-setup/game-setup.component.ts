e402d33c8b4765f7bef75c77619c95e12c6c23d7
John Coppola <jdcoppola@gmail.com>
2022-11-13 10:36:20 -0500

    FEATURE: implements card-group in game-setup

--- libs/spirit-islander/feature-game-setup/src/lib/game-setup/game-setup.component.ts
+++ libs/spirit-islander/feature-game-setup/src/lib/game-setup/game-setup.component.ts
@@ -2,13 +2,23 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { PluralPipe } from '@atocha/core/ui';
-import { CardComponent, PageComponent } from '@atocha/spirit-islander/ui';
+import {
+  CardComponent,
+  CardGroupComponent,
+  PageComponent,
+} from '@atocha/spirit-islander/ui';
 import { GameSetup } from '@atocha/spirit-islander/util';
 
 @Component({
   selector: 'app-game-setup',
   standalone: true,
-  imports: [CardComponent, CommonModule, PageComponent, PluralPipe],
+  imports: [
+    CardComponent,
+    CardGroupComponent,
+    CommonModule,
+    PageComponent,
+    PluralPipe,
+  ],
   templateUrl: './game-setup.component.html',
   styleUrls: ['./game-setup.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
