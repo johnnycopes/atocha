e8d1d3a20ad393e99bbf9d639dde82ab26742c75
John Coppola <jdcoppola@gmail.com>
2022-11-20 09:45:29 -0500

    MANUAL: exports snake case pipe from module

--- libs/core/ui/src/index.ts
+++ libs/core/ui/src/index.ts
@@ -9,4 +9,5 @@ export * from './lib/performance/track-by';
 export * from './lib/sanitization/safe.pipe';
 export * from './lib/search-input/search-input.component';
 export * from './lib/strings/plural.pipe';
+export * from './lib/strings/snake-case.pipe';
 export * from './lib/tree/tree.component';
