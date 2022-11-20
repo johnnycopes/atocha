427cfe6fb8e758c5ae53e98eced4d62ae47ac2bf
John Coppola <jdcoppola@gmail.com>
2022-11-20 12:08:16 -0500

    FILE: creates CsvService

---
+++ libs/lorenzo/data-access/src/lib/_state/csv.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+import { DEVELOPMENTS } from './_cards/developments';
+import { FAMILIES } from './_cards/families';
+import { LEADERS } from './_cards/leaders';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CsvService {}
