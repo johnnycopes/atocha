6363cbbcc01927491d01e86c04e719e96b46deeb
John Coppola <jdcoppola@gmail.com>
2022-11-26 08:59:45 -0500

    MANUAL: removes extra comment

--- libs/lorenzo/data-access/src/lib/_state/csv.service.ts
+++ libs/lorenzo/data-access/src/lib/_state/csv.service.ts
@@ -101,8 +101,6 @@ export class CsvService {
     const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
     const link = document.createElement('a');
     if (link.download !== undefined) {
-      // feature detection
-      // Browsers that support HTML5 download attribute
       const url = URL.createObjectURL(blob);
       link.setAttribute('href', url);
       link.setAttribute('download', filename);
