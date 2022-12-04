1c62b844d7295d1764db674957a50da62ff3efa5
John Coppola <jdcoppola@gmail.com>
2022-12-04 16:24:41 -0500

    FEATURE: writes tests for formatCsvStr functionality

--- libs/core/util/src/lib/csv.spec.ts
+++ libs/core/util/src/lib/csv.spec.ts
@@ -1,8 +1,25 @@
 import { Csv } from './csv';
 
 describe('Csv', () => {
-  it('works', () => {
-    const csv = new Csv();
+  let csv: Csv;
+  beforeEach(() => {
+    csv = new Csv({} as Document);
+  });
+
+  it('initializes with Document object passed in', () => {
     expect(csv).toBeDefined();
+  });
+
+  it('formats standard strings in correct syntax', () => {
+    expect(csv.formatCsvStr('this is a sentence')).toBe('"this is a sentence"');
+  });
+
+  it('formats multiline strings in correct syntax', () => {
+    expect(
+      csv.formatCsvStr(`
+        this is a sentence with  large  gaps and
+        excessive whitespace
+      `)
+    ).toBe('"this is a sentence with large gaps and excessive whitespace"');
   });
 });
--- libs/core/util/src/lib/csv.ts
+++ libs/core/util/src/lib/csv.ts
@@ -1,1 +1,30 @@
-export class Csv {}
+import { removeWhitespace } from './remove-whitespace';
+
+export class Csv {
+  constructor(private _document: Document) {
+    if (!_document) {
+      throw new Error(
+        'Document object must be passed to Csv class on initialization'
+      );
+    }
+  }
+
+  generateCsv(filename: string, rows: string[][]): void {
+    const csvContent = rows.map((e) => e.join(',')).join('\n');
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const link = this._document.createElement('a');
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob);
+      link.setAttribute('href', url);
+      link.setAttribute('download', filename);
+      link.style.visibility = 'hidden';
+      this._document.body.appendChild(link);
+      link.click();
+      this._document.body.removeChild(link);
+    }
+  }
+
+  formatCsvStr(str: string): string {
+    return `"${removeWhitespace(str)}"`;
+  }
+}
