e012b7458987313dac2641ffbd8d03c9895bf1a5
John Coppola <jdcoppola@gmail.com>
2022-12-09 07:48:12 -0500

    FEATURE: begins fleshing out contents

--- libs/core/ui/src/lib/button/button.component.stories.ts
+++ libs/core/ui/src/lib/button/button.component.stories.ts
@@ -1,4 +1,11 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import {
+  moduleMetadata,
+  Story,
+  Meta,
+  componentWrapperDecorator,
+} from '@storybook/angular';
+
+import { StorybookWrapperComponent } from '../../../.storybook/storybook-wrapper/storybook-wrapper.component';
 import { ButtonComponent } from './button.component';
 
 export default {
@@ -6,16 +13,39 @@ export default {
   component: ButtonComponent,
   decorators: [
     moduleMetadata({
-      imports: [],
-    })
+      declarations: [StorybookWrapperComponent],
+    }),
+    componentWrapperDecorator(StorybookWrapperComponent),
   ],
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
 } as Meta<ButtonComponent>;
 
 const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
   props: args,
+  template: `
+    <button core-button
+      [disabled]="disabled"
+      (click)="onClick($event)"
+    >
+      {{ slot }}
+    </button>
+  `,
 });
 
+export const base = Template.bind({});
+base.args = createArgs({
+  slot: 'Click me!',
+});
 
-export const Primary = Template.bind({});
-Primary.args = {
+type Args = Partial<ButtonComponent> & {
+  slot?: string;
+  className?: string;
+};
+
+function createArgs(
+  { slot = '', className = '', disabled = false } = {} as Args
+): Args {
+  return { slot, className, disabled };
 }
--- libs/core/ui/src/lib/button/button.component.ts
+++ libs/core/ui/src/lib/button/button.component.ts
@@ -2,16 +2,30 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  HostBinding,
+  Input,
   ViewEncapsulation,
 } from '@angular/core';
 
+export type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
+
 @Component({
   standalone: true,
-  selector: 'core-button',
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: '[core-button]',
   imports: [CommonModule],
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  @HostBinding('disabled')
+  @Input()
+  disabled = false;
+
+  @Input() variant: ButtonVariant = 'primary';
+
+  @HostBinding('class')
+  classes = ['core-button'];
+}
