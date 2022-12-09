1b6a5a7cd2e5f615eb623b6dd0bbfe59b3500702
John Coppola <jdcoppola@gmail.com>
2022-12-09 07:12:22 -0500

    TOOL: generates button component files

--- libs/core/ui/src/index.ts
+++ libs/core/ui/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/animations/animated-component.class';
 export * from './lib/autofocus/autofocus.directive';
+export * from './lib/button/button.component';
 export * from './lib/checkbox/checkbox.component';
 export * from './lib/external-link/external-link.directive';
 export * from './lib/measurement/measurement.pipe';
---
+++ libs/core/ui/src/lib/button/button.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'core-button',
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class ButtonComponent {}
