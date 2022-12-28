aad44746cfb4c7c30b50fe2449151953225649dd
John Coppola <jdcoppola@gmail.com>
2022-12-28 11:39:52 -0500

    FEATURE: simplifies create-model logic

--- libs/spirit-islander/feature-config/src/lib/config/create-model.ts
+++ libs/spirit-islander/feature-config/src/lib/config/create-model.ts
@@ -6,8 +6,6 @@ export function createModel<T>(
   tree: ConfigTree<T>,
   model: string[]
 ): Record<string, CheckboxState> {
-  const state: Record<string, CheckboxState> = {};
-
   const childrenIds = reduceRecursively({
     item: tree,
     getItems: ({ children }) => children ?? [],
@@ -18,25 +16,25 @@ export function createModel<T>(
 
   return Array.from(childrenIds.keys())
     .reverse() // Reverse the keys because we want to ascend the tree starting from the leaf nodes
-    .reduce((accum, curr) => {
+    .reduce((state, curr) => {
       if (model.includes(curr)) {
-        accum[curr] = 'checked';
+        state[curr] = 'checked';
       } else {
         const ids = childrenIds.get(curr) ?? [];
         if (ids.length) {
           const idsInState = ids.reduce(
-            (accum, id) => accum + (state[id] ? 1 : 0),
+            (total, id) => total + (state[id] ? 1 : 0),
             0
           );
           const totalIds = ids.length;
 
           if (totalIds === idsInState) {
-            accum[curr] = 'checked';
+            state[curr] = 'checked';
           } else if (idsInState > 0) {
-            accum[curr] = 'indeterminate';
+            state[curr] = 'indeterminate';
           }
         }
       }
-      return accum;
-    }, state);
+      return state;
+    }, {} as Record<string, CheckboxState>);
 }
