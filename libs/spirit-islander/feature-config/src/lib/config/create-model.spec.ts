dc9ffff8b55c1af0006c4d7a0470de815a54f71f
John Coppola <jdcoppola@gmail.com>
2022-12-28 09:55:17 -0500

    FEATURE: writes tests for shallow trees

--- libs/spirit-islander/feature-config/src/lib/config/create-model.spec.ts
+++ libs/spirit-islander/feature-config/src/lib/config/create-model.spec.ts
@@ -1,14 +1,34 @@
-import { createExpansionsTree } from './create-tree';
+import { createAdversariesTree, createExpansionsTree } from './create-tree';
 import { createTreeModel } from './create-model';
 
 describe('createModel', () => {
-  it('converts empty array model to empty object model', () => {
+  it('converts empty array model with shallow tree', () => {
     expect(createTreeModel(createExpansionsTree(), [])).toEqual({});
   });
 
-  //  array model to object model of checkbox states
-  // expect(createTreeModel(createExpansionsTree(), ['Horizons'])).toEqual({
-  //   Expansions: 'indeterminate',
-  //   Horizons: 'checked',
-  // });
+  it('converts partial array model with shallow tree', () => {
+    expect(createTreeModel(createExpansionsTree(), ['Horizons'])).toEqual({
+      Expansions: 'indeterminate',
+      Horizons: 'checked',
+    });
+  });
+
+  it('converts full array model with shallow tree', () => {
+    expect(
+      createTreeModel(createExpansionsTree(), [
+        'Horizons',
+        'Branch & Claw',
+        'Jagged Earth',
+        'Promo Pack 1',
+        'Promo Pack 2',
+      ])
+    ).toEqual({
+      Expansions: 'checked',
+      Horizons: 'checked',
+      'Branch & Claw': 'checked',
+      'Jagged Earth': 'checked',
+      'Promo Pack 1': 'checked',
+      'Promo Pack 2': 'checked',
+    });
+  });
 });
--- libs/spirit-islander/feature-config/src/lib/config/create-model.ts
+++ libs/spirit-islander/feature-config/src/lib/config/create-model.ts
@@ -1,4 +1,5 @@
 import { CheckboxState } from '@atocha/core/ui';
+import { reduceRecursively } from '@atocha/core/util';
 import { ConfigTree } from './create-tree';
 
 export function createTreeModel<T>(
@@ -6,15 +7,31 @@ export function createTreeModel<T>(
   model: string[]
 ): Record<string, CheckboxState> {
   const state: Record<string, CheckboxState> = {};
+  const childrenKeyedByParentId = reduceRecursively({
+    item: tree,
+    getItems: ({ children }) => children ?? [],
+    initialValue: {} as Record<string, string[]>,
+    reducer: (accum, item) => ({
+      ...accum,
+      [item.id]: item.children?.map(({ id }) => id) ?? [],
+    }),
+  });
 
-  // if (model.length === this._items.length) {
-  //   state[this.name] = 'checked';
-  // } else if (model.length > 0) {
-  //   state[this.name] = 'indeterminate';
-  // }
-
-  return model.reduce((accum, curr) => {
-    accum[curr] = 'checked';
+  return Object.keys(childrenKeyedByParentId).reduce((accum, curr) => {
+    if (model.includes(curr)) {
+      accum[curr] = 'checked';
+    } else if (childrenKeyedByParentId[curr].length > 0) {
+      const childrenInModel = childrenKeyedByParentId[curr].reduce(
+        (accum, child) => accum + (model.includes(child) ? 1 : 0),
+        0
+      );
+      const totalChildren = childrenKeyedByParentId[curr].length;
+      if (totalChildren === childrenInModel) {
+        accum[curr] = 'checked';
+      } else if (childrenInModel > 0) {
+        accum[curr] = 'indeterminate';
+      }
+    }
     return accum;
   }, state);
 }
