acf0f7edab0c3c142d1b9a12bf3094d1fe6b102b
John Coppola <jdcoppola@gmail.com>
2022-12-30 09:03:32 -0500

    FEATURE: writes tests for updateObj

--- libs/core/ui/src/lib/checkbox-tree-new/model-transformer.spec.ts
+++ libs/core/ui/src/lib/checkbox-tree-new/model-transformer.spec.ts
@@ -7,6 +7,7 @@ import {
   ALL_SELECTED_NEW,
   SOME_SELECTED,
   ALL_SELECTED,
+  TestItem,
 } from '../../../.storybook/mock-data/checkbox-tree';
 
 describe('ModelTransformer', () => {
@@ -45,8 +46,70 @@ describe('ModelTransformer', () => {
   });
 
   describe('updateObj', () => {
-    it('exists', () => {
-      expect(transformer.updateObj).toBeTruthy();
+    let item: TestItem = AFRICA;
+
+    beforeEach(() => {
+      item = AFRICA;
+    });
+
+    it('selects all states when the top node is selected', () => {
+      expect(transformer.updateObj(true, item, {})).toEqual({
+        Africa: 'checked',
+        'Central Africa': 'checked',
+        Fes: 'checked',
+        Marrakesh: 'checked',
+        Morocco: 'checked',
+        Namibia: 'checked',
+        'Northern Africa': 'checked',
+        'Southern Africa': 'checked',
+        Swaziland: 'checked',
+      });
+    });
+
+    it('deselects all states when the top node is deselected', () => {
+      expect(transformer.updateObj(false, item, ALL_SELECTED)).toEqual({});
+    });
+
+    it('updates states when middle checkbox is selected', () => {
+      item = AFRICA.children
+        ?.find(({ id }) => id === 'Northern Africa')
+        ?.children?.find(({ id }) => id === 'Morocco') ?? { id: 'Morocco' };
+
+      expect(transformer.updateObj(true, item, {})).toEqual({
+        Africa: 'indeterminate',
+        Fes: 'checked',
+        Marrakesh: 'checked',
+        Morocco: 'checked',
+        'Northern Africa': 'checked',
+      });
+    });
+
+    it('updates states when leaf checkbox is selected', () => {
+      item = AFRICA.children
+        ?.find(({ id }) => id === 'Southern Africa')
+        ?.children?.find(({ id }) => id === 'Namibia') ?? { id: 'Namibia' };
+
+      expect(transformer.updateObj(true, item, {})).toEqual({
+        Africa: 'indeterminate',
+        Namibia: 'checked',
+        'Southern Africa': 'indeterminate',
+      });
+    });
+
+    it('updates indeterminate states to checked when selected', () => {
+      item = AFRICA.children?.find(({ id }) => id === 'Southern Africa') ?? {
+        id: 'Southern Africa',
+      };
+
+      expect(transformer.updateObj(true, item, SOME_SELECTED)).toEqual({
+        Africa: 'indeterminate',
+        Fes: 'checked',
+        Morocco: 'indeterminate',
+        Namibia: 'checked',
+        'Northern Africa': 'indeterminate',
+        'Southern Africa': 'checked',
+        Swaziland: 'checked',
+      });
     });
   });
 });
