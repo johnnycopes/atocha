0056f6b5d10160d0f166ec26188c41ca7b45ebbe
John Coppola <jdcoppola@gmail.com>
2023-01-05 07:24:45 -0500

    fEATURE: deletes obsolete code

--- libs/globetrotter/feature-learn/src/lib/select/select-places/select-places.component.ts
+++ libs/globetrotter/feature-learn/src/lib/select/select-places/select-places.component.ts
@@ -6,12 +6,8 @@ import {
   EventEmitter,
 } from '@angular/core';
 
-import { trackByFactory } from '@atocha/core/ui';
 import {
-  Place,
   PlaceSelection,
-  isSubregion,
-  mapRegionsToPlaceSelection,
   Region,
   mapRegionsToPlaceModel,
 } from '@atocha/globetrotter/util';
@@ -23,13 +19,6 @@ import {
   PlaceTree,
 } from './create-places-tree';
 
-interface RegionState {
-  region: Place;
-  model: string[];
-  selected: number;
-  total: number;
-}
-
 @Component({
   selector: 'app-select-places',
   templateUrl: './select-places.component.html',
@@ -39,61 +28,21 @@ interface RegionState {
 export class SelectPlacesComponent {
   @Input()
   set regions(value: Region[]) {
-    this._allSelectedState = mapRegionsToPlaceSelection(value);
     this._allSelectedModel = mapRegionsToPlaceModel(value);
-    this.regionStates = value.map((region) => ({
-      region,
-      model: [],
-      selected: 0,
-      total: 0,
-    }));
-
     this.tree = createPlaceTree({ root: 'Places', regions: value });
   }
-
-  @Input()
-  set model(value: string[]) {
-    if (value) {
-      console.log(value);
-      this.placesModel = value;
-    }
-  }
-
-  tree: PlaceTree | undefined;
-  placesModel: string[] = [];
-
+  @Input() model: string[] = [];
   @Output() placesChange = new EventEmitter<PlaceSelection>();
   @Output() modelChange = new EventEmitter<string[]>();
 
-  regionStates: RegionState[] = [];
-  overallSelected = 0;
-  overallTotal = 0;
-  readonly trackByFn = trackByFactory<RegionState>(({ region }) => region.name);
-  private _allSelectedState: PlaceSelection = {};
-  private _allSelectedModel: string[] = [];
-
+  tree: PlaceTree | undefined;
   getId = getId;
   getChildren = getChildren;
   getNumberOfCountries = getNumberOfCountries;
+  private _allSelectedModel: string[] = [];
 
   onModelChange(model: string[]): void {
     this.modelChange.emit(model);
-  }
-
-  onSelectedChange(regionState: RegionState, quantity: number): void {
-    regionState.selected = quantity;
-    this.overallSelected = this.regionStates.reduce(
-      (accum, { selected }) => accum + selected,
-      0
-    );
-  }
-
-  onTotalChange(regionState: RegionState, quantity: number): void {
-    regionState.total = quantity;
-    this.overallTotal = this.regionStates.reduce(
-      (accum, { total }) => accum + total,
-      0
-    );
   }
 
   onSelectAll(): void {
