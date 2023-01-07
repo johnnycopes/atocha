1d83da3b69d3c34a799fd8ebba8c2df75c7f29a1
John Coppola <jdcoppola@gmail.com>
2023-01-07 08:15:45 -0500

    FILE: deletes checkbox-tree component files

--- libs/core/ui/src/index.ts
+++ libs/core/ui/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib/animations/animated-component.class';
 export * from './lib/autofocus/autofocus.directive';
 export * from './lib/button/button.component';
 export * from './lib/checkbox/checkbox.component';
-export * from './lib/checkbox-tree/checkbox-tree.component';
 export * from './lib/checkbox-tree-new/checkbox-tree-new.component';
 export * from './lib/counted-checkbox-tree-new/counted-checkbox-tree-new.component';
 export * from './lib/external-link/external-link.directive';
