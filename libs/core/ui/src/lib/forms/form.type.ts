1860d4b9ae2ff14338b4db498f968efc5bac33b5
John Coppola <jdcoppola@gmail.com>
2023-01-12 11:44:31 -0500

    COMMENT: simplifies form type comment

--- libs/core/ui/src/lib/forms/form.type.ts
+++ libs/core/ui/src/lib/forms/form.type.ts
@@ -1,11 +1,9 @@
 import { FormControl } from '@angular/forms';
 
 /*
-  A mapped type that accepts a form model type
-  and returns a new type with every property wrapped
-  in a FormControl. This new type can be passed as a
-  type argument into a FormGroup to specifiy the type
-  of each of its controls.
+  A mapped type that accepts a form model type and returns
+  a new type with every property wrapped in a FormControl.
+  This new type can be passed as a type argument into a FormGroup.
 */
 export type Form<TModel> = Required<{
   [Property in keyof TModel]: FormControl<TModel[Property]>;
