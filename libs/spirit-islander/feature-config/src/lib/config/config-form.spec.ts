3772b9f7f345f2f37af8c1d67b14a4ede376a8cf
John Coppola <jdcoppola@gmail.com>
2023-01-25 15:19:04 -0500

    FEATURE: sets config in beforeEach in tests

--- libs/spirit-islander/feature-config/src/lib/config/config-form.spec.ts
+++ libs/spirit-islander/feature-config/src/lib/config/config-form.spec.ts
@@ -1,42 +1,11 @@
+import { Config } from '@atocha/spirit-islander/util';
 import { ConfigForm } from './config-form';
 
 describe('ConfigForm', () => {
-  it('renders', () => {
-    expect(
-      new ConfigForm({
-        expansions: ['Branch & Claw'],
-        players: 2,
-        difficultyRange: [0, 3],
-        spiritNames: [
-          'A Spread of Rampant Green',
-          'Thunderspeaker',
-          'Keeper of the Forbidden Wilds',
-        ],
-        mapNames: ['Balanced'],
-        boardNames: ['A', 'C', 'D'],
-        scenarioNames: ['No Scenario', 'Blitz'],
-        adversaryNamesAndIds: ['No Adversary', 'bp-0', 'bp-1', 'bp-2'],
-      })
-    ).toBeTruthy();
-  });
+  let config: Config;
 
-  it('has value that matches the passed-in model', () => {
-    expect(
-      new ConfigForm({
-        expansions: ['Branch & Claw'],
-        players: 2,
-        difficultyRange: [0, 3],
-        spiritNames: [
-          'A Spread of Rampant Green',
-          'Thunderspeaker',
-          'Keeper of the Forbidden Wilds',
-        ],
-        mapNames: ['Balanced'],
-        boardNames: ['A', 'C', 'D'],
-        scenarioNames: ['No Scenario', 'Blitz'],
-        adversaryNamesAndIds: ['No Adversary', 'bp-0', 'bp-1', 'bp-2'],
-      }).value
-    ).toStrictEqual({
+  beforeEach(() => {
+    config = {
       expansions: ['Branch & Claw'],
       players: 2,
       difficultyRange: [0, 3],
@@ -49,6 +18,14 @@ describe('ConfigForm', () => {
       boardNames: ['A', 'C', 'D'],
       scenarioNames: ['No Scenario', 'Blitz'],
       adversaryNamesAndIds: ['No Adversary', 'bp-0', 'bp-1', 'bp-2'],
-    });
+    };
+  });
+
+  it('renders', () => {
+    expect(new ConfigForm(config)).toBeTruthy();
+  });
+
+  it('has value that matches the passed-in model', () => {
+    expect(new ConfigForm(config).value).toStrictEqual(config);
   });
 });
