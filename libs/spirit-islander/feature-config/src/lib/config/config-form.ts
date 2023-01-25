f175dcaa5734c88a9149cbe8be0979ea7c20b43b
John Coppola <jdcoppola@gmail.com>
2023-01-25 15:34:03 -0500

    FEATURE: adds validators to config-form

--- libs/spirit-islander/feature-config/src/lib/config/config-form.ts
+++ libs/spirit-islander/feature-config/src/lib/config/config-form.ts
@@ -2,7 +2,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 
 import { Form } from '@atocha/core/ui';
 import { Config } from '@atocha/spirit-islander/util';
-import { required } from './validators';
+import {
+  invalidDifficultyRange,
+  playersOutnumberSelectedBoards,
+  playersOutnumberSpirits,
+  playersOutnumberTotalBoards,
+  required,
+} from './validators';
 
 export class ConfigForm extends FormGroup<Form<Config>> {
   constructor(
@@ -10,19 +16,29 @@ export class ConfigForm extends FormGroup<Form<Config>> {
     readonly fb: FormBuilder = new FormBuilder()
   ) {
     super(
-      fb.nonNullable.group({
-        expansions: fb.nonNullable.control(model.expansions),
-        players: fb.nonNullable.control(model.players),
-        difficultyRange: fb.nonNullable.control(model.difficultyRange),
-        spiritNames: fb.nonNullable.control(model.spiritNames),
-        mapNames: fb.nonNullable.control(model.mapNames, required),
-        boardNames: fb.nonNullable.control(model.boardNames),
-        scenarioNames: fb.nonNullable.control(model.scenarioNames, required),
-        adversaryNamesAndIds: fb.nonNullable.control(
-          model.adversaryNamesAndIds,
-          required
-        ),
-      }).controls
+      fb.nonNullable.group(
+        {
+          expansions: fb.nonNullable.control(model.expansions),
+          players: fb.nonNullable.control(model.players),
+          difficultyRange: fb.nonNullable.control(model.difficultyRange),
+          spiritNames: fb.nonNullable.control(model.spiritNames),
+          mapNames: fb.nonNullable.control(model.mapNames, required),
+          boardNames: fb.nonNullable.control(model.boardNames),
+          scenarioNames: fb.nonNullable.control(model.scenarioNames, required),
+          adversaryNamesAndIds: fb.nonNullable.control(
+            model.adversaryNamesAndIds,
+            required
+          ),
+        },
+        {
+          validators: [
+            playersOutnumberSpirits,
+            playersOutnumberTotalBoards,
+            playersOutnumberSelectedBoards,
+            invalidDifficultyRange,
+          ],
+        }
+      ).controls
     );
   }
 }
