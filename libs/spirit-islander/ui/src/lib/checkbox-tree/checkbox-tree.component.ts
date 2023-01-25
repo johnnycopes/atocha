d493e3a5a2492b569766c76408b020442d9694fe
John Coppola <jdcoppola@gmail.com>
2023-01-25 07:14:44 -0500

    FEATURE: fleshes out component contents

--- libs/spirit-islander/ui/src/lib/checkbox-tree/checkbox-tree.component.ts
+++ libs/spirit-islander/ui/src/lib/checkbox-tree/checkbox-tree.component.ts
@@ -1,18 +1,33 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { CheckboxTreeComponent as CoreCheckboxTreeComponent } from '@atocha/core/ui';
+import {
+  CheckboxComponent,
+  CheckboxTreeComponent as CoreCheckboxTreeComponent,
+} from '@atocha/core/ui';
 import { ConfigTree } from './create-tree';
+import { DifficultyEmblemComponent } from '../difficulty-emblem/difficulty-emblem.component';
+import { ExpansionEmblemComponent } from '../expansion-emblem/expansion-emblem.component';
 
 @Component({
   selector: 'ui-checkbox-tree',
   standalone: true,
-  imports: [CoreCheckboxTreeComponent, CommonModule],
+  imports: [
+    CheckboxComponent,
+    CoreCheckboxTreeComponent,
+    CommonModule,
+    DifficultyEmblemComponent,
+    ExpansionEmblemComponent,
+    FormsModule,
+  ],
   templateUrl: './checkbox-tree.component.html',
   styleUrls: ['./checkbox-tree.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxTreeComponent {
+export class CheckboxTreeComponent<T> {
+  @Input() tree: ConfigTree<T> | undefined;
+
   getId = <T>({ id }: ConfigTree<T>) => id;
   getChildren = <T>({ children }: ConfigTree<T>) => children ?? [];
 }
