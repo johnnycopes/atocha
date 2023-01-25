383ad4d8dc2e8ff032dec77cbd4e41f015d9f211
John Coppola <jdcoppola@gmail.com>
2023-01-25 16:17:52 -0500

    FEATURE: moves validators to second argument of FormGroup in ConfigForm class

--- libs/spirit-islander/feature-config/src/lib/config/config-form.ts
+++ libs/spirit-islander/feature-config/src/lib/config/config-form.ts
@@ -16,29 +16,27 @@ export class ConfigForm extends FormGroup<Form<Config>> {
     readonly fb: FormBuilder = new FormBuilder()
   ) {
     super(
-      fb.nonNullable.group(
-        {
-          expansions: fb.nonNullable.control(model.expansions),
-          players: fb.nonNullable.control(model.players),
-          difficultyRange: fb.nonNullable.control(model.difficultyRange),
-          spiritNames: fb.nonNullable.control(model.spiritNames),
-          mapNames: fb.nonNullable.control(model.mapNames, required),
-          boardNames: fb.nonNullable.control(model.boardNames),
-          scenarioNames: fb.nonNullable.control(model.scenarioNames, required),
-          adversaryNamesAndIds: fb.nonNullable.control(
-            model.adversaryNamesAndIds,
-            required
-          ),
-        },
-        {
-          validators: [
-            playersOutnumberSpirits,
-            playersOutnumberTotalBoards,
-            playersOutnumberSelectedBoards,
-            invalidDifficultyRange,
-          ],
-        }
-      ).controls
+      fb.nonNullable.group({
+        expansions: fb.nonNullable.control(model.expansions),
+        players: fb.nonNullable.control(model.players),
+        difficultyRange: fb.nonNullable.control(model.difficultyRange),
+        spiritNames: fb.nonNullable.control(model.spiritNames),
+        mapNames: fb.nonNullable.control(model.mapNames, required),
+        boardNames: fb.nonNullable.control(model.boardNames),
+        scenarioNames: fb.nonNullable.control(model.scenarioNames, required),
+        adversaryNamesAndIds: fb.nonNullable.control(
+          model.adversaryNamesAndIds,
+          required
+        ),
+      }).controls,
+      {
+        validators: [
+          playersOutnumberSpirits,
+          playersOutnumberTotalBoards,
+          playersOutnumberSelectedBoards,
+          invalidDifficultyRange,
+        ],
+      }
     );
   }
 }
