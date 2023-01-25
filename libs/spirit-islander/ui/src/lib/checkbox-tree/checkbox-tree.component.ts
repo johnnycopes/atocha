d47204e6489ede073b9a30e67bff7eeb8adb15f0
John Coppola <jdcoppola@gmail.com>
2023-01-25 07:43:47 -0500

    FEATURE: adds nodeClick Output to component

--- libs/spirit-islander/ui/src/lib/checkbox-tree/checkbox-tree.component.ts
+++ libs/spirit-islander/ui/src/lib/checkbox-tree/checkbox-tree.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -30,6 +36,7 @@ export class CheckboxTreeComponent<T> {
   @Input() tree: ConfigTree<T> | undefined;
   @Input() form: FormGroup | undefined;
   @Input() controlName = '';
+  @Output() nodeClick = new EventEmitter<string>();
 
   getId = <T>({ id }: ConfigTree<T>) => id;
   getChildren = <T>({ children }: ConfigTree<T>) => children ?? [];
