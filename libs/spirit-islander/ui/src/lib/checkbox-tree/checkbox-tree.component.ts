0dcd378610d7b02c47ca41ed263d146bd5a910b6
John Coppola <jdcoppola@gmail.com>
2023-01-25 07:11:20 -0500

    FEATURE: imports core-ui CheckboxTreeComponent and defines getId and getChildren methods

--- libs/spirit-islander/ui/src/lib/checkbox-tree/checkbox-tree.component.ts
+++ libs/spirit-islander/ui/src/lib/checkbox-tree/checkbox-tree.component.ts
@@ -1,12 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CheckboxTreeComponent as CoreCheckboxTreeComponent } from '@atocha/core/ui';
+import { ConfigTree } from './create-tree';
+
 @Component({
   selector: 'ui-checkbox-tree',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CoreCheckboxTreeComponent, CommonModule],
   templateUrl: './checkbox-tree.component.html',
   styleUrls: ['./checkbox-tree.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxTreeComponent {}
+export class CheckboxTreeComponent {
+  getId = <T>({ id }: ConfigTree<T>) => id;
+  getChildren = <T>({ children }: ConfigTree<T>) => children ?? [];
+}
