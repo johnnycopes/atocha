8f0bd3fd12134d9764f99618c1d12d92a4875409
John Coppola <jdcoppola@gmail.com>
2023-01-26 09:33:16 -0500

    FEATURE: writes expansions changes tests that only examine affected models

--- libs/spirit-islander/feature-config/src/lib/config/config-form.spec.ts
+++ libs/spirit-islander/feature-config/src/lib/config/config-form.spec.ts
@@ -113,21 +113,31 @@ describe('ConfigForm', () => {
     });
   });
 
-  describe('form models change', () => {
+  describe('changes to expansions affect models', () => {
     it('when an expansion is clicked once', () => {
       const form = new ConfigForm(config);
       form.updateModels([], 'Branch & Claw');
 
-      expect(form.value).toEqual({
-        expansions: ['Branch & Claw'],
-        players: 2,
-        difficultyRange: [0, 3],
-        spiritNames: ['A Spread of Rampant Green', 'Thunderspeaker'],
-        mapNames: ['Balanced'],
-        boardNames: ['A', 'C', 'D'],
-        scenarioNames: ['No Scenario', 'Blitz'],
-        adversaryNamesAndIds: ['No Adversary', 'bp-0', 'bp-1', 'bp-2'],
-      });
+      const {
+        spiritNames,
+        mapNames,
+        boardNames,
+        scenarioNames,
+        adversaryNamesAndIds,
+      } = form.value;
+      expect(spiritNames).toEqual([
+        'A Spread of Rampant Green',
+        'Thunderspeaker',
+      ]);
+      expect(mapNames).toEqual(['Balanced']);
+      expect(boardNames).toEqual(['A', 'C', 'D']);
+      expect(scenarioNames).toEqual(['No Scenario', 'Blitz']);
+      expect(adversaryNamesAndIds).toEqual([
+        'No Adversary',
+        'bp-0',
+        'bp-1',
+        'bp-2',
+      ]);
     });
 
     it('when an expansion is clicked twice', () => {
@@ -135,41 +145,179 @@ describe('ConfigForm', () => {
       form.updateModels([], 'Branch & Claw');
       form.updateModels(['Branch & Claw'], 'Branch & Claw');
 
-      expect(form.value).toEqual({
-        expansions: ['Branch & Claw'],
-        players: 2,
-        difficultyRange: [0, 3],
-        spiritNames: [
-          'A Spread of Rampant Green',
-          'Thunderspeaker',
-          'Keeper of the Forbidden Wilds',
-          'Sharp Fangs Behind the Leaves',
+      const {
+        spiritNames,
+        mapNames,
+        boardNames,
+        scenarioNames,
+        adversaryNamesAndIds,
+      } = form.value;
+      expect(spiritNames).toEqual([
+        'A Spread of Rampant Green',
+        'Thunderspeaker',
+        'Keeper of the Forbidden Wilds',
+        'Sharp Fangs Behind the Leaves',
+      ]);
+      expect(mapNames).toEqual(['Balanced']);
+      expect(boardNames).toEqual(['A', 'C', 'D']);
+      expect(scenarioNames).toEqual([
+        'No Scenario',
+        'Blitz',
+        'Second Wave',
+        'Powers Long Forgotten',
+        'Ward the Shores',
+        'Rituals of Destroying Flame',
+      ]);
+      expect(adversaryNamesAndIds).toEqual([
+        'No Adversary',
+        'bp-0',
+        'bp-1',
+        'bp-2',
+        'France',
+        'fr-0',
+        'fr-1',
+        'fr-2',
+        'fr-3',
+        'fr-4',
+        'fr-5',
+        'fr-6',
+      ]);
+    });
+
+    it('when "Expansions" is clicked once', () => {
+      const form = new ConfigForm(config);
+      form.updateModels(
+        [
+          'Branch & Claw',
+          'Horizons',
+          'Jagged Earth',
+          'Promo Pack 1',
+          'Promo Pack 2',
         ],
-        mapNames: ['Balanced'],
-        boardNames: ['A', 'C', 'D'],
-        scenarioNames: [
-          'No Scenario',
-          'Blitz',
-          'Second Wave',
-          'Powers Long Forgotten',
-          'Ward the Shores',
-          'Rituals of Destroying Flame',
+        'Expansions'
+      );
+
+      const {
+        spiritNames,
+        mapNames,
+        boardNames,
+        scenarioNames,
+        adversaryNamesAndIds,
+      } = form.value;
+      expect(spiritNames).toEqual([
+        'A Spread of Rampant Green',
+        'Thunderspeaker',
+        'Keeper of the Forbidden Wilds',
+        'Devouring Teeth Lurk Underfoot',
+        'Downpour Drenches the World',
+        'Eyes Watch From the Trees',
+        'Fathomless Mud of the Swamp',
+        'Finder of Paths Unseen',
+        'Fractured Days Split the Sky',
+        'Grinning Trickster Stirs Up Trouble',
+        'Heart of the Wildfire',
+        'Lure of the Deep Wilderness',
+        'Many Minds Move as One',
+        'Rising Heat of Stone and Sand',
+        'Serpent Slumbering Beneath the Island',
+        'Sharp Fangs Behind the Leaves',
+        'Shifting Memory of Ages',
+        'Shroud of Silent Mist',
+        'Sun-Bright Whirlwind',
+        'Starlight Seeks Its Form',
+        "Stone's Unyielding Defiance",
+        'Vengeance as a Burning Plague',
+        'Volcano Looming High',
+      ]);
+      expect(mapNames).toEqual(['Balanced']);
+      expect(boardNames).toEqual(['A', 'C', 'D', 'E', 'F']);
+      expect(scenarioNames).toEqual([
+        'No Scenario',
+        'Blitz',
+        'Second Wave',
+        'A Diversity of Spirits',
+        'Powers Long Forgotten',
+        'Elemental Invocation',
+        'Varied Terrains',
+        'Despicable Theft',
+        'Ward the Shores',
+        'Rituals of Destroying Flame',
+        'The Great River',
+      ]);
+      expect(adversaryNamesAndIds).toEqual([
+        'No Adversary',
+        'bp-0',
+        'bp-1',
+        'bp-2',
+        'France',
+        'fr-0',
+        'fr-1',
+        'fr-2',
+        'fr-3',
+        'fr-4',
+        'fr-5',
+        'fr-6',
+        'Habsburg Monarchy',
+        'hm-0',
+        'hm-1',
+        'hm-2',
+        'hm-3',
+        'hm-4',
+        'hm-5',
+        'hm-6',
+        'Russia',
+        'ru-0',
+        'ru-1',
+        'ru-2',
+        'ru-3',
+        'ru-4',
+        'ru-5',
+        'ru-6',
+        'Scotland',
+        'sc-0',
+        'sc-1',
+        'sc-2',
+        'sc-3',
+        'sc-4',
+        'sc-5',
+        'sc-6',
+      ]);
+    });
+
+    it('when "Expansions" is clicked twice', () => {
+      const form = new ConfigForm(config);
+      form.updateModels(
+        [
+          'Branch & Claw',
+          'Horizons',
+          'Jagged Earth',
+          'Promo Pack 1',
+          'Promo Pack 2',
         ],
-        adversaryNamesAndIds: [
-          'No Adversary',
-          'bp-0',
-          'bp-1',
-          'bp-2',
-          'France',
-          'fr-0',
-          'fr-1',
-          'fr-2',
-          'fr-3',
-          'fr-4',
-          'fr-5',
-          'fr-6',
-        ],
-      });
+        'Expansions'
+      );
+      form.updateModels([], 'Expansions');
+
+      const {
+        spiritNames,
+        mapNames,
+        boardNames,
+        scenarioNames,
+        adversaryNamesAndIds,
+      } = form.value;
+      expect(spiritNames).toEqual([
+        'A Spread of Rampant Green',
+        'Thunderspeaker',
+      ]);
+      expect(mapNames).toEqual(['Balanced']);
+      expect(boardNames).toEqual(['A', 'C', 'D']);
+      expect(scenarioNames).toEqual(['No Scenario', 'Blitz']);
+      expect(adversaryNamesAndIds).toEqual([
+        'No Adversary',
+        'bp-0',
+        'bp-1',
+        'bp-2',
+      ]);
     });
   });
 });
