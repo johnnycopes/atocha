39c1a20c295bf0839e35c7788c2afd8253fe5a71
John Coppola <jdcoppola@gmail.com>
2023-01-29 20:04:55 -0500

    FEATURE: gets select-range component working

--- libs/spirit-islander/ui/src/lib/select-range/select-range.component.ts
+++ libs/spirit-islander/ui/src/lib/select-range/select-range.component.ts
@@ -1,12 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormsModule,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+type Range = [number, number];
 
 @Component({
   selector: 'ui-select-range',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './select-range.component.html',
   styleUrls: ['./select-range.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: SelectRangeComponent,
+      multi: true,
+    },
+  ],
 })
-export class SelectRangeComponent {}
+export class SelectRangeComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() options: number[] = [];
+  range: Range = [0, 0];
+
+  writeValue(value: Range): void {
+    this.range = value;
+  }
+
+  registerOnChange(fn: (value: Range) => void): void {
+    fn;
+  }
+
+  registerOnTouched(fn: (value: Range) => void): void {
+    fn;
+  }
+}
