d46f44fc2933580e55fbe53e9e33cc6d2fbe89f6
John Coppola <jdcoppola@gmail.com>
2023-01-29 21:29:26 -0500

    Revert "TOOL: generates select-one component files"
    This reverts commit 51c1e4d5f7c154c321210321791e3b6be4445498.

--- libs/spirit-islander/ui/src/index.ts
+++ libs/spirit-islander/ui/src/index.ts
@@ -6,6 +6,5 @@ export * from './lib/difficulty-emblem/difficulty-emblem.component';
 export * from './lib/expansion-emblem/expansion-emblem.component';
 export * from './lib/card-group/card-group.component';
 export * from './lib/page/page.component';
-export * from './lib/select-one/select-one.component';
 export * from './lib/select-range/select-range.component';
 export * from './lib/separator/separator.component';
