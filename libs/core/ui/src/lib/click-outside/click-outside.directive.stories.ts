92d14d7f8a12342431afa8895a9b550646d00ffb
John Coppola <jdcoppola@gmail.com>
2023-02-02 20:11:23 -0500

    FILE: writes .stories file

---
+++ libs/core/ui/src/lib/click-outside/click-outside.directive.stories.ts
@@ -0,0 +1,38 @@
+import {
+  moduleMetadata,
+  Story,
+  Meta,
+  componentWrapperDecorator,
+} from '@storybook/angular';
+
+import { ClickOutsideDirective } from './click-outside.directive';
+import { StorybookWrapperComponent } from '../../../.storybook/storybook-wrapper/storybook-wrapper.component';
+
+export default {
+  title: 'Click Outside',
+  decorators: [
+    moduleMetadata({
+      imports: [ClickOutsideDirective],
+      declarations: [StorybookWrapperComponent],
+    }),
+    componentWrapperDecorator(StorybookWrapperComponent),
+  ],
+  argTypes: {
+    inside: { action: 'INSIDE' },
+    outside: { action: 'OUTSIDE' },
+  },
+} as Meta<ClickOutsideDirective>;
+
+export const base: Story<Args> = (args: Args) => ({
+  props: args,
+  template: `
+    <p style="background: goldenrod; padding: 16px;"
+      (click)="inside($event)"
+      (coreClickOutside)="outside($event)"
+    >
+      Click inside or outside of me to see different actions fire
+    </p>
+  `,
+});
+
+type Args = Partial<ClickOutsideDirective>;
