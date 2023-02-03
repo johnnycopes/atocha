3beaced8c74d2a8ca9ac28da2502a9d76f5cae9d
John Coppola <jdcoppola@gmail.com>
2023-02-02 21:53:50 -0500

    Gets/sets config object in LocalStorage (#273)
    * FEATURE: moves state logic into ConfigService
    
    * FILE: renames config.service.ts to app-state.service.ts
    
    * TOOL: renames ConfigService token to AppStateService
    
    * FEATURE: gets local storage get/set working
    
    * OOPS: wrong default maps value

---
+++ libs/spirit-islander/data-access/src/lib/app-state.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { first, tap } from 'rxjs';
+
+import { LocalStorageService } from '@atocha/core/data-access';
+import { State } from '@atocha/core/util';
+import {
+  Combo,
+  Config,
+  createAdversariesModel,
+  createBoardsModel,
+  createGameSetup,
+  createMapsModel,
+  createScenariosModel,
+  createSpiritsModel,
+  EXPANSIONS,
+  GameSetup,
+  getValidCombos,
+  Page,
+} from '@atocha/spirit-islander/util';
+
+interface AppState {
+  page: Page;
+  config: Config;
+  validCombos: Combo[] | undefined;
+  gameSetup: GameSetup | undefined;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppStateService {
+  private readonly _key = 'CONFIG';
+  private _config: Config = this._getConfig();
+  private _state = new State<AppState>({
+    page: Page.Config,
+    config: this._config,
+    validCombos: undefined,
+    gameSetup: createGameSetup(this._config, getValidCombos(this._config)),
+  });
+
+  state$ = this._state.get().pipe(tap(({ config }) => this._setConfig(config)));
+
+  constructor(private _localStorageService: LocalStorageService) {}
+
+  edit(): void {
+    this._state.updateProp('page', Page.Config);
+  }
+
+  generate(config: Config, validCombos: Combo[]): void {
+    this._state.updateProp('config', config);
+    this._state.updateProp('validCombos', validCombos);
+    this._state.updateProp('gameSetup', createGameSetup(config, validCombos));
+    this._state.updateProp('page', Page.GameSetup);
+  }
+
+  regenerate(): void {
+    this._state
+      .get()
+      .pipe(first())
+      .subscribe(({ config, validCombos }) =>
+        this._state.updateProp(
+          'gameSetup',
+          createGameSetup(config, validCombos ?? [])
+        )
+      );
+  }
+
+  private _getConfig(): Config {
+    const config = this._localStorageService.getItem(this._key);
+    const expansions = EXPANSIONS.slice();
+    return config
+      ? JSON.parse(config)
+      : {
+          expansions: expansions,
+          players: 5,
+          difficultyRange: [0, 8],
+          spiritNames: createSpiritsModel(expansions),
+          mapNames: createMapsModel(expansions),
+          boardNames: createBoardsModel(expansions),
+          scenarioNames: createScenariosModel(expansions),
+          adversaryNamesAndIds: createAdversariesModel(expansions),
+        };
+  }
+
+  private _setConfig(config: Config): void {
+    this._localStorageService.setItem(this._key, JSON.stringify(config));
+  }
+}
