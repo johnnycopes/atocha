bd409f2b8336ce8d07798338b43401c6917baa50
John Coppola <jdcoppola@gmail.com>
2023-03-29 18:20:30 -0400

    FEATURE: fleshes out page-not-found component

--- libs/spirit-islander/feature-shell/src/lib/page-not-found/page-not-found.component.ts
+++ libs/spirit-islander/feature-shell/src/lib/page-not-found/page-not-found.component.ts
@@ -1,13 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonComponent } from '@atocha/core/ui';
 import { PageComponent } from '@atocha/spirit-islander/ui';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [CommonModule, PageComponent],
+  imports: [ButtonComponent, CommonModule, PageComponent],
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  constructor(private _router: Router) {}
+
+  onClick(): void {
+    this._router.navigate(['']);
+  }
+}
