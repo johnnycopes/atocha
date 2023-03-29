fcf75dad64072e65b090894d0a3418e882fc38ae
John Coppola <jdcoppola@gmail.com>
2023-03-29 17:42:54 -0400

    FILE: scaffolds error component

--- libs/spirit-islander/feature-shell/src/index.ts
+++ libs/spirit-islander/feature-shell/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/error/error.component';
 export * from './lib/header/header.component';
 export * from './lib/footer/footer.component';
 export * from './lib/page-not-found/page-not-found.component';
---
+++ libs/spirit-islander/feature-shell/src/lib/error/error.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-error',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ErrorComponent {}
