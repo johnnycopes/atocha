72fb9691fb1faf0f25c70bc5f74fd6c83a85abb5
John Coppola <jdcoppola@gmail.com>
2023-03-30 08:26:43 -0400

    FILE: creates AppRoutingService

---
+++ libs/spirit-islander/data-access/src/lib/app-routing.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Config, Route } from '@atocha/spirit-islander/util';
+import { mapConfigToQueryParams } from './routing';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppRoutingService {
+  constructor(private _router: Router) {}
+
+  async navigateToHome(): Promise<void> {
+    await this._router.navigate([Route.Home]);
+  }
+
+  async navigateToGameSetup(config: Config): Promise<void> {
+    await this._router.navigate([Route.GameSetup], {
+      queryParams: mapConfigToQueryParams(config),
+    });
+  }
+
+  async navigateToConfig(): Promise<void> {
+    await this._router.navigate([Route.Config]);
+  }
+
+  async navigateToError(): Promise<void> {
+    await this._router.navigate([Route.Error], {
+      skipLocationChange: true,
+      queryParamsHandling: 'preserve',
+    });
+  }
+}
