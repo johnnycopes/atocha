409fec6972ccdb3edcddbe04578f901b6d42b546
John Coppola <jdcoppola@gmail.com>
2023-04-04 17:56:35 -0400

    FILE: writes mappers.spec.ts

---
+++ libs/spirit-islander/data-access/src/lib/mappers.spec.ts
@@ -0,0 +1,71 @@
+import { convertToParamMap } from '@angular/router';
+import {
+  mapConfigToQueryParams as mapConfigToParamMap,
+  mapParamMapToConfig,
+} from './mappers';
+
+describe('mappers', () => {
+  describe('mapConfigToQueryParams', () => {
+    it('returns a ParamMap', () => {
+      expect(
+        mapConfigToParamMap({
+          expansions: ['Branch & Claw'],
+          players: 2,
+          difficultyRange: [0, 3],
+          spiritNames: [
+            'A Spread of Rampant Green',
+            'Thunderspeaker',
+            'Keeper of the Forbidden Wilds',
+          ],
+          mapNames: ['Balanced'],
+          boardNames: ['A', 'C', 'D'],
+          scenarioNames: ['No Scenario', 'Blitz'],
+          adversaryLevelIds: ['none', 'bp-0', 'bp-1', 'bp-2'],
+        })
+      ).toEqual({
+        expansions: '["Branch & Claw"]',
+        players: '2',
+        difficultyRange: '[0,3]',
+        spiritNames:
+          '["A Spread of Rampant Green","Thunderspeaker","Keeper of the Forbidden Wilds"]',
+        boardNames: '["A","C","D"]',
+        mapNames: '["Balanced"]',
+        scenarioNames: '["No Scenario","Blitz"]',
+        adversaryLevelIds: '["none","bp-0","bp-1","bp-2"]',
+      });
+    });
+  });
+
+  describe('mapParamMapToConfig', () => {
+    it('returns a Config object', () => {
+      expect(
+        mapParamMapToConfig(
+          convertToParamMap({
+            expansions: '["Branch & Claw"]',
+            players: '2',
+            difficultyRange: '[0,3]',
+            spiritNames:
+              '["A Spread of Rampant Green","Thunderspeaker","Keeper of the Forbidden Wilds"]',
+            boardNames: '["A","C","D"]',
+            mapNames: '["Balanced"]',
+            scenarioNames: '["No Scenario","Blitz"]',
+            adversaryLevelIds: '["none","bp-0","bp-1","bp-2"]',
+          })
+        )
+      ).toEqual({
+        expansions: ['Branch & Claw'],
+        players: 2,
+        difficultyRange: [0, 3],
+        spiritNames: [
+          'A Spread of Rampant Green',
+          'Thunderspeaker',
+          'Keeper of the Forbidden Wilds',
+        ],
+        mapNames: ['Balanced'],
+        boardNames: ['A', 'C', 'D'],
+        scenarioNames: ['No Scenario', 'Blitz'],
+        adversaryLevelIds: ['none', 'bp-0', 'bp-1', 'bp-2'],
+      });
+    });
+  });
+});
