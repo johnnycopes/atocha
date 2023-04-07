5c53e584a4eb7ee5b7a38b95a85096a84237d0cb
John Coppola <jdcoppola@gmail.com>
2023-04-07 16:40:31 -0400

    FEATURE: removes unused routerAnimation from app.component

--- apps/spirit-islander/src/app/app.component.ts
+++ apps/spirit-islander/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { routerTransition } from '@atocha/core/ui';
-
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [RouterModule],
   template: `<router-outlet></router-outlet>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [routerTransition],
 })
 export class AppComponent {}
