2176391098c62352cf1e5ea494bf9537817c8839
John Coppola <jdcoppola@gmail.com>
2023-04-06 23:15:36 -0400

    FEATURE: sets up animations in app.component

--- apps/spirit-islander/src/app/app.component.ts
+++ apps/spirit-islander/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 import { ConfigComponent } from '@atocha/spirit-islander/feature-config';
 import { GameSetupComponent } from '@atocha/spirit-islander/feature-game-setup';
@@ -9,12 +8,12 @@ import {
   HeaderComponent,
 } from '@atocha/spirit-islander/feature-shell';
 import { PageComponent } from '@atocha/spirit-islander/ui';
+import { routerTransition } from './router-transition';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   imports: [
-    CommonModule,
     ConfigComponent,
     FooterComponent,
     GameSetupComponent,
@@ -25,5 +24,10 @@ import { PageComponent } from '@atocha/spirit-islander/ui';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [routerTransition],
 })
-export class AppComponent {}
+export class AppComponent {
+  getState(outlet: RouterOutlet): string | undefined {
+    return outlet.activatedRouteData['state'];
+  }
+}
