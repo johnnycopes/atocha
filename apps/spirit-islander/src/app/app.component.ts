e54841f4845f60f751013fc3017e9df277fb1d12
John Coppola <jdcoppola@gmail.com>
2023-04-07 15:56:31 -0400

    FEATURE: moves app.component logic to shell.component and updates routes

--- apps/spirit-islander/src/app/app.component.ts
+++ apps/spirit-islander/src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-
-import { routerTransition } from '@atocha/core/ui';
-import {
-  FooterComponent,
-  HeaderComponent,
-} from '@atocha/spirit-islander/feature-shell';
-import { PageComponent } from '@atocha/spirit-islander/ui';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [FooterComponent, HeaderComponent, PageComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [routerTransition],
 })
-export class AppComponent {
-  getState(outlet: RouterOutlet): string | undefined {
-    return outlet.activatedRouteData['state'];
-  }
-}
+export class AppComponent {}
--- libs/spirit-islander/feature-shell/src/lib/shell/shell.component.ts
+++ libs/spirit-islander/feature-shell/src/lib/shell/shell.component.ts
@@ -1,11 +1,22 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+import { routerTransition } from '@atocha/core/ui';
+import { PageComponent } from '@atocha/spirit-islander/ui';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   standalone: true,
   selector: 'app-shell',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, PageComponent, RouterModule],
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [routerTransition],
 })
-export class NameComponent {}
+export class ShellComponent {
+  getState(outlet: RouterOutlet): string | undefined {
+    return outlet.activatedRouteData['state'];
+  }
+}
