0f19b4cca6abf2bcf4a44fa1b4f6474e8c752254
John Coppola <jdcoppola@gmail.com>
2023-04-07 15:58:20 -0400

    FEATURE: adds routerTransition to app.component again

--- apps/spirit-islander/src/app/app.component.ts
+++ apps/spirit-islander/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { routerTransition } from '@atocha/core/ui';
+
 @Component({
   standalone: true,
   selector: 'app-root',
@@ -8,5 +10,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [routerTransition],
 })
 export class AppComponent {}
