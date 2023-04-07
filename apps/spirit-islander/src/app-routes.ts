45eccb5585dc8cd7afefffdab49bbbdee81643e1
John Coppola <jdcoppola@gmail.com>
2023-04-06 23:14:51 -0400

    FEATURE: adds data props to all SI app-routes

--- apps/spirit-islander/src/app-routes.ts
+++ apps/spirit-islander/src/app-routes.ts
@@ -5,6 +5,7 @@ export const APP_ROUTES: Route[] = [
   {
     path: AppRoute.Config,
     title: 'Config | Spirit Islander',
+    data: { state: AppRoute.Config },
     loadComponent: () =>
       import('@atocha/spirit-islander/feature-config').then(
         (m) => m.ConfigComponent
@@ -13,6 +14,7 @@ export const APP_ROUTES: Route[] = [
   {
     path: AppRoute.GameSetup,
     title: 'Game Setup | Spirit Islander',
+    data: { state: AppRoute.GameSetup },
     loadComponent: () =>
       import('@atocha/spirit-islander/feature-game-setup').then(
         (m) => m.GameSetupComponent
@@ -20,12 +22,14 @@ export const APP_ROUTES: Route[] = [
   },
   {
     path: AppRoute.Home,
+    data: { state: AppRoute.Home },
     redirectTo: AppRoute.Config,
     pathMatch: 'full',
   },
   {
     path: AppRoute.Error,
     title: 'Error | Spirit Islander',
+    data: { state: AppRoute.Error },
     loadComponent: () =>
       import('@atocha/spirit-islander/feature-shell').then(
         (m) => m.PageNotFoundComponent
