d302b8d95e82a6716adff332a318bd662c5ea278
John Coppola <jdcoppola@gmail.com>
2023-04-08 13:54:31 -0400

    FEATURE: makes app.coponent standalone and moves bootstrap logic to main.ts

--- apps/globetrotter/src/app/app.component.ts
+++ apps/globetrotter/src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { ErrorService, LoaderService } from '@atocha/globetrotter/data-access';
+import { ErrorComponent, LoaderComponent } from '@atocha/globetrotter/ui';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [CommonModule, ErrorComponent, LoaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- apps/globetrotter/src/main.ts
+++ apps/globetrotter/src/main.ts
@@ -1,13 +1,23 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
-import { AppModule } from './app/app.module';
+import { APP_ROUTES } from './app/app-routes';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.log(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      APP_ROUTES,
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+    ),
+    importProvidersFrom([BrowserAnimationsModule, HttpClientModule]),
+  ],
+}).catch(console.error);
