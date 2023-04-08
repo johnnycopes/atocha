f9456473811679e5563a0f162336fdf561a96ad2
John Coppola <jdcoppola@gmail.com>
2023-04-08 09:06:42 -0400

    FILE: deletes ShellModule file

--- libs/menu-matriarch/feature-shell/src/index.ts
+++ libs/menu-matriarch/feature-shell/src/index.ts
@@ -1,5 +1,3 @@
-export * from './lib/menu-matriarch-feature-shell.module';
-
 export * from './lib/error/error.component';
 export * from './lib/page-not-found/page-not-found.component';
 export * from './lib/shell/shell.component';
