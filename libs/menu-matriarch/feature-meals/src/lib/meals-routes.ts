1b8127f2aa56fb925f66dd0cda19b639f5f4a398
John Coppola <jdcoppola@gmail.com>
2023-04-08 14:37:39 -0400

    OOPS: missed child routes files

--- libs/menu-matriarch/feature-meals/src/lib/meals-routes.ts
+++ libs/menu-matriarch/feature-meals/src/lib/meals-routes.ts
@@ -1,10 +1,11 @@
-import { Route } from '@angular/router';
+import { Routes } from '@angular/router';
+
 import { MealDetailsComponent } from './meal-details/meal-details.component';
 import { MealEditComponent } from './meal-edit/meal-edit.component';
 import { MealPlaceholderComponent } from './meal-placeholder/meal-placeholder.component';
 import { MealsComponent } from './meals.component';
 
-export const MEALS_ROUTES: Route[] = [
+export const MEALS_ROUTES: Routes = [
   {
     path: '',
     component: MealsComponent,
