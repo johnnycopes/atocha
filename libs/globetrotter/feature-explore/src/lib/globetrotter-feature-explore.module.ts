56ef265c3b123b52fa8bebe243742b96dc2316ac
John Coppola <jdcoppola@gmail.com>
2023-04-08 12:42:05 -0400

    FEATURE: makes ExploreModule contents standalone

--- libs/globetrotter/feature-explore/src/lib/globetrotter-feature-explore.module.ts
+++ libs/globetrotter/feature-explore/src/lib/globetrotter-feature-explore.module.ts
@@ -1,23 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { MeasurementPipe } from '@atocha/core/ui';
-import {
-  ListDetailsComponent,
-  SmallCapsComponent,
-} from '@atocha/globetrotter/ui';
 import { ExploreComponent } from './explore.component';
-import { ExploreCountryComponent } from './explore-country/explore-country.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ListDetailsComponent,
-    MeasurementPipe,
-    RouterModule.forChild([{ path: '', component: ExploreComponent }]),
-    SmallCapsComponent,
-  ],
-  declarations: [ExploreComponent, ExploreCountryComponent],
+  imports: [RouterModule.forChild([{ path: '', component: ExploreComponent }])],
 })
 export class GlobetrotterFeatureExploreModule {}
