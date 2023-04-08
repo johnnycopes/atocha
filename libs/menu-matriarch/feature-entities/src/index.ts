139602f9f5862f0212c35ae1e6b1899c4b53aeb0
John Coppola <jdcoppola@gmail.com>
2023-04-08 08:32:50 -0400

    FILE: deletes EntitiesModule file

--- libs/menu-matriarch/feature-entities/src/index.ts
+++ libs/menu-matriarch/feature-entities/src/index.ts
@@ -1,5 +1,3 @@
-export * from './lib/menu-matriarch-feature-entities.module';
-
 export * from './lib/dishes-list/dish-def.directive';
 export * from './lib/dishes-list/dishes-list.component';
 export * from './lib/meals-list/meal-def.directive';
