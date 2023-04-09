a83eae2c8d5818cff435f3d86e92a516bc5e41df
John Coppola <jdcoppola@gmail.com>
2023-04-08 22:16:12 -0400

    FILE: creates IngredientDataService

---
+++ libs/menu-matriarch/data-access/src/lib/internal/ingredient-data.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { DataService } from '@atocha/core/data-access';
+import { lower, sort } from '@atocha/core/util';
+import {
+  Endpoint,
+  Ingredient,
+  IngredientDto,
+  createIngredientDto,
+} from '@atocha/menu-matriarch/util';
+import { BatchService } from './batch.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IngredientDataService {
+  private _endpoint = Endpoint.ingredients;
+
+  constructor(
+    private _batchService: BatchService,
+    private _dataService: DataService
+  ) {}
+
+  getIngredient(id: string): Observable<IngredientDto | undefined> {
+    return this._dataService.getOne<IngredientDto>(this._endpoint, id);
+  }
+
+  getIngredients(uid: string): Observable<IngredientDto[]> {
+    return this._dataService
+      .getMany<IngredientDto>(this._endpoint, uid)
+      .pipe(
+        map((ingredientDtos) =>
+          sort(ingredientDtos, (ingredientDto) => lower(ingredientDto.name))
+        )
+      );
+  }
+
+  async createIngredient({
+    uid,
+    ingredient,
+  }: {
+    uid: string;
+    ingredient: Partial<Omit<IngredientDto, 'id' | 'uid'>>;
+  }): Promise<string> {
+    const id = this._dataService.createId();
+
+    await this._dataService.create<IngredientDto>(
+      this._endpoint,
+      id,
+      createIngredientDto({ id, uid, ...ingredient })
+    );
+
+    return id;
+  }
+
+  async updateIngredient(
+    ingredient: Ingredient,
+    updates: Partial<IngredientDto>
+  ): Promise<void> {
+    const batch = this._batchService.createBatch();
+
+    batch.update({
+      endpoint: Endpoint.ingredients,
+      id: ingredient.id,
+      data: updates,
+    });
+    // TODO: update ingredient doc and relevant dishes docs
+
+    await batch.commit();
+  }
+
+  async deleteIngredient(ingredient: Ingredient): Promise<void> {
+    const batch = this._batchService.createBatch();
+
+    batch.delete(Endpoint.ingredients, ingredient.id);
+    // TODO: update dish docs to remove ingredient
+
+    await batch.commit();
+  }
+}
