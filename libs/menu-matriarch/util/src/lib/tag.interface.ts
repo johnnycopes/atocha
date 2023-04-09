8d6cd0dbc60e2d98d5a06236874e4fecf64f5fcc
John Coppola <jdcoppola@gmail.com>
2023-04-09 16:02:21 -0400

    COMMENT: writes notes for tag.interface.ts

--- libs/menu-matriarch/util/src/lib/tag.interface.ts
+++ libs/menu-matriarch/util/src/lib/tag.interface.ts
@@ -1,9 +1,18 @@
+/** A customizable tag that can be applied to any number of dishes
+ * and/or meals in order to allow easier filtering when browsing
+ */
 export interface Tag {
+  /** Tag's unique ID */
   id: string;
+  /** Unique ID of the user associated with the tag */
   uid: string;
+  /** Display name of the tag */
   name: string;
+  /** (INACTIVE) Color of the tag */
   color: string;
+  /** The unique IDs of the meals the tag is applied to */
   mealIds: string[];
+  /** The unique IDs of the dishes the tag is applied to */
   dishIds: string[];
 }
 
