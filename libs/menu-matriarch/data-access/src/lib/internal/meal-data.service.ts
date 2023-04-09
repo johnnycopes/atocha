d030ae2240b83027dbb8d78443242077623b8b47
John Coppola <jdcoppola@gmail.com>
2023-04-09 09:34:33 -0400

    MANUAL: nests dtos folder inside internal folder

--- libs/menu-matriarch/data-access/src/lib/internal/meal-data.service.ts
+++ libs/menu-matriarch/data-access/src/lib/internal/meal-data.service.ts
@@ -5,7 +5,7 @@ import { map } from 'rxjs/operators';
 import { DataService } from '@atocha/core/data-access';
 import { lower, sort } from '@atocha/core/util';
 import { Meal } from '@atocha/menu-matriarch/util';
-import { MealDto, createMealDto } from '../dtos/meal-dto';
+import { MealDto, createMealDto } from './dtos/meal-dto';
 import { BatchService } from './batch.service';
 import { Endpoint } from './endpoint.enum';
 
