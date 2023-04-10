8bdf10fa1888426736dd809a5c6bcdf6b1722e53
John Coppola <jdcoppola@gmail.com>
2023-04-10 08:53:59 -0400

    FILE: writes ingredient-dto.spec

---
+++ libs/menu-matriarch/data-access/src/lib/internal/dtos/ingredient-dto.spec.ts
@@ -0,0 +1,31 @@
+import { createIngredientDto } from './ingredient-dto';
+
+describe('createIngredientDto', () => {
+  it('creates default ingredient when no arguments are passed in', () => {
+    const ingredient = createIngredientDto({});
+    expect(ingredient).toEqual({
+      id: '',
+      uid: '',
+      name: '',
+      type: 'uncategorized',
+      dishIds: [],
+    });
+  });
+
+  it('creates ingredient when arguments are passed in', () => {
+    const ingredient = createIngredientDto({
+      id: '1',
+      uid: '1',
+      name: 'Paprika',
+      type: 'spice',
+      dishIds: ['26'],
+    });
+    expect(ingredient).toEqual({
+      id: '1',
+      uid: '1',
+      name: 'Paprika',
+      type: 'spice',
+      dishIds: ['26'],
+    });
+  });
+});
