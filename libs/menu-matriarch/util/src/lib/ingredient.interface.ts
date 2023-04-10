f5bd102e4365045283708424523ee21e0c56b50f
John Coppola <jdcoppola@gmail.com>
2023-04-10 08:58:26 -0400

    COMMENT: annotates type properties of ingredient.interface

--- libs/menu-matriarch/util/src/lib/ingredient.interface.ts
+++ libs/menu-matriarch/util/src/lib/ingredient.interface.ts
@@ -1,8 +1,13 @@
 export interface Ingredient {
+  /** Ingredient's unique ID */
   id: string;
+  /** Unique ID of the user associated with the ingredient */
   uid: string;
+  /** Display name of the ingredient */
   name: string;
+  /** Category of ingredient */
   type: IngredientType;
+  /** The unique IDs of any dishes the ingredient belongs to */
   dishIds: string[];
 }
 
