2e233fea8a4cf27fa65cb0b7afbea3fb2fc06c42
John Coppola <jdcoppola@gmail.com>
2023-04-16 10:16:59 -0400

    FILE: deletes example component

--- libs/menu-matriarch/ui-generic/src/index.ts
+++ libs/menu-matriarch/ui-generic/src/index.ts
@@ -1,4 +1,2 @@
-export * from './lib/menu-matriarch-ui-generic/menu-matriarch-ui-generic.component';
-
 export * from './lib/tabset/tabset.component';
 export * from './lib/tabset/tab/tab.component';
