b4708eac5ba11e970a1356f7ebcfee3cbf0287a6
John Coppola <jdcoppola@gmail.com>
2023-04-16 10:24:54 -0400

    TOOL: duplicates inline-form component into ui-generic

--- libs/menu-matriarch/ui-generic/src/index.ts
+++ libs/menu-matriarch/ui-generic/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/alert/alert.component';
 export * from './lib/card/card.component';
 export * from './lib/input/input.component';
+export * from './lib/inline-form/inline-form.component';
 export * from './lib/tabset/tabset.component';
 export * from './lib/tabset/tab/tab.component';
