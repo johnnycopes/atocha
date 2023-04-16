ac5637ba7a23e46ed8b60569b820cbe80d4eb4cd
John Coppola <jdcoppola@gmail.com>
2023-04-16 10:47:28 -0400

    FEATURE: deletes _generic folder components

--- libs/menu-matriarch/ui/src/index.ts
+++ libs/menu-matriarch/ui/src/index.ts
@@ -1,14 +1,5 @@
 export * from './lib/track-by-functions';
 
-export * from './lib/_generic/alert/alert.component';
-export * from './lib/_generic/card/card.component';
-export * from './lib/_generic/inline-form/inline-form.component';
-export * from './lib/_generic/input/input.component';
-export * from './lib/_generic/options-menu/options-menu.component';
-export * from './lib/_generic/options-menu/options-menu-item/options-menu-item.component';
-export * from './lib/_generic/options-menu/options-menu-trigger.directive';
-export * from './lib/_generic/section/section.component';
-export * from './lib/_generic/small-caps-label/small-caps-label.component';
 export * from './lib/count/count.component';
 export * from './lib/dish-summary/dish-summary.component';
 export * from './lib/empty-view-placeholder/empty-view-placeholder.component';
