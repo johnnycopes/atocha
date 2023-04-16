9edc7b7e342a9cb3f55322619106c2573eca7e89
John Coppola <jdcoppola@gmail.com>
2023-04-16 10:28:35 -0400

    TOOL: duplicates small-caps-label component into ui-generic

--- libs/menu-matriarch/ui-generic/src/index.ts
+++ libs/menu-matriarch/ui-generic/src/index.ts
@@ -8,3 +8,4 @@ export * from './lib/options-menu/options-menu-trigger.directive';
 export * from './lib/tabset/tabset.component';
 export * from './lib/tabset/tab/tab.component';
 export * from './lib/section/section.component';
+export * from './lib/small-caps-label/small-caps-label.component';
---
+++ libs/menu-matriarch/ui-generic/src/lib/small-caps-label/small-caps-label.component.ts
@@ -0,0 +1,19 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  ViewEncapsulation,
+} from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'ui-small-caps-label',
+  templateUrl: './small-caps-label.component.html',
+  styleUrls: ['./small-caps-label.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class SmallCapsLabelComponent {
+  @HostBinding('class')
+  hostClass = 'app-small-caps-label';
+}
