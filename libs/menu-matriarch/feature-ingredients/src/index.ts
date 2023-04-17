7f167c741c7b48625a8f4cdcfdc7139cca74d369
John Coppola <jdcoppola@gmail.com>
2023-04-17 15:41:45 -0400

    FILE: scaffolds ingredients component

--- libs/menu-matriarch/feature-ingredients/src/index.ts
+++ libs/menu-matriarch/feature-ingredients/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/menu-matriarch-feature-ingredients/menu-matriarch-feature-ingredients.component';
+export * from './lib/ingredients.component';
---
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredients.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredients',
+  template: `<p>ingredients works!</p>`,
+})
+export class IngredientsComponent {}
