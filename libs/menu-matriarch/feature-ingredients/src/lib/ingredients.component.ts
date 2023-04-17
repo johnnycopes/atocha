33a7687dd2c74204130ff51abfc0323e78166ec3
John Coppola <jdcoppola@gmail.com>
2023-04-17 16:55:37 -0400

    FEATURE: pipes ingredients from service into template

--- libs/menu-matriarch/feature-ingredients/src/lib/ingredients.component.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredients.component.ts
@@ -1,8 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { IngredientService } from '@atocha/menu-matriarch/data-access';
 
 @Component({
   standalone: true,
   selector: 'app-ingredients',
-  template: `<p>ingredients works!</p>`,
+  imports: [CommonModule],
+  template: `{{ ingredients$ | async | json }}`,
 })
-export class IngredientsComponent {}
+export class IngredientsComponent {
+  ingredients$ = this._ingredientService.getIngredients();
+
+  constructor(private _ingredientService: IngredientService) {}
+}
