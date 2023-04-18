be58b96aab0c505418a1fcb7e8a8b15b9bb2fd8f
John Coppola <jdcoppola@gmail.com>
2023-04-18 10:03:49 -0400

    FEATURE: puts basic ingredient-card on the page to display data

--- libs/menu-matriarch/feature-ingredients/src/lib/ingredient-card/ingredient-card.component.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredient-card/ingredient-card.component.ts
@@ -1,12 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { CardComponent } from '@atocha/menu-matriarch/ui-generic';
+import { IngredientType } from '@atocha/menu-matriarch/util';
 
 @Component({
   selector: 'app-ingredient-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CardComponent, CommonModule],
   templateUrl: './ingredient-card.component.html',
   styleUrls: ['./ingredient-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IngredientCardComponent {}
+export class IngredientCardComponent {
+  @Input() name = '';
+  @Input() type: IngredientType = 'uncategorized';
+  @Input() dishIds: string[] = [];
+}
