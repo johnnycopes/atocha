fb7cb0b96ffa802fd49aa07b8f037d4f09b7a494
John Coppola <jdcoppola@gmail.com>
2023-04-18 18:47:28 -0400

    FEATURE: imports column inside board and puts board on main route page

--- libs/menu-matriarch/feature-ingredients/src/lib/ingredients-board/ingredients-board.component.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredients-board/ingredients-board.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { IngredientsBoardColumnComponent } from './ingredients-board-column/ingredients-board-column.component';
+
 @Component({
   selector: 'app-ingredients-board',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IngredientsBoardColumnComponent],
   templateUrl: './ingredients-board.component.html',
   styleUrls: ['./ingredients-board.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
--- libs/menu-matriarch/feature-ingredients/src/lib/ingredients.component.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredients.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IngredientService } from '@atocha/menu-matriarch/data-access';
 import { ingredientTrackByFn } from '@atocha/menu-matriarch/ui-domain';
 import { SectionComponent } from '@atocha/menu-matriarch/ui-generic';
-import { IngredientCardComponent } from './ingredient-card/ingredient-card.component';
+import { IngredientsBoardComponent } from './ingredients-board/ingredients-board.component';
 
 @Component({
   standalone: true,
@@ -12,7 +12,7 @@ import { IngredientCardComponent } from './ingredient-card/ingredient-card.compo
   templateUrl: './ingredients.component.html',
   styleUrls: ['./ingredients.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, IngredientCardComponent, SectionComponent],
+  imports: [CommonModule, IngredientsBoardComponent, SectionComponent],
 })
 export class IngredientsComponent {
   ingredients$ = this._ingredientService.getIngredients();
