03bad01b7b4187f91eb60dc95822580885826cec
John Coppola <jdcoppola@gmail.com>
2023-04-18 21:23:42 -0400

    FEATURE: simplifies ingredients-board-form component

--- libs/menu-matriarch/feature-ingredients/src/lib/ingredients-board/ingredients-board-form/ingredients-board-form.component.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredients-board/ingredients-board-form/ingredients-board-form.component.ts
@@ -6,32 +6,29 @@ import {
   EventEmitter,
   ChangeDetectionStrategy,
 } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+import { ButtonComponent } from '@atocha/core/ui';
+import { InlineNameEditComponent } from '@atocha/menu-matriarch/ui-domain';
 
 @Component({
   standalone: true,
   selector: 'app-ingredients-board-form',
-  imports: [CommonModule, FontAwesomeModule, FormsModule],
+  imports: [
+    ButtonComponent,
+    CommonModule,
+    FontAwesomeModule,
+    InlineNameEditComponent,
+  ],
   templateUrl: './ingredients-board-form.component.html',
   styleUrls: ['./ingredients-board-form.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IngredientsBoardFormComponent {
   @Input() name = '';
-  @Output() add: EventEmitter<string> = new EventEmitter();
+  @Output() add = new EventEmitter<string>();
+
   readonly addNewIcon = faPlus;
   adding = false;
-  model = '';
-
-  submitForm(form: NgForm): void {
-    this.add.emit(this.model);
-    this.resetForm(form);
-  }
-
-  resetForm(form: NgForm): void {
-    form.resetForm();
-    this.adding = false;
-  }
 }
