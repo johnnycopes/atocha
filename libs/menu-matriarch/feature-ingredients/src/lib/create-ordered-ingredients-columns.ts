0d44be4d2dd46d27276ef0ba10a0097558940141
John Coppola <jdcoppola@gmail.com>
2023-04-21 11:56:00 -0400

    FEATURE: simplifies logic

--- libs/menu-matriarch/feature-ingredients/src/lib/create-ordered-ingredients-columns.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/create-ordered-ingredients-columns.ts
@@ -1,17 +1,14 @@
-import { Ingredient, IngredientType } from '@atocha/menu-matriarch/util';
+import { IngredientType } from '@atocha/menu-matriarch/util';
 import { IngredientColumn } from './ingredients-board/ingredients-board.component';
 
 export function createOrderedIngredientsColumns(
   ordersRecord: Record<IngredientType, number>
 ): IngredientColumn[] {
+  const orders = Object.entries(ordersRecord);
   const columns: IngredientColumn[] = [];
 
-  for (const type in ordersRecord) {
-    const typedType = type as IngredientType;
-    columns[ordersRecord[typedType]] = {
-      name: typedType,
-      ingredients: new Array<Ingredient>(),
-    };
+  for (const [type, position] of orders) {
+    columns[position] = { name: type as IngredientType, ingredients: [] };
   }
 
   return columns;
