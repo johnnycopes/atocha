082052c28356e112130f317dc148df212a9cfe20
John Coppola <jdcoppola@gmail.com>
2023-04-21 08:54:20 -0400

    FILE: makes createOrderedIngredientsColumn function and spec

---
+++ libs/menu-matriarch/feature-ingredients/src/lib/create-ordered-ingredients-columns.spec.ts
@@ -0,0 +1,7 @@
+import { createOrderedIngredientsColumns } from './create-ordered-ingredients-columns';
+
+describe('createOrderedIngredientsColumns', () => {
+  it('returns', () => {
+    expect(createOrderedIngredientsColumns).toBeTruthy();
+  });
+});
---
+++ libs/menu-matriarch/feature-ingredients/src/lib/create-ordered-ingredients-columns.ts
@@ -0,0 +1,9 @@
+import { IngredientType } from '@atocha/menu-matriarch/util';
+import { IngredientColumn } from './ingredients-board/ingredients-board.component';
+
+export function createOrderedIngredientsColumns(
+  ingredientTypeOrder: Record<IngredientType, number>
+): IngredientColumn[] {
+  ingredientTypeOrder;
+  return [];
+}
