981f70af0b8c1ae66d5e9add7306f4bdcc5c1a1c
John Coppola <jdcoppola@gmail.com>
2023-04-21 08:59:00 -0400

    FEATURE: writes first test

--- libs/menu-matriarch/feature-ingredients/src/lib/create-ordered-ingredients-columns.spec.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/create-ordered-ingredients-columns.spec.ts
@@ -1,7 +1,35 @@
 import { createOrderedIngredientsColumns } from './create-ordered-ingredients-columns';
 
 describe('createOrderedIngredientsColumns', () => {
-  it('returns', () => {
-    expect(createOrderedIngredientsColumns).toBeTruthy();
+  it('returns correctly ordered columns', () => {
+    expect(
+      createOrderedIngredientsColumns({
+        'bread/bakery': 0,
+        'canned/jarred good': 1,
+        condiment: 2,
+        'dry good': 3,
+        frozen: 4,
+        grocery: 5,
+        'meat/seafood': 6,
+        oil: 7,
+        produce: 8,
+        refrigerated: 9,
+        spice: 10,
+        uncategorized: 11,
+      })
+    ).toEqual([
+      { name: 'bread/bakery', ingredients: [] },
+      { name: 'canned/jarred good', ingredients: [] },
+      { name: 'condiment', ingredients: [] },
+      { name: 'dry good', ingredients: [] },
+      { name: 'frozen', ingredients: [] },
+      { name: 'grocery', ingredients: [] },
+      { name: 'meat/seafood', ingredients: [] },
+      { name: 'oil', ingredients: [] },
+      { name: 'produce', ingredients: [] },
+      { name: 'refrigerated', ingredients: [] },
+      { name: 'spice', ingredients: [] },
+      { name: 'uncategorized', ingredients: [] },
+    ]);
   });
 });
