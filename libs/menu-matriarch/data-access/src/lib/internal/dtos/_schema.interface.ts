7ac9ff7845b2e1d257f755c603a52f0fa3f88c2d
John Coppola <jdcoppola@gmail.com>
2023-04-21 08:24:55 -0400

    Documents database schema interface (#319)
    * FILE: adds _schema.interface.ts
    
    * MANUAL: alphabetical imports

---
+++ libs/menu-matriarch/data-access/src/lib/internal/dtos/_schema.interface.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { MenuDto } from './menu-dto';
+import { MealDto } from './meal-dto';
+import { DishDto } from './dish-dto';
+import { TagDto } from './tag-dto';
+import { UserDto } from './user-dto';
+
+/**
+ * This interface documents the database schema and is intentionally not exported
+ */
+interface DatabaseSchema {
+  menus: MenuDto[];
+  meals: MealDto[];
+  dishes: DishDto[];
+  tags: TagDto[];
+  users: UserDto[];
+}
