6bc70b1dfc8a3ce4073b14a981cc8ff680bd99c9
John Coppola <jdcoppola@gmail.com>
2023-06-01 08:42:43 -0400

    FEATURE: sets up options menu in card

--- libs/menu-matriarch/feature-ingredients/src/lib/ingredient-card/ingredient-card.component.ts
+++ libs/menu-matriarch/feature-ingredients/src/lib/ingredient-card/ingredient-card.component.ts
@@ -1,15 +1,38 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
+import { ButtonComponent } from '@atocha/core/ui';
+import {
+  CardComponent,
+  OptionsMenuComponent,
+  OptionsMenuItemComponent,
+  OptionsMenuTriggerDirective,
+} from '@atocha/menu-matriarch/ui-generic';
 import { CountComponent } from '@atocha/menu-matriarch/ui-domain';
-import { CardComponent } from '@atocha/menu-matriarch/ui-generic';
 import { IngredientType } from '@atocha/menu-matriarch/util';
 
 @Component({
   standalone: true,
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: '[app-ingredient-card]',
-  imports: [CardComponent, CommonModule, CountComponent],
+  imports: [
+    ButtonComponent,
+    CardComponent,
+    CommonModule,
+    CountComponent,
+    FontAwesomeModule,
+    OptionsMenuComponent,
+    OptionsMenuItemComponent,
+    OptionsMenuTriggerDirective,
+  ],
   templateUrl: './ingredient-card.component.html',
   styleUrls: ['./ingredient-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -18,4 +41,10 @@ export class IngredientCardComponent {
   @Input() name = '';
   @Input() type: IngredientType = 'misc';
   @Input() dishIds: string[] = [];
+  @Output() delete = new EventEmitter<void>();
+  readonly menuToggleIcon = faEllipsisV;
+
+  rename(): void {
+    // do something
+  }
 }
