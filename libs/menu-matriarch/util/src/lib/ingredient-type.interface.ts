4f9b17c92d58884ec7bf678b4e8de1ec06bf3489
John Coppola <jdcoppola@gmail.com>
2023-06-18 11:57:32 -0400

    COMMENT: fixes IngredientType interface comments

--- libs/menu-matriarch/util/src/lib/ingredient-type.interface.ts
+++ libs/menu-matriarch/util/src/lib/ingredient-type.interface.ts
@@ -1,12 +1,12 @@
 import { Ingredient } from './ingredient.interface';
 
 export interface IngredientType {
-  /** Ingredient's unique ID */
+  /** Ingredient type's unique ID */
   id: string;
-  /** Unique ID of the user associated with the ingredient */
+  /** Unique ID of the user associated with the ingredient type */
   uid: string;
-  /** Display name of the ingredient */
+  /** Display name of the ingredient type */
   name: string;
-  /** The unique IDs of any ingredients classified as the type */
+  /** The unique IDs of any ingredients classified as the ingredient type */
   ingredients: Ingredient[];
 }
