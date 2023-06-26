c57ac37d70064e858a0f07ca6137f18d78d95aef
John Coppola <jdcoppola@gmail.com>
2023-06-25 22:29:37 -0700

    FEATURE: adds batch.service to core/data-access

--- libs/core/data-access/src/index.ts
+++ libs/core/data-access/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/auth.service';
+export * from './lib/batch.service';
 export * from './lib/batch';
 export * from './lib/data.service';
 export * from './lib/firestore.service';
