43cbebb5476c037a59e2740cf240780947c33351
John Coppola <jdcoppola@gmail.com>
2023-06-25 20:21:41 -0700

    FEATURE: fixes mappers' import statements

--- libs/menu-matriarch/shared/data-access-dtos/src/lib/mappers/map-dish-dto-to-dish.ts
+++ libs/menu-matriarch/shared/data-access-dtos/src/lib/mappers/map-dish-dto-to-dish.ts
@@ -1,5 +1,5 @@
 import { Tag, Dish, Ingredient } from '@atocha/menu-matriarch/shared/util';
-import { DishDto } from '../../../../data-access-dtos/src/lib/dtos/dish-dto';
+import { DishDto } from '../dtos/dish-dto';
 
 export function mapDishDtoToDish(
   dishDto: DishDto,
