674b8f8fd09813ef5a3247ff587b9e0db20a3787
John Coppola <jdcoppola@gmail.com>
2023-06-26 18:34:37 -0700

    FILE: creates core/data-access batch.service

--- libs/core/data-access/src/index.ts
+++ libs/core/data-access/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/auth.service';
 export * from './lib/batch';
+export * from './lib/batch.service';
 export * from './lib/data.service';
 export * from './lib/firestore.service';
 export * from './lib/local-storage.service';
---
+++ libs/core/data-access/src/lib/batch.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+import { Batch, BatchUpdate } from './batch';
+import { FirestoreService } from './firestore.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+class BatchService {
+  constructor(private _firestoreService: FirestoreService) {}
+
+  createBatch(): Batch {
+    return new Batch(this._firestoreService.createBatch(), (endpoint, id) =>
+      this._firestoreService.getDocRef(endpoint, id)
+    );
+  }
+
+  addToArray(...ids: string[]): string[] {
+    return this._firestoreService.addToArray(...ids);
+  }
+
+  removeFromArray(...ids: string[]): string[] {
+    return this._firestoreService.removeFromArray(...ids);
+  }
+
+  changeCounter(value: number): number {
+    return this._firestoreService.changeCounter(value);
+  }
+}
+
+export { BatchUpdate, BatchService };
