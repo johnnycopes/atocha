24cffabfd20c2e1cd80233ddc0d9938293833113
John Coppola <jdcoppola@gmail.com>
2023-10-30 14:40:01 -0400

    FEATURE: implements toStates function in ModelTransformer method

--- libs/core/util-selection/src/lib/model-transformer/model-transformer.ts
+++ libs/core/util-selection/src/lib/model-transformer/model-transformer.ts
@@ -2,6 +2,7 @@ import { reduceRecursively } from '../reduce-recursively';
 import { Ids, IdsMap } from './ids';
 import { toArray } from './to-array';
 import { toSet } from './to-set';
+import { toStates } from './to-states';
 
 export type SelectionModel = string[] | Set<string>;
 export type SelectionState = 'checked' | 'indeterminate';
@@ -31,40 +32,7 @@ export class ModelTransformer<T> {
   }
 
   toStates(model: SelectionModel): SelectionStates {
-    const states: SelectionStates = {};
-    const idsModel = Array.isArray(model) ? new Set(model) : model;
-
-    /*
-      Iterating through the IDs backwards builds up `states` from the leaf nodes
-      of the tree up towards the root. The code is procedural and highly depends
-      on this order but is more performant since each node reliably knows the state
-      of all of its children.
-    */
-    for (let i = this._idsArr.length - 1; i >= 0; i--) {
-      const id = this._idsArr[i];
-      if (idsModel.has(id)) {
-        states[id] = 'checked';
-      } else {
-        const ids = this._idsMap.get(id)?.childrenIds ?? [];
-        if (ids.length) {
-          const idsInState = ids.reduce(
-            (total, id) => total + (states[id] ? 1 : 0),
-            0
-          );
-          const totalIds = ids.length;
-
-          if (totalIds === 1 && states[ids[0]] === 'indeterminate') {
-            states[id] = 'indeterminate';
-          } else if (totalIds === idsInState) {
-            states[id] = 'checked';
-          } else if (idsInState > 0) {
-            states[id] = 'indeterminate';
-          }
-        }
-      }
-    }
-
-    return states;
+    return toStates(model, this._ids);
   }
 
   updateStates(
