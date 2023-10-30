ad43fbf75cd04dc12613523d29a99f817a4c4c11
John Coppola <jdcoppola@gmail.com>
2023-10-30 10:44:39 -0400

    FILE: extracts toArray to standalone function

---
+++ libs/core/util-selection/src/lib/model-transformer/to-array.ts
@@ -0,0 +1,20 @@
+import { Ids } from './ids';
+import { SelectionModel, SelectionStates } from './model-transformer';
+
+export function toArray<T>(
+  states: SelectionStates,
+  ids: Ids<T>
+): Extract<SelectionModel, string[]> {
+  const model: string[] = [];
+
+  for (const id of ids.descending) {
+    if (
+      states[id] === 'checked' &&
+      (ids.map.get(id)?.childrenIds ?? []).length === 0
+    ) {
+      model.push(id);
+    }
+  }
+
+  return model;
+}
