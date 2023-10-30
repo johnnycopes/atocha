bceed56b51e3271f2983a10d33fc7b12195ab76a
John Coppola <jdcoppola@gmail.com>
2023-10-30 08:58:49 -0400

    FILE: writes standalone createIdsMap function and test

---
+++ libs/core/util-selection/src/lib/model-transformer/create-ids-map.ts
@@ -0,0 +1,23 @@
+import { reduceRecursively } from '../reduce-recursively';
+
+export type IdsMap = Map<
+  string,
+  { parentId: string | undefined; childrenIds: string[] }
+>;
+
+export function createIdsMap<T>(
+  tree: T,
+  getId: (tree: T) => string,
+  getChildren: (tree: T) => T[]
+) {
+  return reduceRecursively<T, IdsMap>({
+    item: tree,
+    getItems: getChildren,
+    initialValue: new Map(),
+    reducer: (accum, item, parent) =>
+      accum.set(getId(item), {
+        parentId: parent ? getId(parent) : undefined,
+        childrenIds: getChildren(item).map(getId),
+      }),
+  });
+}
