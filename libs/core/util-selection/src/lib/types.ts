b1c0d19ec09629042b54007a878f36a1fea48c13
John Coppola <jdcoppola@gmail.com>
2023-10-31 16:22:31 -0400

    OOPS: forgot to export Counts

--- libs/core/util-selection/src/lib/types.ts
+++ libs/core/util-selection/src/lib/types.ts
@@ -1,5 +1,7 @@
 import { Model, State, States } from './internal/types';
+import { Counts as InternalCounts } from './internal/get-counts';
 
 export type SelectionModel = Model;
 export type SelectionState = State;
 export type SelectionStates = States;
+export type Counts = InternalCounts;
