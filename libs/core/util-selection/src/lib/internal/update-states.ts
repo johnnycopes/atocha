6be26c38870bd1ee5b0780de9260a7a8d9bdb598
John Coppola <jdcoppola@gmail.com>
2023-10-31 07:41:09 -0400

    FEATURE: implements getConnectedIds method in updateStates

--- libs/core/util-selection/src/lib/internal/update-states.ts
+++ libs/core/util-selection/src/lib/internal/update-states.ts
@@ -1,4 +1,3 @@
-import { reduceRecursively } from './reduce-recursively';
 import { Ids } from './ids';
 import { SelectionState, SelectionStates } from './types';
 
@@ -8,36 +7,7 @@ export function updateStates<T>(
   states: SelectionStates,
   ids: Ids<T>
 ): SelectionStates {
-  let updatedStates = updateItemAndDescendantStates({
-    id,
-    checked,
-    states,
-    ids,
-  });
-  updatedStates = updateAncestorStates(id, updatedStates, ids);
-  return updatedStates;
-}
-
-function updateItemAndDescendantStates<T>({
-  id,
-  checked,
-  states,
-  ids,
-}: {
-  id: string;
-  checked: boolean;
-  states: SelectionStates;
-  ids: Ids<T>;
-}): SelectionStates {
-  const itemAndDescendantsIds = reduceRecursively<string, string[]>({
-    item: id,
-    getItems: (id: string) => ids.map.get(id)?.childrenIds ?? [],
-    initialValue: [],
-    reducer: (accum, curr) => {
-      accum.push(curr);
-      return accum;
-    },
-  });
+  const { ancestorIds, itemAndDescendantsIds } = ids.getConnectedIds(id);
 
   itemAndDescendantsIds.forEach((id) => {
     if (checked) {
@@ -45,28 +15,6 @@ function updateItemAndDescendantStates<T>({
     } else {
       delete states[id];
     }
-  });
-
-  return states;
-}
-
-function updateAncestorStates<T>(
-  id: string,
-  states: SelectionStates,
-  ids: Ids<T>
-): SelectionStates {
-  const ancestorIds = reduceRecursively<string, string[]>({
-    item: id,
-    getItems: (id) => {
-      const parentId = ids.map.get(id)?.parentId;
-      return parentId ? [parentId] : [];
-    },
-    initialValue: [],
-    reducer: (accum, curr) => {
-      if (id === curr) return accum;
-      accum.push(curr);
-      return accum;
-    },
   });
 
   ancestorIds.forEach((ancestorId) => {
