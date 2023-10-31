b318c2bfd6937eaa3ab630e29a987cf612420008
John Coppola <jdcoppola@gmail.com>
2023-10-31 14:27:47 -0400

    FILE: extracts createMap to standalone function and tests

---
+++ libs/core/util-selection/src/lib/internal/create-map.spec.ts
@@ -0,0 +1,36 @@
+import { AFRICA, getChildren, getId } from '../mock-data';
+import { IdsMap, createMap } from './create-map';
+
+describe('createMap', () => {
+  it("returns a flat map structure with references to every node's parent ID and children IDs", () => {
+    expect(createMap(AFRICA, getId, getChildren)).toEqual<IdsMap>(
+      new Map([
+        [
+          'Africa',
+          {
+            parentId: undefined,
+            childrenIds: [
+              'Southern Africa',
+              'Central Africa',
+              'Northern Africa',
+            ],
+          },
+        ],
+        [
+          'Southern Africa',
+          { parentId: 'Africa', childrenIds: ['Swaziland', 'Namibia'] },
+        ],
+        ['Central Africa', { parentId: 'Africa', childrenIds: [] }],
+        ['Northern Africa', { parentId: 'Africa', childrenIds: ['Morocco'] }],
+        ['Swaziland', { parentId: 'Southern Africa', childrenIds: [] }],
+        ['Namibia', { parentId: 'Southern Africa', childrenIds: [] }],
+        [
+          'Morocco',
+          { parentId: 'Northern Africa', childrenIds: ['Marrakesh', 'Fes'] },
+        ],
+        ['Marrakesh', { parentId: 'Morocco', childrenIds: [] }],
+        ['Fes', { parentId: 'Morocco', childrenIds: [] }],
+      ])
+    );
+  });
+});
---
+++ libs/core/util-selection/src/lib/internal/create-map.ts
@@ -0,0 +1,23 @@
+import { reduceRecursively } from './reduce-recursively';
+
+export type IdsMap = Map<
+  string,
+  { parentId: string | undefined; childrenIds: readonly string[] }
+>;
+
+export function createMap<T>(
+  tree: T,
+  getId: (item: T) => string,
+  getChildren: (item: T) => T[]
+) {
+  return reduceRecursively<T, IdsMap>({
+    item: tree,
+    getItems: getChildren,
+    initialValue: new Map(),
+    reducer: (accum, item, parent) =>
+      accum.set(getId(item), {
+        parentId: parent ? getId(parent) : undefined,
+        childrenIds: getChildren(item).map(getId),
+      }),
+  });
+}
