fdc3e7cd17122e196628644714ee61069ce0cd6f
John Coppola <jdcoppola@gmail.com>
2023-10-31 07:11:33 -0400

    FEATURE: implements updateState function in ModelTransformer

--- libs/core/util-selection/src/lib/model-transformer/model-transformer.ts
+++ libs/core/util-selection/src/lib/model-transformer/model-transformer.ts
@@ -1,14 +1,12 @@
-import { reduceRecursively } from '../reduce-recursively';
-import { SelectionModel, SelectionState, SelectionStates } from './types';
-import { Ids, IdsMap } from './ids';
+import { SelectionModel, SelectionStates } from './types';
+import { Ids } from './ids';
 import { toArray } from './to-array';
 import { toSet } from './to-set';
 import { toStates } from './to-states';
+import { updateStates } from './update-states';
 
 export class ModelTransformer<T> {
   private readonly _ids: Ids<T>;
-  private readonly _idsMap: IdsMap;
-  private readonly _idsArr: readonly string[];
 
   constructor(
     private _tree: T,
@@ -16,8 +14,6 @@ export class ModelTransformer<T> {
     private _getChildren: (tree: T) => T[]
   ) {
     this._ids = new Ids(this._tree, this._getId, this._getChildren);
-    this._idsMap = this._ids.map;
-    this._idsArr = this._ids.descending;
   }
 
   toArray(states: SelectionStates): Extract<SelectionModel, string[]> {
@@ -37,90 +33,6 @@ export class ModelTransformer<T> {
     id: string,
     states: SelectionStates
   ): SelectionStates {
-    let updatedStates = this._updateItemAndDescendantStates({
-      id,
-      checked,
-      states,
-    });
-    updatedStates = this._updateAncestorStates(id, updatedStates);
-    return updatedStates;
-  }
-
-  private _updateItemAndDescendantStates({
-    id,
-    checked,
-    states,
-  }: {
-    id: string;
-    checked: boolean;
-    states: SelectionStates;
-  }): SelectionStates {
-    const itemAndDescendantsIds = reduceRecursively<string, string[]>({
-      item: id,
-      getItems: (id: string) => this._idsMap.get(id)?.childrenIds ?? [],
-      initialValue: [],
-      reducer: (accum, curr) => {
-        accum.push(curr);
-        return accum;
-      },
-    });
-
-    itemAndDescendantsIds.forEach((id) => {
-      if (checked) {
-        states[id] = 'checked';
-      } else {
-        delete states[id];
-      }
-    });
-
-    return states;
-  }
-
-  private _updateAncestorStates(
-    id: string,
-    states: SelectionStates
-  ): SelectionStates {
-    const ancestorIds = reduceRecursively<string, string[]>({
-      item: id,
-      getItems: (id) => {
-        const parentId = this._idsMap.get(id)?.parentId;
-        return parentId ? [parentId] : [];
-      },
-      initialValue: [],
-      reducer: (accum, curr) => {
-        if (id === curr) return accum;
-        accum.push(curr);
-        return accum;
-      },
-    });
-
-    ancestorIds.forEach((ancestorId) => {
-      const ancestorChildrenIds =
-        this._idsMap.get(ancestorId)?.childrenIds ?? [];
-      const ancestorChildrenStates: Record<SelectionState, number> = {
-        checked: 0,
-        indeterminate: 0,
-      };
-
-      ancestorChildrenIds.forEach((childId) => {
-        const childState = states[childId];
-        if (states[childId]) {
-          ancestorChildrenStates[childState]++;
-        }
-      });
-
-      if (ancestorChildrenStates.checked === ancestorChildrenIds.length) {
-        states[ancestorId] = 'checked';
-      } else if (
-        ancestorChildrenStates.checked > 0 ||
-        ancestorChildrenStates.indeterminate > 0
-      ) {
-        states[ancestorId] = 'indeterminate';
-      } else {
-        delete states[ancestorId];
-      }
-    });
-
-    return states;
+    return updateStates(checked, id, states, this._ids);
   }
 }
