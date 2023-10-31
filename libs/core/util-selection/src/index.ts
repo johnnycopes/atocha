80757c667f55591447f07b0742eaa7fcc16faaaf
John Coppola <jdcoppola@gmail.com>
2023-10-31 16:01:17 -0400

    FILE: creates top-level types barrel file

--- libs/core/util-selection/src/index.ts
+++ libs/core/util-selection/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/counter';
 export * from './lib/model-transformer';
 export * from './lib/mock-data';
+export * from './lib/types';
---
+++ libs/core/util-selection/src/lib/types.ts
@@ -0,0 +1,5 @@
+import { Model, State, States } from './internal/types';
+
+export type SelectionModel = Model;
+export type SelectionState = State;
+export type SelectionStates = States;
