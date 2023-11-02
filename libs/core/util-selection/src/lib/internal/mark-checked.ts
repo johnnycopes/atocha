179c1e1fa67378ef06f7fc959acb996e4e711283
John Coppola <jdcoppola@gmail.com>
2023-11-02 14:27:10 -0400

    Revert "FILE: writes markChecked function and tests"
    This reverts commit cd86dcd483c32d3904ebfec9a7cb8e9b4ca3185d.

--- libs/core/util-selection/src/lib/internal/mark-checked.ts
+++
@@ -1,8 +0,0 @@
-import { State } from './types';
-
-export function markChecked(
-  target: boolean | undefined,
-  initial: State | undefined
-): boolean {
-  return typeof target === 'boolean' ? target : initial !== 'checked';
-}
