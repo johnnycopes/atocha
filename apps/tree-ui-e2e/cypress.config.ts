87d648bf3e07989c93751996ab506fdf7d691f4f
John Coppola <jdcoppola@gmail.com>
2023-11-03 19:15:11 -0400

    FEATURE: updates ports to make room

--- apps/tree-ui-e2e/cypress.config.ts
+++ apps/tree-ui-e2e/cypress.config.ts
@@ -8,7 +8,7 @@ const cypressJsonConfig = {
   videosFolder: '../../dist/cypress/apps/tree-ui-e2e/videos',
   screenshotsFolder: '../../dist/cypress/apps/tree-ui-e2e/screenshots',
   chromeWebSecurity: false,
-  baseUrl: 'http://localhost:4200',
+  baseUrl: 'http://localhost:4700',
   specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
   supportFile: 'src/support/e2e.ts',
 };
