1312ac79cd910cbf6262725602fa6cd0104f2427
John Coppola <jdcoppola@gmail.com>
2023-11-03 19:05:50 -0400

    FEATURE: gets tests working

--- apps/tree-ui-e2e/cypress.config.ts
+++ apps/tree-ui-e2e/cypress.config.ts
@@ -1,6 +1,21 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
+const cypressJsonConfig = {
+  fileServerFolder: '.',
+  fixturesFolder: './src/fixtures',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/tree-ui-e2e/videos',
+  screenshotsFolder: '../../dist/cypress/apps/tree-ui-e2e/screenshots',
+  chromeWebSecurity: false,
+  baseUrl: 'http://localhost:4200',
+  specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
+  supportFile: 'src/support/e2e.ts',
+};
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__filename),
+    ...cypressJsonConfig,
+    testIsolation: true,
+  },
 });
