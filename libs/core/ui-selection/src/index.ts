2433d2ae20ed31b94bad008bd2559d1a6c5c3009
John Coppola <jdcoppola@gmail.com>
2023-11-03 15:46:11 -0400

    FILE: deletes boilerplate module and export

--- libs/core/ui-selection/src/index.ts
+++ libs/core/ui-selection/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/core-ui-selection.module';
 export * from './lib/counted-selection-tree/counted-selection-tree.component';
 export * from './lib/selection-tree/selection-tree.component';
