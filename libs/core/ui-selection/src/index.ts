264023c925a6cbd8cd0f58641efe6c3f1c8e0363
John Coppola <jdcoppola@gmail.com>
2023-11-03 15:45:23 -0400

    FILE: copies counted-selection-tree into lib

--- libs/core/ui-selection/src/index.ts
+++ libs/core/ui-selection/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/core-ui-selection.module';
+export * from './lib/counted-selection-tree/counted-selection-tree.component';
 export * from './lib/selection-tree/selection-tree.component';
