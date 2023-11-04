cbcf78d03e818aba512e859aaf31683556390fbf
John Coppola <jdcoppola@gmail.com>
2023-11-04 09:08:23 -0400

    FEATURE: implements snakeCase in format.ts

--- libs/core/util/src/lib/format.spec.ts
+++ libs/core/util/src/lib/format.spec.ts
@@ -1,4 +1,4 @@
-import { lower, upper, upperFirst } from './format';
+import { lower, snakeCase, upper, upperFirst } from './format';
 
 describe('formatting functions', () => {
   describe('lower', () => {
@@ -19,6 +19,24 @@ describe('formatting functions', () => {
     it('transforms the first letter of a string to uppercase', () => {
       expect(upperFirst('test')).toEqual('Test');
       expect(upperFirst('second test')).toEqual('Second test');
+    });
+  });
+
+  describe('snakeCase', () => {
+    it("doesn't affect an empty string", () => {
+      expect(snakeCase('')).toBe('');
+    });
+
+    it('transforms a lowercase string with spaces', () => {
+      expect(snakeCase('unique object identifier')).toBe(
+        'unique-object-identifier'
+      );
+    });
+
+    it('transforms an uppercase string with spaces', () => {
+      expect(snakeCase('UNIQUE OBJECT IDENTIFIER')).toBe(
+        'unique-object-identifier'
+      );
     });
   });
 });
