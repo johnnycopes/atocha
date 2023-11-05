a816821ce2e33b8f6c5b9f5aa2b659e9a9f22062
John Coppola <jdcoppola@gmail.com>
2023-11-05 12:39:03 -0500

    COMMENT: additional comments

--- libs/tree/util/src/lib/shared/types.ts
+++ libs/tree/util/src/lib/shared/types.ts
@@ -1,12 +1,38 @@
+/**
+ * A `readonly` collection of selected node IDs.
+ */
 export type Model = ArrayModel | SetModel;
+
+/**
+ * A `readonly` array of selected node IDs.
+ */
 export type ArrayModel = readonly string[];
+
+/**
+ * A `readonly` set of selected node IDs.
+ */
 export type SetModel = ReadonlySet<string>;
+
+/**
+ * Used to distinguish between array vs. set models.
+ */
 export function isArrayModel(model: Model): model is ArrayModel {
   return Array.isArray(model);
 }
 
+/**
+ * A `readonly` record of the tree's partially or completely selected states.
+ */
 export type MutableStates = Record<string, State>;
+
+/**
+ * A readonly version of `MutableStates`.
+ */
 export type States = Readonly<MutableStates>;
+
+/**
+ * The state of an individual node. Unselected isn't represented as a state, but rather the absence of the node from the States record.
+ */
 export type State = 'checked' | 'indeterminate';
 
 /**
