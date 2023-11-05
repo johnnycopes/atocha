14770ffd57f1cdc8dd3bfeb0e22bbb8799400b40
John Coppola <jdcoppola@gmail.com>
2023-11-05 14:53:02 -0500

    FEATURE: removes ArrayModel and SetModel from lib exports

--- libs/tree/util/src/index.ts
+++ libs/tree/util/src/index.ts
@@ -2,13 +2,11 @@ export * from './lib/counter';
 export * from './lib/transformer';
 export * from './lib/shared/mock-data';
 export {
-  ArrayModel,
   Counts,
   GetChildren,
   GetId,
   GetLeafCount,
   Model,
-  SetModel,
   State,
   States,
   Tree,
