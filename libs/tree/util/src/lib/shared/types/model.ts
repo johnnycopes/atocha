cd868296bd148b1a969a4fb9039486f6f3590c47
John Coppola <jdcoppola@gmail.com>
2023-11-06 20:18:36 -0500

    FEATURE: implements collection types in model.ts

--- libs/tree/util/src/lib/shared/types/model.ts
+++ libs/tree/util/src/lib/shared/types/model.ts
@@ -1,21 +1,23 @@
+import { ReadonlyCollection, isReadonlyArray } from './collection';
+
 /**
  * A `readonly` collection of selected node IDs.
  */
-export type Model = ArrayModel | SetModel;
+export type Model = ReadonlyCollection<string>;
 
 /**
  * A `readonly` array of selected node IDs.
  */
-export type ArrayModel = readonly string[];
+export type ArrayModel = Extract<ReadonlyCollection<string>, readonly string[]>;
 
 /**
  * A `readonly` set of selected node IDs.
  */
-export type SetModel = ReadonlySet<string>;
+export type SetModel = Extract<ReadonlyCollection<string>, ReadonlySet<string>>;
 
 /**
  * Used to distinguish between array vs. set models.
  */
 export function isArrayModel(model: Model): model is ArrayModel {
-  return Array.isArray(model);
+  return isReadonlyArray<string>(model);
 }
