11aefe69d2075061e526f7673520cf9e6ed63230
John Coppola <jdcoppola@gmail.com>
2023-11-08 18:56:41 -0500

    Revert "TOOL: changes getters to getIds methods"
    This reverts commit f25059f007daf3bd0782c4055d002cbfd788ff99.

--- libs/tree/util/src/lib/selection-tree.ts
+++ libs/tree/util/src/lib/selection-tree.ts
@@ -15,8 +15,8 @@ import { updateStates } from './selection-tree/update-states';
 import { ITree, Tree } from './tree';
 
 export interface ISelectionTree<T> extends ITree<T> {
-  getIdsArray(): IdsArray;
-  getIdsSet(): IdsSet;
+  array: IdsArray;
+  set: IdsSet;
   getState(id: string): State | undefined;
   updateOne(id: string): SelectionTree<T>;
   updateMultiple(ids: Ids): SelectionTree<T>;
@@ -37,11 +37,11 @@ export class SelectionTree<T> extends Tree<T> implements ISelectionTree<T> {
     this._states = this._toStates(ids);
   }
 
-  getIdsArray(): IdsArray {
+  get array(): IdsArray {
     return toArray(this._states, this._tree);
   }
 
-  getIdsSet(): IdsSet {
+  get set(): IdsSet {
     return toSet(this._states, this._tree);
   }
 
