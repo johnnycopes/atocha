2dcfa3b302a586bed5abc69d5532cc90f01cc483
John Coppola <jdcoppola@gmail.com>
2023-11-08 08:46:57 -0500

    Reorganizes tree classes (#403)
    * TOOL: renames Tree class to SelectionTree
    
    * TOOL: renames CountableTree class to CountedSelectionTree
    
    * FILE: renames internal folders to match new class names
    
    * FILE: creates Tree class
    
    * FEATURE: extends ITree in ISelectionTree and updates SelectionTree class definition
    
    * FEATURE: uses class inheritance to reduce class size
    
    * FEATURE: exports new tree class from lib
    
    * FEATURE: makes getLeafCount readonly

---
+++ libs/tree/util/src/lib/selection-tree.ts
@@ -0,0 +1,69 @@
+import {
+  IdsArray,
+  GetChildren,
+  GetId,
+  Ids,
+  MutableStates,
+  IdsSet,
+  States,
+} from './shared/types';
+import { IdsTree } from './selection-tree/ids/ids-tree';
+import { toArray } from './selection-tree/to-array';
+import { toSet } from './selection-tree/to-set';
+import { toStates } from './selection-tree/to-states';
+import { updateStates } from './selection-tree/update-states';
+import { ITree, Tree } from './tree';
+
+export interface ISelectionTree<T> extends ITree<T> {
+  states: States;
+  array: IdsArray;
+  set: IdsSet;
+  updateOne(id: string): SelectionTree<T>;
+  updateMultiple(ids: Ids): SelectionTree<T>;
+}
+
+export class SelectionTree<T> extends Tree<T> implements ISelectionTree<T> {
+  private _states: MutableStates;
+  private readonly _tree: IdsTree<T>;
+
+  constructor(
+    root: T,
+    getId: GetId<T>,
+    getChildren: GetChildren<T>,
+    ids: Ids = []
+  ) {
+    super(root, getId, getChildren);
+    this._tree = new IdsTree(root, getId, getChildren);
+    this._states = this._toStates(ids);
+  }
+
+  get states(): States {
+    return this._states;
+  }
+
+  get array(): IdsArray {
+    return toArray(this.states, this._tree);
+  }
+
+  get set(): IdsSet {
+    return toSet(this.states, this._tree);
+  }
+
+  updateOne(id: string): SelectionTree<T> {
+    updateStates({
+      states: this._states,
+      tree: this._tree,
+      targetId: id,
+    });
+    return this;
+  }
+
+  updateMultiple(ids: Ids): SelectionTree<T> {
+    this._states = this._toStates(ids);
+    return this;
+  }
+
+  private _toStates(ids: Ids): MutableStates {
+    return toStates(ids, this._tree);
+  }
+}
