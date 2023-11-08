6e74782539c40f8b115fce8d2ca1ef90fff40bdd
John Coppola <jdcoppola@gmail.com>
2023-11-07 19:37:59 -0500

    FILE: deletes counter class, spec, and export

--- libs/tree/util/src/index.ts
+++ libs/tree/util/src/index.ts
@@ -1,6 +1,4 @@
 export * from './lib/countable-tree';
-export * from './lib/counter';
-export * from './lib/transformer';
 export * from './lib/tree';
 export * from './lib/shared/mock-data';
 export {
