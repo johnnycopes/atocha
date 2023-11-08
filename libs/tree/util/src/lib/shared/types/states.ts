264f430e9eb4ab86eab227bb059a041f9e10d567
John Coppola <jdcoppola@gmail.com>
2023-11-08 18:47:42 -0500

    MANUAL: moves State definition to top of the file

--- libs/tree/util/src/lib/shared/types/states.ts
+++ libs/tree/util/src/lib/shared/types/states.ts
@@ -1,4 +1,9 @@
 /**
+ * The state of an individual node. Unselected isn't represented as a state, but rather the absence of the node from the States record.
+ */
+export type State = 'checked' | 'indeterminate';
+
+/**
  * A mutable record of the tree nodes' partially or completely selected states, keyed by node ID.
  */
 export type States = Record<string, State>;
@@ -7,8 +12,3 @@ export type States = Record<string, State>;
  * A readonly version of `MutableStates`.
  */
 export type ReadonlyStates = Readonly<States>;
-
-/**
- * The state of an individual node. Unselected isn't represented as a state, but rather the absence of the node from the States record.
- */
-export type State = 'checked' | 'indeterminate';
