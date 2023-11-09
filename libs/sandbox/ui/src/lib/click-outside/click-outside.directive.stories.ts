a71a320d5d8895cd6d7705a100bafc3e64c4bf10
John Coppola <jdcoppola@gmail.com>
2023-11-09 17:35:23 -0500

    FEATURE: duplicates ClickOutsideDirective into sandbox-ui

---
+++ libs/sandbox/ui/src/lib/click-outside/click-outside.directive.stories.ts
@@ -0,0 +1,39 @@
+import {
+  StoryObj,
+  moduleMetadata,
+  Meta,
+  componentWrapperDecorator,
+} from '@storybook/angular';
+
+import { ClickOutsideDirective } from './click-outside.directive';
+import { StorybookWrapperComponent } from '../../../.storybook/storybook-wrapper.component';
+
+export default {
+  title: 'Click Outside',
+  decorators: [
+    moduleMetadata({
+      imports: [ClickOutsideDirective, StorybookWrapperComponent],
+    }),
+    componentWrapperDecorator(StorybookWrapperComponent),
+  ],
+  argTypes: {
+    inside: { action: 'INSIDE' },
+    outside: { action: 'OUTSIDE' },
+  },
+} as Meta<ClickOutsideDirective>;
+
+export const base: StoryObj<Args> = {
+  render: (args: Args) => ({
+    props: args,
+    template: `
+      <p style="background: goldenrod; padding: 16px;"
+        (click)="inside($event)"
+        (appClickOutside)="outside($event)"
+      >
+        Click inside or outside of me to see different actions fire
+      </p>
+    `,
+  }),
+};
+
+type Args = Partial<ClickOutsideDirective>;
