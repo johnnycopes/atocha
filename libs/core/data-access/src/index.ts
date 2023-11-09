d0614da59374cd36a3ccff709a4215fede87f6ad
John Coppola <jdcoppola@gmail.com>
2023-11-09 07:48:55 -0500

    FILE: deletes Firebase files from @atocha/core/data-access

--- libs/core/data-access/src/index.ts
+++ libs/core/data-access/src/index.ts
@@ -1,4 +1,1 @@
-export * from './lib/auth.service';
-export * from './lib/batch.service';
-export * from './lib/data.service';
 export * from './lib/local-storage.service';
--- libs/core/data-access/src/lib/util/dedupe.ts
+++
@@ -1,9 +0,0 @@
-export function dedupe<T>(...arrays: T[][]): T[] {
-  const elements = [];
-
-  for (const arr of arrays) {
-    elements.push(...arr);
-  }
-
-  return Array.from(new Set(elements));
-}
