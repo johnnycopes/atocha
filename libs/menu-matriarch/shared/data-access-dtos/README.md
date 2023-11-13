7e35c716b871272bbae05e6b98689e240535d7f3
John Coppola <jdcoppola@gmail.com>
2023-11-13 00:46:18 -0600

    TOOL: deletes menu-matriarch-data-access-dtos lib

--- libs/menu-matriarch/shared/data-access-dtos/README.md
+++
@@ -1,7 +0,0 @@
-# menu-matriarch-shared-data-access-dtos
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test menu-matriarch-shared-data-access-dtos` to execute the unit tests.
--- libs/menu-matriarch/shared/data-access-dtos/src/lib/dto-service.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface DtoService<TEntity, TDto, TData = Partial<TEntity>> {
-  getOne(id: string): Observable<TDto | undefined>;
-  getMany(uid: string): Observable<TDto[]>;
-  create(uid: string, data: TData): Promise<string>;
-  update(item: TEntity, data: TData): Promise<void>;
-  delete(item: TEntity): Promise<void>;
-}
