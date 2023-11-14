39d4f2a94143bca24bfa039cd45c458ad7c223ee
John Coppola <jdcoppola@gmail.com>
2023-11-14 17:11:16 -0600

    COMMENT: updates export file

--- libs/spirit-islander/util/src/index.ts
+++ libs/spirit-islander/util/src/index.ts
@@ -12,7 +12,7 @@ export * from './lib/game/players';
 export * from './lib/game/scenarios';
 export * from './lib/game/spirits';
 
-// App types
+// App data and types
 export * from './lib/app/app-migration';
 export * from './lib/app/combo.interface';
 export * from './lib/app/config.interface';
