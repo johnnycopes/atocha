32a7bcfbbc707d52d7c8a5167500bef71702303a
John Coppola <jdcoppola@gmail.com>
2023-11-14 12:24:19 -0600

    MANUAL: organizes exports

--- libs/spirit-islander/util/src/index.ts
+++ libs/spirit-islander/util/src/index.ts
@@ -3,6 +3,9 @@ export * from './lib/game/adversaries';
 export * from './lib/game/boards';
 export * from './lib/game/difficulty';
 export * from './lib/game/expansions';
+export * from './lib/game/get-adversary-by-id';
+export * from './lib/game/get-difficulty';
+export * from './lib/game/get-options';
 export * from './lib/game/maps';
 export * from './lib/game/option';
 export * from './lib/game/players';
@@ -13,15 +16,10 @@ export * from './lib/game/spirits';
 export * from './lib/app/app-migration';
 export * from './lib/app/combo.interface';
 export * from './lib/app/config.interface';
-export * from './lib/app/game-setup.interface';
-export * from './lib/app/route.enum';
-
-// Functions
 export * from './lib/app/create-game-setup';
 export * from './lib/app/create-model';
-export * from './lib/game/get-adversary-by-id';
-export * from './lib/game/get-difficulty';
-export * from './lib/game/get-options';
+export * from './lib/app/game-setup.interface';
 export * from './lib/app/get-valid-combos';
+export * from './lib/app/route.enum';
 export * from './lib/app/select-boards';
 export * from './lib/app/update-model';
