c4a39e46aca50c7d73745d9c17b7147e0490d472
John Coppola <jdcoppola@gmail.com>
2023-11-15 17:25:02 -0600

    FEATURE: improves Updater class

--- libs/spirit-islander/util/src/lib/updater.ts
+++ libs/spirit-islander/util/src/lib/updater.ts
@@ -1,7 +1,106 @@
+import { AdversaryLevelId } from './game/adversaries';
+import { BalancedBoardName } from './game/boards';
 import { ExpansionName } from './game/expansions';
+import { MapName } from './game/maps';
+import { ScenarioName } from './game/scenarios';
+import { SpiritName } from './game/spirits';
+import {
+  createAdversariesModel,
+  createBoardsModel,
+  createMapsModel,
+  createScenariosModel,
+  createSpiritsModel,
+} from './app/create-model';
 
 export class Updater {
-  updateModel<TName>(
+  private _spiritNames: readonly SpiritName[] = [];
+  get spiritNames() {
+    return this._spiritNames;
+  }
+
+  private _boardNames: readonly BalancedBoardName[] = [];
+  get boardNames() {
+    return this._boardNames;
+  }
+
+  private _mapNames: readonly MapName[] = [];
+  get mapNames() {
+    return this._mapNames;
+  }
+
+  private _scenarioNames: readonly ScenarioName[] = [];
+  get scenarioNames() {
+    return this._scenarioNames;
+  }
+
+  private _adversaryLevelIds: readonly AdversaryLevelId[] = [];
+  get adversaryLevelIds() {
+    return this._adversaryLevelIds;
+  }
+
+  constructor({
+    spiritNames,
+    boardNames,
+    mapNames,
+    scenarioNames,
+    adversaryLevelIds,
+  }: {
+    spiritNames?: readonly SpiritName[];
+    boardNames?: readonly BalancedBoardName[];
+    mapNames?: readonly MapName[];
+    scenarioNames?: readonly ScenarioName[];
+    adversaryLevelIds?: readonly AdversaryLevelId[];
+  } = {}) {
+    this._spiritNames = spiritNames ?? [];
+    this._boardNames = boardNames ?? [];
+    this._mapNames = mapNames ?? [];
+    this._scenarioNames = scenarioNames ?? [];
+    this._adversaryLevelIds = adversaryLevelIds ?? [];
+  }
+
+  update(
+    expansions: readonly ExpansionName[],
+    target: 'Expansions' | ExpansionName
+  ): Updater {
+    this._spiritNames = this._updateModel(
+      createSpiritsModel,
+      this._spiritNames,
+      expansions,
+      target
+    );
+
+    this._boardNames = this._updateModel(
+      createBoardsModel,
+      this._boardNames,
+      expansions,
+      target
+    );
+
+    this._mapNames = this._updateModel(
+      createMapsModel,
+      this._mapNames,
+      expansions,
+      target
+    );
+
+    this._scenarioNames = this._updateModel(
+      createScenariosModel,
+      this._scenarioNames,
+      expansions,
+      target
+    );
+
+    this._adversaryLevelIds = this._updateModel(
+      createAdversariesModel,
+      this._adversaryLevelIds,
+      expansions,
+      target
+    );
+
+    return this;
+  }
+
+  _updateModel<TName>(
     createModel: (expansions?: readonly ExpansionName[]) => readonly TName[],
     existingModel: readonly TName[],
     expansions: readonly ExpansionName[],
