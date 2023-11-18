4c49eccd71e89948ae8c6157e8a0c017d921bd92
John Coppola <jdcoppola@gmail.com>
2023-11-18 15:17:32 -0500

    FEATURE: exports options.ts contents

--- libs/spirit-islander/shared/util/src/index.ts
+++ libs/spirit-islander/shared/util/src/index.ts
@@ -4,6 +4,7 @@ export * from './lib/game/boards';
 export * from './lib/game/difficulty';
 export * from './lib/game/expansions';
 export * from './lib/game/maps';
+export * from './lib/game/option';
 export * from './lib/game/options';
 export * from './lib/game/players';
 export * from './lib/game/scenarios';
