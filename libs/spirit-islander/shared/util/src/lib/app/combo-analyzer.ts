acf3858667e7e4f663d9062c74fa4ccfb6d1d495
John Coppola <jdcoppola@gmail.com>
2023-11-18 10:14:47 -0500

    FEATURE: makes ComboAnalyzer arrays readonly where possible

--- libs/spirit-islander/shared/util/src/lib/app/combo-analyzer.ts
+++ libs/spirit-islander/shared/util/src/lib/app/combo-analyzer.ts
@@ -16,11 +16,22 @@ export class ComboAnalyzer<T> {
     T9 extends T,
     T10 extends T
   >(
-    allOptions: [T1[], T2[], T3[], T4[], T5[], T6[], T7[], T8[], T9[], T10[]],
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[],
+      readonly T5[],
+      readonly T6[],
+      readonly T7[],
+      readonly T8[],
+      readonly T9[],
+      T10[]
+    ],
     isValidCombo?: (
-      options: [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]
+      options: readonly [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10]
     ) => boolean
-  ): [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10][];
+  ): readonly [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10][];
   getPossibleCombos<
     T1 extends T,
     T2 extends T,
@@ -32,9 +43,21 @@ export class ComboAnalyzer<T> {
     T8 extends T,
     T9 extends T
   >(
-    allOptions: [T1[], T2[], T3[], T4[], T5[], T6[], T7[], T8[], T9[]],
-    isValidCombo?: (options: [T1, T2, T3, T4, T5, T6, T7, T8, T9]) => boolean
-  ): [T1, T2, T3, T4, T5, T6, T7, T8, T9][];
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[],
+      readonly T5[],
+      readonly T6[],
+      readonly T7[],
+      readonly T8[],
+      readonly T9[]
+    ],
+    isValidCombo?: (
+      options: readonly [T1, T2, T3, T4, T5, T6, T7, T8, T9]
+    ) => boolean
+  ): readonly [T1, T2, T3, T4, T5, T6, T7, T8, T9][];
   getPossibleCombos<
     T1 extends T,
     T2 extends T,
@@ -45,9 +68,20 @@ export class ComboAnalyzer<T> {
     T7 extends T,
     T8 extends T
   >(
-    allOptions: [T1[], T2[], T3[], T4[], T5[], T6[], T7[], T8[]],
-    isValidCombo?: (options: [T1, T2, T3, T4, T5, T6, T7, T8]) => boolean
-  ): [T1, T2, T3, T4, T5, T6, T7, T8][];
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[],
+      readonly T5[],
+      readonly T6[],
+      readonly T7[],
+      readonly T8[]
+    ],
+    isValidCombo?: (
+      options: readonly [T1, T2, T3, T4, T5, T6, T7, T8]
+    ) => boolean
+  ): readonly [T1, T2, T3, T4, T5, T6, T7, T8][];
   getPossibleCombos<
     T1 extends T,
     T2 extends T,
@@ -57,9 +91,17 @@ export class ComboAnalyzer<T> {
     T6 extends T,
     T7 extends T
   >(
-    allOptions: [T1[], T2[], T3[], T4[], T5[], T6[], T7[]],
-    isValidCombo?: (options: [T1, T2, T3, T4, T5, T6, T7]) => boolean
-  ): [T1, T2, T3, T4, T5, T6, T7][];
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[],
+      readonly T5[],
+      readonly T6[],
+      readonly T7[]
+    ],
+    isValidCombo?: (options: readonly [T1, T2, T3, T4, T5, T6, T7]) => boolean
+  ): readonly [T1, T2, T3, T4, T5, T6, T7][];
   getPossibleCombos<
     T1 extends T,
     T2 extends T,
@@ -68,9 +110,16 @@ export class ComboAnalyzer<T> {
     T5 extends T,
     T6 extends T
   >(
-    allOptions: [T1[], T2[], T3[], T4[], T5[], T6[]],
-    isValidCombo?: (options: [T1, T2, T3, T4, T5, T6]) => boolean
-  ): [T1, T2, T3, T4, T5, T6][];
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[],
+      readonly T5[],
+      readonly T6[]
+    ],
+    isValidCombo?: (options: readonly [T1, T2, T3, T4, T5, T6]) => boolean
+  ): readonly [T1, T2, T3, T4, T5, T6][];
   getPossibleCombos<
     T1 extends T,
     T2 extends T,
@@ -78,43 +127,57 @@ export class ComboAnalyzer<T> {
     T4 extends T,
     T5 extends T
   >(
-    allOptions: [T1[], T2[], T3[], T4[], T5[]],
-    isValidCombo?: (options: [T1, T2, T3, T4, T5]) => boolean
-  ): [T1, T2, T3, T4, T5][];
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[],
+      readonly T5[]
+    ],
+    isValidCombo?: (options: readonly [T1, T2, T3, T4, T5]) => boolean
+  ): readonly [T1, T2, T3, T4, T5][];
   getPossibleCombos<T1 extends T, T2 extends T, T3 extends T, T4 extends T>(
-    allOptions: [T1[], T2[], T3[], T4[]],
-    isValidCombo?: (options: [T1, T2, T3, T4]) => boolean
-  ): [T1, T2, T3, T4][];
+    allOptions: readonly [
+      readonly T1[],
+      readonly T2[],
+      readonly T3[],
+      readonly T4[]
+    ],
+    isValidCombo?: (options: readonly [T1, T2, T3, T4]) => boolean
+  ): readonly [T1, T2, T3, T4][];
   getPossibleCombos<T1 extends T, T2 extends T, T3 extends T>(
-    allOptions: [T1[], T2[], T3[]],
-    isValidCombo?: (options: [T1, T2, T3]) => boolean
-  ): [T1, T2, T3][];
+    allOptions: readonly [readonly T1[], readonly T2[], readonly T3[]],
+    isValidCombo?: (options: readonly [T1, T2, T3]) => boolean
+  ): readonly [T1, T2, T3][];
   getPossibleCombos<T1 extends T, T2 extends T>(
-    allOptions: [T1[], T2[]],
-    isValidCombo?: (options: [T1, T2]) => boolean
-  ): [T1, T2][];
+    allOptions: readonly [readonly T1[], readonly T2[]],
+    isValidCombo?: (options: readonly [T1, T2]) => boolean
+  ): readonly [T1, T2][];
   getPossibleCombos<T1 extends T>(
-    allOptions: [T1[]],
-    isValidCombo?: (options: [T1]) => boolean
-  ): [T1][];
+    allOptions: readonly [readonly T1[]],
+    isValidCombo?: (options: readonly [T1]) => boolean
+  ): readonly (readonly [T1])[];
   getPossibleCombos(
-    allOptions: T[][],
-    isValidCombo?: (options: T[]) => boolean
-  ): T[][];
-  getPossibleCombos(allOptions: T[][], isValidCombo?: unknown): T[][] {
+    allOptions: readonly (readonly T[])[],
+    isValidCombo?: (options: readonly T[]) => boolean
+  ): readonly (readonly T[])[];
+  getPossibleCombos(
+    allOptions: readonly (readonly T[])[],
+    isValidCombo?: unknown
+  ): readonly (readonly T[])[] {
     return this._getPossibleCombos(
       allOptions,
-      isValidCombo as (options: T[]) => boolean
+      isValidCombo as (options: readonly T[]) => boolean
     );
   }
 
   private _getPossibleCombos(
-    allOptionsLibrary: T[][],
-    isValidCombo: (options: T[]) => boolean = () => true,
+    allOptionsLibrary: readonly (readonly T[])[],
+    isValidCombo: (options: readonly T[]) => boolean = () => true,
     possibleCombos: T[][] = [],
     allOptionsIndex = 0,
     currentCombo: T[] = []
-  ): T[][] {
+  ): readonly T[][] {
     if (allOptionsIndex === allOptionsLibrary.length) {
       if (isValidCombo(currentCombo)) {
         possibleCombos.push([...currentCombo]);
