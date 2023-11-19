36d031203f3f4f7eb57fdde64496b2412c3982eb
John Coppola <jdcoppola@gmail.com>
2023-11-19 10:18:41 -0500

    TOOL: better accumulator names in reduce functions

--- libs/spirit-islander/game-setup/feature/src/lib/game-setup-output/get-adversary-name-by-id.ts
+++ libs/spirit-islander/game-setup/feature/src/lib/game-setup-output/get-adversary-name-by-id.ts
@@ -5,9 +5,9 @@ import {
 } from '@atocha/spirit-islander/shared/util';
 
 const namesById = ADVERSARIES.reduce<Record<AdversaryLevelId, AdversaryName>>(
-  (accum, adversary) => {
-    adversary.levels.forEach(({ id }) => (accum[id] = adversary.name));
-    return accum;
+  (record, adversary) => {
+    adversary.levels.forEach(({ id }) => (record[id] = adversary.name));
+    return record;
   },
   { none: 'No Adversary' } as Record<AdversaryLevelId, AdversaryName>
 );
--- libs/spirit-islander/shared/util/src/lib/functions/get-adversary-level-ids.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-adversary-level-ids.ts
@@ -3,8 +3,8 @@ import { Adversary, AdversaryLevelId } from '../types';
 export function getAdversaryLevelIds(
   adversaries: readonly Adversary[]
 ): AdversaryLevelId[] {
-  return adversaries.reduce<AdversaryLevelId[]>((model, adversary) => {
-    adversary.levels.forEach((level) => model.push(level.id));
-    return model;
+  return adversaries.reduce<AdversaryLevelId[]>((ids, adversary) => {
+    adversary.levels.forEach(({ id }) => ids.push(id));
+    return ids;
   }, []);
 }
