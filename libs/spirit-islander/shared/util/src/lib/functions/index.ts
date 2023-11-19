9a14d543f09dcc504924a7442138c5ed1a46a923
John Coppola <jdcoppola@gmail.com>
2023-11-19 09:21:59 -0500

    FEATURE: barrel file in functions folder

---
+++ libs/spirit-islander/shared/util/src/lib/functions/index.ts
@@ -0,0 +1,5 @@
+export * from './get-adversary-level-ids';
+export * from './get-difficulty';
+export * from './get-names';
+export * from './get-options-by-name';
+export * from './get-options-by-expansion';
