36998d7abd904575ba8fb2d895729a3327fede6c
John Coppola <jdcoppola@gmail.com>
2023-11-19 11:09:02 -0500

    FEATURE: adds optional 'derivesFrom' property to Spirit interface

--- libs/spirit-islander/shared/util/src/lib/types/spirits.ts
+++ libs/spirit-islander/shared/util/src/lib/types/spirits.ts
@@ -1,6 +1,8 @@
 import { SPIRITS } from '../data';
 import { ExpansionOption } from './option';
 
-export type Spirit = ExpansionOption<SpiritName>;
+export interface Spirit extends ExpansionOption<SpiritName> {
+  derivesFrom?: SpiritName;
+}
 
 export type SpiritName = (typeof SPIRITS)[number]['name'];
