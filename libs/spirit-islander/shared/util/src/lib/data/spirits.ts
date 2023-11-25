70fadf307ff18dc49a92bfe35048141c9a254c77
John Coppola <jdcoppola@gmail.com>
2023-11-25 10:21:42 -0500

    FEATURE: adds aspects to spirits data

--- libs/spirit-islander/shared/util/src/lib/data/spirits.ts
+++ libs/spirit-islander/shared/util/src/lib/data/spirits.ts
@@ -1,7 +1,27 @@
 export const SPIRITS = [
   { name: 'A Spread of Rampant Green' },
+  {
+    name: 'Regrowth',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'A Spread of Rampant Green',
+  },
+  {
+    name: 'Tangles',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'A Spread of Rampant Green',
+  },
   { name: 'Breath of Darkness Down Your Spine', expansion: 'Nature Incarnate' },
   { name: 'Bringer of Dreams and Nightmares' },
+  {
+    name: 'Enticing',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Bringer of Dreams and Nightmares',
+  },
+  {
+    name: 'Violence',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Bringer of Dreams and Nightmares',
+  },
   { name: 'Dances Up Earthquakes', expansion: 'Nature Incarnate' },
   { name: 'Devouring Teeth Lurk Underfoot', expansion: 'Horizons' },
   { name: 'Downpour Drenches the World', expansion: 'Promo Pack 2' },
@@ -12,8 +32,18 @@ export const SPIRITS = [
   { name: 'Fractured Days Split the Sky', expansion: 'Jagged Earth' },
   { name: 'Grinning Trickster Stirs Up Trouble', expansion: 'Jagged Earth' },
   { name: 'Heart of the Wildfire', expansion: 'Promo Pack 1' },
+  {
+    name: 'Transforming',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Heart of the Wildfire',
+  },
   { name: 'Hearth-Vigil', expansion: 'Nature Incarnate' },
   { name: 'Keeper of the Forbidden Wilds', expansion: 'Branch & Claw' },
+  {
+    name: 'Spreading Hostility',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Keeper of the Forbidden Wilds',
+  },
   { name: "Lightning's Swift Strike" },
   {
     name: 'Immense',
@@ -26,16 +56,36 @@ export const SPIRITS = [
     aspectOf: "Lightning's Swift Strike",
   },
   {
+    name: 'Sparking',
+    expansion: 'Nature Incarnate',
+    aspectOf: "Lightning's Swift Strike",
+  },
+  {
     name: 'Wind',
     expansion: 'Jagged Earth',
     aspectOf: "Lightning's Swift Strike",
   },
   { name: 'Lure of the Deep Wilderness', expansion: 'Jagged Earth' },
+  {
+    name: 'Lair',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Lure of the Deep Wilderness',
+  },
   { name: 'Many Minds Move as One', expansion: 'Jagged Earth' },
   { name: "Ocean's Hungry Grasp" },
+  {
+    name: 'Deeps',
+    expansion: 'Nature Incarnate',
+    aspectOf: "Ocean's Hungry Grasp",
+  },
   { name: 'Relentless Gaze of the Sun', expansion: 'Nature Incarnate' },
   { name: 'Rising Heat of Stone and Sand', expansion: 'Horizons' },
   { name: 'River Surges in Sunlight' },
+  {
+    name: 'Haven',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'River Surges in Sunlight',
+  },
   {
     name: 'Sunshine',
     expansion: 'Jagged Earth',
@@ -47,10 +97,20 @@ export const SPIRITS = [
     aspectOf: 'River Surges in Sunlight',
   },
   { name: 'Serpent Slumbering Beneath the Island', expansion: 'Promo Pack 1' },
+  {
+    name: 'Locus',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Serpent Slumbering Beneath the Island',
+  },
   { name: 'Shadows Flicker Like Flame' },
   {
     name: 'Amorphous',
     expansion: 'Promo Pack 2',
+    aspectOf: 'Shadows Flicker Like Flame',
+  },
+  {
+    name: 'Dark Fire',
+    expansion: 'Nature Incarnate',
     aspectOf: 'Shadows Flicker Like Flame',
   },
   {
@@ -69,18 +129,58 @@ export const SPIRITS = [
     aspectOf: 'Shadows Flicker Like Flame',
   },
   { name: 'Sharp Fangs Behind the Leaves', expansion: 'Branch & Claw' },
+  {
+    name: 'Encircle',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Sharp Fangs Behind the Leaves',
+  },
+  {
+    name: 'Unconstrained',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Sharp Fangs Behind the Leaves',
+  },
   { name: 'Shifting Memory of Ages', expansion: 'Jagged Earth' },
+  {
+    name: 'Intensity',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Shifting Memory of Ages',
+  },
+  {
+    name: 'Mentor',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Shifting Memory of Ages',
+  },
   { name: 'Shroud of Silent Mist', expansion: 'Jagged Earth' },
+  {
+    name: 'Stranded',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Shroud of Silent Mist',
+  },
   { name: 'Sun-Bright Whirlwind', expansion: 'Horizons' },
   { name: 'Starlight Seeks Its Form', expansion: 'Jagged Earth' },
   { name: "Stone's Unyielding Defiance", expansion: 'Jagged Earth' },
   { name: 'Towering Roots of the Jungle', expansion: 'Nature Incarnate' },
   { name: 'Thunderspeaker' },
+  {
+    name: 'Tactician',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Thunderspeaker',
+  },
+  {
+    name: 'Warrior',
+    expansion: 'Nature Incarnate',
+    aspectOf: 'Thunderspeaker',
+  },
   { name: 'Vengeance as a Burning Plague', expansion: 'Jagged Earth' },
   { name: 'Vital Strength of the Earth' },
   {
     name: 'Might',
     expansion: 'Promo Pack 2',
+    aspectOf: 'Vital Strength of the Earth',
+  },
+  {
+    name: 'Nourishing',
+    expansion: 'Nature Incarnate',
     aspectOf: 'Vital Strength of the Earth',
   },
   {
