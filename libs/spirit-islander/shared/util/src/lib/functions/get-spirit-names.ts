463f606fdaf6ebe9e0b46324f83e0da2e829d518
John Coppola <jdcoppola@gmail.com>
2023-11-26 09:55:42 -0500

    FILE: deletes unused getSpiritNames

--- libs/spirit-islander/shared/util/src/lib/functions/get-spirit-names.ts
+++
@@ -1,8 +0,0 @@
-import { Spirit, SpiritName } from '../types';
-import { getNames } from './get-names';
-
-export function getSpiritNames(
-  spirits: readonly Spirit[]
-): readonly SpiritName[] {
-  return getNames(spirits);
-}
