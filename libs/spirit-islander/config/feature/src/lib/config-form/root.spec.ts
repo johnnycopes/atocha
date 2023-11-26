1ffc460114a34375087976d448761c705d68b1af
John Coppola <jdcoppola@gmail.com>
2023-11-26 11:31:45 -0500

    TEST: fixes tests after spirit updates

--- libs/spirit-islander/config/feature/src/lib/config-form/root.spec.ts
+++ libs/spirit-islander/config/feature/src/lib/config-form/root.spec.ts
@@ -33,7 +33,43 @@ describe('Root', () => {
       id: 'Spirits',
       children: [
         { id: 'A Spread of Rampant Green' },
+        {
+          id: 'Regrowth',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'A Spread of Rampant Green',
+          },
+        },
+        {
+          id: 'Tangles',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'A Spread of Rampant Green',
+          },
+        },
+        {
+          id: 'Breath of Darkness Down Your Spine',
+          display: { expansion: 'Nature Incarnate' },
+        },
         { id: 'Bringer of Dreams and Nightmares' },
+        {
+          id: 'Enticing',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Bringer of Dreams and Nightmares',
+          },
+        },
+        {
+          id: 'Violence',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Bringer of Dreams and Nightmares',
+          },
+        },
+        {
+          id: 'Dances Up Earthquakes',
+          display: { expansion: 'Nature Incarnate' },
+        },
         {
           id: 'Devouring Teeth Lurk Underfoot',
           display: { expansion: 'Horizons' },
@@ -41,6 +77,10 @@ describe('Root', () => {
         {
           id: 'Downpour Drenches the World',
           display: { expansion: 'Promo Pack 2' },
+        },
+        {
+          id: 'Ember-Eyed Behemoth',
+          display: { expansion: 'Nature Incarnate' },
         },
         {
           id: 'Eyes Watch From the Trees',
@@ -67,14 +107,28 @@ describe('Root', () => {
           display: { expansion: 'Promo Pack 1' },
         },
         {
+          id: 'Transforming',
           display: {
-            expansion: 'Branch & Claw',
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Heart of the Wildfire',
           },
-          id: 'Keeper of the Forbidden Wilds',
         },
         {
-          id: "Lightning's Swift Strike",
+          id: 'Hearth-Vigil',
+          display: { expansion: 'Nature Incarnate' },
         },
+        {
+          id: 'Keeper of the Forbidden Wilds',
+          display: { expansion: 'Branch & Claw' },
+        },
+        {
+          id: 'Spreading Hostility',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Keeper of the Forbidden Wilds',
+          },
+        },
+        { id: "Lightning's Swift Strike" },
         {
           id: 'Immense',
           display: {
@@ -90,6 +144,13 @@ describe('Root', () => {
           },
         },
         {
+          id: 'Sparking',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: "Lightning's Swift Strike",
+          },
+        },
+        {
           id: 'Wind',
           display: {
             expansion: 'Jagged Earth',
@@ -101,15 +162,40 @@ describe('Root', () => {
           display: { expansion: 'Jagged Earth' },
         },
         {
+          id: 'Lair',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Lure of the Deep Wilderness',
+          },
+        },
+        {
           id: 'Many Minds Move as One',
           display: { expansion: 'Jagged Earth' },
         },
         { id: "Ocean's Hungry Grasp" },
         {
+          id: 'Deeps',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: "Ocean's Hungry Grasp",
+          },
+        },
+        {
+          id: 'Relentless Gaze of the Sun',
+          display: { expansion: 'Nature Incarnate' },
+        },
+        {
           id: 'Rising Heat of Stone and Sand',
           display: { expansion: 'Horizons' },
         },
         { id: 'River Surges in Sunlight' },
+        {
+          id: 'Haven',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'River Surges in Sunlight',
+          },
+        },
         {
           id: 'Sunshine',
           display: {
@@ -128,11 +214,25 @@ describe('Root', () => {
           id: 'Serpent Slumbering Beneath the Island',
           display: { expansion: 'Promo Pack 1' },
         },
+        {
+          id: 'Locus',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Serpent Slumbering Beneath the Island',
+          },
+        },
         { id: 'Shadows Flicker Like Flame' },
         {
           id: 'Amorphous',
           display: {
             expansion: 'Promo Pack 2',
+            aspectOf: 'Shadows Flicker Like Flame',
+          },
+        },
+        {
+          id: 'Dark Fire',
+          display: {
+            expansion: 'Nature Incarnate',
             aspectOf: 'Shadows Flicker Like Flame',
           },
         },
@@ -162,12 +262,47 @@ describe('Root', () => {
           display: { expansion: 'Branch & Claw' },
         },
         {
+          id: 'Encircle',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Sharp Fangs Behind the Leaves',
+          },
+        },
+        {
+          id: 'Unconstrained',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Sharp Fangs Behind the Leaves',
+          },
+        },
+        {
           id: 'Shifting Memory of Ages',
           display: { expansion: 'Jagged Earth' },
         },
         {
+          id: 'Intensity',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Shifting Memory of Ages',
+          },
+        },
+        {
+          id: 'Mentor',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Shifting Memory of Ages',
+          },
+        },
+        {
           id: 'Shroud of Silent Mist',
           display: { expansion: 'Jagged Earth' },
+        },
+        {
+          id: 'Stranded',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Shroud of Silent Mist',
+          },
         },
         {
           id: 'Sun-Bright Whirlwind',
@@ -181,7 +316,25 @@ describe('Root', () => {
           id: "Stone's Unyielding Defiance",
           display: { expansion: 'Jagged Earth' },
         },
+        {
+          id: 'Towering Roots of the Jungle',
+          display: { expansion: 'Nature Incarnate' },
+        },
         { id: 'Thunderspeaker' },
+        {
+          id: 'Tactician',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Thunderspeaker',
+          },
+        },
+        {
+          id: 'Warrior',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Thunderspeaker',
+          },
+        },
         {
           id: 'Vengeance as a Burning Plague',
           display: { expansion: 'Jagged Earth' },
@@ -195,6 +348,13 @@ describe('Root', () => {
           },
         },
         {
+          id: 'Nourishing',
+          display: {
+            expansion: 'Nature Incarnate',
+            aspectOf: 'Vital Strength of the Earth',
+          },
+        },
+        {
           id: 'Resilience',
           display: {
             expansion: 'Jagged Earth',
@@ -204,6 +364,14 @@ describe('Root', () => {
         {
           id: 'Volcano Looming High',
           display: { expansion: 'Jagged Earth' },
+        },
+        {
+          id: 'Wandering Voice Keens Delirium',
+          display: { expansion: 'Nature Incarnate' },
+        },
+        {
+          id: 'Wounded Waters Bleeding',
+          display: { expansion: 'Nature Incarnate' },
         },
       ],
     });
--- libs/spirit-islander/shared/util/src/lib/functions/group-spirits.spec.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/group-spirits.spec.ts
@@ -5,20 +5,26 @@ import { getNames } from './get-names';
 describe('groupSpirits', () => {
   it('returns static spirits sorted into General group', () => {
     expect(
-      groupSpirits([
-        'Bringer of Dreams and Nightmares',
-        'Fractured Days Split the Sky',
-        'Many Minds Move as One',
-      ])
+      groupSpirits(['Fractured Days Split the Sky', 'Many Minds Move as One'])
     ).toEqual({
       General: [
-        { name: 'Bringer of Dreams and Nightmares' },
         { name: 'Fractured Days Split the Sky', expansion: 'Jagged Earth' },
         { name: 'Many Minds Move as One', expansion: 'Jagged Earth' },
       ],
+      'A Spread of Rampant Green': [],
+      'Bringer of Dreams and Nightmares': [],
+      'Heart of the Wildfire': [],
+      'Keeper of the Forbidden Wilds': [],
       "Lightning's Swift Strike": [],
+      'Lure of the Deep Wilderness': [],
+      "Ocean's Hungry Grasp": [],
       'River Surges in Sunlight': [],
+      'Serpent Slumbering Beneath the Island': [],
       'Shadows Flicker Like Flame': [],
+      'Sharp Fangs Behind the Leaves': [],
+      'Shifting Memory of Ages': [],
+      'Shroud of Silent Mist': [],
+      Thunderspeaker: [],
       'Vital Strength of the Earth': [],
     });
   });
@@ -28,6 +34,10 @@ describe('groupSpirits', () => {
       groupSpirits(["Lightning's Swift Strike", 'Immense', 'Pandemonium'])
     ).toEqual({
       General: [],
+      'A Spread of Rampant Green': [],
+      'Bringer of Dreams and Nightmares': [],
+      'Heart of the Wildfire': [],
+      'Keeper of the Forbidden Wilds': [],
       "Lightning's Swift Strike": [
         {
           name: "Lightning's Swift Strike",
@@ -43,112 +53,346 @@ describe('groupSpirits', () => {
           name: 'Pandemonium',
         },
       ],
+      'Lure of the Deep Wilderness': [],
+      "Ocean's Hungry Grasp": [],
       'River Surges in Sunlight': [],
+      'Serpent Slumbering Beneath the Island': [],
       'Shadows Flicker Like Flame': [],
+      'Sharp Fangs Behind the Leaves': [],
+      'Shifting Memory of Ages': [],
+      'Shroud of Silent Mist': [],
+      Thunderspeaker: [],
       'Vital Strength of the Earth': [],
     });
   });
 
   it('returns all possible spirits sorted into correct groups', () => {
     expect(groupSpirits(getNames(SPIRITS))).toEqual({
+      'A Spread of Rampant Green': [
+        {
+          name: 'A Spread of Rampant Green',
+        },
+        {
+          aspectOf: 'A Spread of Rampant Green',
+          expansion: 'Nature Incarnate',
+          name: 'Regrowth',
+        },
+        {
+          aspectOf: 'A Spread of Rampant Green',
+          expansion: 'Nature Incarnate',
+          name: 'Tangles',
+        },
+      ],
+      'Bringer of Dreams and Nightmares': [
+        {
+          name: 'Bringer of Dreams and Nightmares',
+        },
+        {
+          aspectOf: 'Bringer of Dreams and Nightmares',
+          expansion: 'Nature Incarnate',
+          name: 'Enticing',
+        },
+        {
+          aspectOf: 'Bringer of Dreams and Nightmares',
+          expansion: 'Nature Incarnate',
+          name: 'Violence',
+        },
+      ],
       General: [
-        { name: 'A Spread of Rampant Green' },
-        { name: 'Bringer of Dreams and Nightmares' },
-        { name: 'Devouring Teeth Lurk Underfoot', expansion: 'Horizons' },
-        { name: 'Downpour Drenches the World', expansion: 'Promo Pack 2' },
-        { name: 'Eyes Watch From the Trees', expansion: 'Horizons' },
-        { name: 'Fathomless Mud of the Swamp', expansion: 'Horizons' },
-        { name: 'Finder of Paths Unseen', expansion: 'Promo Pack 2' },
-        { name: 'Fractured Days Split the Sky', expansion: 'Jagged Earth' },
         {
-          name: 'Grinning Trickster Stirs Up Trouble',
+          expansion: 'Nature Incarnate',
+          name: 'Breath of Darkness Down Your Spine',
+        },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Dances Up Earthquakes',
+        },
+        {
+          expansion: 'Horizons',
+          name: 'Devouring Teeth Lurk Underfoot',
+        },
+        {
+          expansion: 'Promo Pack 2',
+          name: 'Downpour Drenches the World',
+        },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Ember-Eyed Behemoth',
+        },
+        {
+          expansion: 'Horizons',
+          name: 'Eyes Watch From the Trees',
+        },
+        {
+          expansion: 'Horizons',
+          name: 'Fathomless Mud of the Swamp',
+        },
+        {
+          expansion: 'Promo Pack 2',
+          name: 'Finder of Paths Unseen',
+        },
+        {
           expansion: 'Jagged Earth',
+          name: 'Fractured Days Split the Sky',
         },
-        { name: 'Heart of the Wildfire', expansion: 'Promo Pack 1' },
-        { name: 'Keeper of the Forbidden Wilds', expansion: 'Branch & Claw' },
-        { name: 'Lure of the Deep Wilderness', expansion: 'Jagged Earth' },
-        { name: 'Many Minds Move as One', expansion: 'Jagged Earth' },
-        { name: "Ocean's Hungry Grasp" },
-        { name: 'Rising Heat of Stone and Sand', expansion: 'Horizons' },
         {
-          name: 'Serpent Slumbering Beneath the Island',
-          expansion: 'Promo Pack 1',
+          expansion: 'Jagged Earth',
+          name: 'Grinning Trickster Stirs Up Trouble',
         },
-        { name: 'Sharp Fangs Behind the Leaves', expansion: 'Branch & Claw' },
-        { name: 'Shifting Memory of Ages', expansion: 'Jagged Earth' },
-        { name: 'Shroud of Silent Mist', expansion: 'Jagged Earth' },
-        { name: 'Sun-Bright Whirlwind', expansion: 'Horizons' },
-        { name: 'Starlight Seeks Its Form', expansion: 'Jagged Earth' },
-        { name: "Stone's Unyielding Defiance", expansion: 'Jagged Earth' },
-        { name: 'Thunderspeaker' },
-        { name: 'Vengeance as a Burning Plague', expansion: 'Jagged Earth' },
-        { name: 'Volcano Looming High', expansion: 'Jagged Earth' },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Hearth-Vigil',
+        },
+        {
+          expansion: 'Jagged Earth',
+          name: 'Many Minds Move as One',
+        },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Relentless Gaze of the Sun',
+        },
+        {
+          expansion: 'Horizons',
+          name: 'Rising Heat of Stone and Sand',
+        },
+        {
+          expansion: 'Horizons',
+          name: 'Sun-Bright Whirlwind',
+        },
+        {
+          expansion: 'Jagged Earth',
+          name: 'Starlight Seeks Its Form',
+        },
+        {
+          expansion: 'Jagged Earth',
+          name: "Stone's Unyielding Defiance",
+        },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Towering Roots of the Jungle',
+        },
+        {
+          expansion: 'Jagged Earth',
+          name: 'Vengeance as a Burning Plague',
+        },
+        {
+          expansion: 'Jagged Earth',
+          name: 'Volcano Looming High',
+        },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Wandering Voice Keens Delirium',
+        },
+        {
+          expansion: 'Nature Incarnate',
+          name: 'Wounded Waters Bleeding',
+        },
+      ],
+      'Heart of the Wildfire': [
+        {
+          expansion: 'Promo Pack 1',
+          name: 'Heart of the Wildfire',
+        },
+        {
+          aspectOf: 'Heart of the Wildfire',
+          expansion: 'Nature Incarnate',
+          name: 'Transforming',
+        },
+      ],
+      'Keeper of the Forbidden Wilds': [
+        {
+          expansion: 'Branch & Claw',
+          name: 'Keeper of the Forbidden Wilds',
+        },
+        {
+          aspectOf: 'Keeper of the Forbidden Wilds',
+          expansion: 'Nature Incarnate',
+          name: 'Spreading Hostility',
+        },
       ],
       "Lightning's Swift Strike": [
-        { name: "Lightning's Swift Strike" },
         {
-          name: 'Immense',
+          name: "Lightning's Swift Strike",
+        },
+        {
+          aspectOf: "Lightning's Swift Strike",
           expansion: 'Promo Pack 2',
-          aspectOf: "Lightning's Swift Strike",
+          name: 'Immense',
         },
         {
+          aspectOf: "Lightning's Swift Strike",
+          expansion: 'Jagged Earth',
           name: 'Pandemonium',
-          expansion: 'Jagged Earth',
-          aspectOf: "Lightning's Swift Strike",
         },
         {
-          name: 'Wind',
-          expansion: 'Jagged Earth',
           aspectOf: "Lightning's Swift Strike",
+          expansion: 'Nature Incarnate',
+          name: 'Sparking',
+        },
+        {
+          aspectOf: "Lightning's Swift Strike",
+          expansion: 'Jagged Earth',
+          name: 'Wind',
+        },
+      ],
+      'Lure of the Deep Wilderness': [
+        {
+          expansion: 'Jagged Earth',
+          name: 'Lure of the Deep Wilderness',
+        },
+        {
+          aspectOf: 'Lure of the Deep Wilderness',
+          expansion: 'Nature Incarnate',
+          name: 'Lair',
+        },
+      ],
+      "Ocean's Hungry Grasp": [
+        {
+          name: "Ocean's Hungry Grasp",
+        },
+        {
+          aspectOf: "Ocean's Hungry Grasp",
+          expansion: 'Nature Incarnate',
+          name: 'Deeps',
         },
       ],
       'River Surges in Sunlight': [
-        { name: 'River Surges in Sunlight' },
         {
-          name: 'Sunshine',
-          expansion: 'Jagged Earth',
-          aspectOf: 'River Surges in Sunlight',
+          name: 'River Surges in Sunlight',
         },
         {
-          name: 'Travel',
-          expansion: 'Promo Pack 2',
           aspectOf: 'River Surges in Sunlight',
+          expansion: 'Nature Incarnate',
+          name: 'Haven',
+        },
+        {
+          aspectOf: 'River Surges in Sunlight',
+          expansion: 'Jagged Earth',
+          name: 'Sunshine',
+        },
+        {
+          aspectOf: 'River Surges in Sunlight',
+          expansion: 'Promo Pack 2',
+          name: 'Travel',
+        },
+      ],
+      'Serpent Slumbering Beneath the Island': [
+        {
+          expansion: 'Promo Pack 1',
+          name: 'Serpent Slumbering Beneath the Island',
+        },
+        {
+          aspectOf: 'Serpent Slumbering Beneath the Island',
+          expansion: 'Nature Incarnate',
+          name: 'Locus',
         },
       ],
       'Shadows Flicker Like Flame': [
-        { name: 'Shadows Flicker Like Flame' },
         {
+          name: 'Shadows Flicker Like Flame',
+        },
+        {
+          aspectOf: 'Shadows Flicker Like Flame',
+          expansion: 'Promo Pack 2',
           name: 'Amorphous',
-          expansion: 'Promo Pack 2',
-          aspectOf: 'Shadows Flicker Like Flame',
         },
         {
+          aspectOf: 'Shadows Flicker Like Flame',
+          expansion: 'Nature Incarnate',
+          name: 'Dark Fire',
+        },
+        {
+          aspectOf: 'Shadows Flicker Like Flame',
+          expansion: 'Promo Pack 2',
           name: 'Foreboding',
-          expansion: 'Promo Pack 2',
-          aspectOf: 'Shadows Flicker Like Flame',
         },
         {
+          aspectOf: 'Shadows Flicker Like Flame',
+          expansion: 'Jagged Earth',
           name: 'Madness',
-          expansion: 'Jagged Earth',
-          aspectOf: 'Shadows Flicker Like Flame',
         },
         {
-          name: 'Reach',
-          expansion: 'Jagged Earth',
           aspectOf: 'Shadows Flicker Like Flame',
+          expansion: 'Jagged Earth',
+          name: 'Reach',
+        },
+      ],
+      'Sharp Fangs Behind the Leaves': [
+        {
+          expansion: 'Branch & Claw',
+          name: 'Sharp Fangs Behind the Leaves',
+        },
+        {
+          aspectOf: 'Sharp Fangs Behind the Leaves',
+          expansion: 'Nature Incarnate',
+          name: 'Encircle',
+        },
+        {
+          aspectOf: 'Sharp Fangs Behind the Leaves',
+          expansion: 'Nature Incarnate',
+          name: 'Unconstrained',
+        },
+      ],
+      'Shifting Memory of Ages': [
+        {
+          expansion: 'Jagged Earth',
+          name: 'Shifting Memory of Ages',
+        },
+        {
+          aspectOf: 'Shifting Memory of Ages',
+          expansion: 'Nature Incarnate',
+          name: 'Intensity',
+        },
+        {
+          aspectOf: 'Shifting Memory of Ages',
+          expansion: 'Nature Incarnate',
+          name: 'Mentor',
+        },
+      ],
+      'Shroud of Silent Mist': [
+        {
+          expansion: 'Jagged Earth',
+          name: 'Shroud of Silent Mist',
+        },
+        {
+          aspectOf: 'Shroud of Silent Mist',
+          expansion: 'Nature Incarnate',
+          name: 'Stranded',
+        },
+      ],
+      Thunderspeaker: [
+        {
+          name: 'Thunderspeaker',
+        },
+        {
+          aspectOf: 'Thunderspeaker',
+          expansion: 'Nature Incarnate',
+          name: 'Tactician',
+        },
+        {
+          aspectOf: 'Thunderspeaker',
+          expansion: 'Nature Incarnate',
+          name: 'Warrior',
         },
       ],
       'Vital Strength of the Earth': [
-        { name: 'Vital Strength of the Earth' },
         {
-          name: 'Might',
-          expansion: 'Promo Pack 2',
-          aspectOf: 'Vital Strength of the Earth',
+          name: 'Vital Strength of the Earth',
         },
         {
-          name: 'Resilience',
-          expansion: 'Jagged Earth',
           aspectOf: 'Vital Strength of the Earth',
+          expansion: 'Promo Pack 2',
+          name: 'Might',
+        },
+        {
+          aspectOf: 'Vital Strength of the Earth',
+          expansion: 'Nature Incarnate',
+          name: 'Nourishing',
+        },
+        {
+          aspectOf: 'Vital Strength of the Earth',
+          expansion: 'Jagged Earth',
+          name: 'Resilience',
         },
       ],
     });
