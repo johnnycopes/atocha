4667750383d60baafd8b603398d764d621026cb2
John Coppola <jdcoppola@gmail.com>
2023-11-26 09:36:55 -0500

    TEST: additional test case

--- libs/spirit-islander/shared/util/src/lib/functions/get-spirits-by-expansion.spec.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-spirits-by-expansion.spec.ts
@@ -1,17 +1,32 @@
 import { getSpiritsByExpansion } from './get-spirits-by-expansion';
 
 describe('getSpiritsByExpansion', () => {
+  it('returns spirits from base expansions no matter what', () => {
+    expect(
+      getSpiritsByExpansion(
+        [
+          { name: 'Finder of Paths Unseen', expansion: 'Promo Pack 2' },
+          { name: "Ocean's Hungry Grasp" },
+          { name: 'Thunderspeaker' },
+        ],
+        []
+      )
+    ).toEqual([{ name: "Ocean's Hungry Grasp" }, { name: 'Thunderspeaker' }]);
+  });
+
   it('returns spirits from certain expansions', () => {
     expect(
       getSpiritsByExpansion(
         [
           { name: 'A Spread of Rampant Green', expansion: 'Branch & Claw' },
+          { name: 'Bringer of Dreams and Nightmares' },
           { name: "Stone's Unyielding Defiance", expansion: 'Jagged Earth' },
         ],
         ['Branch & Claw']
       )
     ).toEqual([
       { name: 'A Spread of Rampant Green', expansion: 'Branch & Claw' },
+      { name: 'Bringer of Dreams and Nightmares' },
     ]);
   });
 });
