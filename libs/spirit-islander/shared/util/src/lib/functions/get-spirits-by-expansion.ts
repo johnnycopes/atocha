ca479e27a2a42e65dd1aec41e12a43e706e5cfc8
John Coppola <jdcoppola@gmail.com>
2023-11-26 09:38:00 -0500

    FEATURE: passes in type arguments to getOptionsByName

--- libs/spirit-islander/shared/util/src/lib/functions/get-spirits-by-expansion.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-spirits-by-expansion.ts
@@ -1,9 +1,9 @@
-import { Expansion, Spirit } from '../types';
+import { Expansion, Spirit, SpiritName } from '../types';
 import { getOptionsByExpansion } from './get-options-by-expansion';
 
 export function getSpiritsByExpansion(
   spirits: readonly Spirit[],
   expansions: readonly Expansion[]
 ): readonly Spirit[] {
-  return getOptionsByExpansion(spirits, expansions);
+  return getOptionsByExpansion<SpiritName, Spirit>(spirits, expansions);
 }
