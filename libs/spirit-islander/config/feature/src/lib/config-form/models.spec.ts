fd64f0c7e2f662b352fd94d4eb17a45c8988dd9f
John Coppola <jdcoppola@gmail.com>
2023-11-26 12:17:26 -0500

    TEST: adds new describe block and test to Models class for pre-exisitng logic

--- libs/spirit-islander/config/feature/src/lib/config-form/models.spec.ts
+++ libs/spirit-islander/config/feature/src/lib/config-form/models.spec.ts
@@ -246,4 +246,16 @@ describe('Models', () => {
       ]);
     });
   });
+
+  describe('adjusting aspects', () => {
+    it('removes aspect names from expansions while leaving base game spirit name alone', () => {
+      const models = new Models({
+        spiritNames: ["Lightning's Swift Strike", 'Immense'],
+      });
+
+      expect(models.update([], 'Promo Pack 2').spiritNames).toEqual([
+        "Lightning's Swift Strike",
+      ]);
+    });
+  });
 });
