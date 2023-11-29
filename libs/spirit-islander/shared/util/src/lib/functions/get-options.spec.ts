27dd4469c19c7a6305d4661c54392f816c89ecc0
John Coppola <jdcoppola@gmail.com>
2023-11-29 16:21:00 -0500

    TEST: updates getOptions spec into multiple describe blocks

--- libs/spirit-islander/shared/util/src/lib/functions/get-options.spec.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-options.spec.ts
@@ -1,7 +1,7 @@
 import { getOptions } from './get-options';
 import { Board, Spirit } from '../types';
 
-describe('getOptionsByExpansion', () => {
+describe('getOptions', () => {
   let mockSpirits: readonly Spirit[];
   let mockBoards: readonly Board[];
 
@@ -22,44 +22,51 @@ describe('getOptionsByExpansion', () => {
     ];
   });
 
-  it('returns options unchanged if expansions argument is omitted', () => {
-    expect(getOptions(mockSpirits)).toStrictEqual(mockSpirits);
+  describe('with no filters applied', () => {
+    it('returns spirits untouched if filters argument is omitted', () => {
+      expect(getOptions(mockSpirits)).toStrictEqual(mockSpirits);
+    });
+
+    it('returns boards untouched if filters argument is omitted', () => {
+      expect(getOptions(mockBoards)).toStrictEqual(mockBoards);
+    });
   });
 
-  it('get spirits by expansion name', () => {
-    expect(getOptions(mockSpirits, { expansions: [] })).toStrictEqual([
-      { name: 'Bringer of Dreams and Nightmares' },
-    ]);
+  describe('with expansions filter applied', () => {
+    it('returns base game spirits when array is empty', () => {
+      expect(getOptions(mockSpirits, { expansions: [] })).toStrictEqual([
+        { name: 'Bringer of Dreams and Nightmares' },
+      ]);
+    });
 
-    expect(
-      getOptions(mockSpirits, { expansions: ['Branch & Claw'] })
-    ).toStrictEqual([
-      { name: 'Bringer of Dreams and Nightmares' },
-      { name: 'Keeper of the Forbidden Wilds', expansion: 'Branch & Claw' },
-    ]);
+    it('returns base game + expansion spirits when expansions are specified', () => {
+      expect(
+        getOptions(mockSpirits, {
+          expansions: ['Promo Pack 1', 'Promo Pack 2'],
+        })
+      ).toStrictEqual([
+        { name: 'Bringer of Dreams and Nightmares' },
+        { name: 'Downpour Drenches the World', expansion: 'Promo Pack 2' },
+        {
+          name: 'Serpent Slumbering Beneath the Island',
+          expansion: 'Promo Pack 1',
+        },
+      ]);
+    });
 
-    expect(
-      getOptions(mockSpirits, { expansions: ['Promo Pack 1', 'Promo Pack 2'] })
-    ).toStrictEqual([
-      { name: 'Bringer of Dreams and Nightmares' },
-      { name: 'Downpour Drenches the World', expansion: 'Promo Pack 2' },
-      {
-        name: 'Serpent Slumbering Beneath the Island',
-        expansion: 'Promo Pack 1',
-      },
-    ]);
-  });
+    it('returns base game boards when array is empty', () => {
+      expect(getOptions(mockBoards, { expansions: [] })).toStrictEqual([
+        { name: 'D', thematicName: 'West' },
+      ]);
+    });
 
-  it('gets boards by expansion name', () => {
-    expect(getOptions(mockBoards, { expansions: [] })).toStrictEqual([
-      { name: 'D', thematicName: 'West' },
-    ]);
-
-    expect(
-      getOptions(mockBoards, { expansions: ['Jagged Earth'] })
-    ).toStrictEqual([
-      { name: 'D', thematicName: 'West' },
-      { name: 'E', thematicName: 'Southeast', expansion: 'Jagged Earth' },
-    ]);
+    it('returns base game + expansion boards when expansions are specified', () => {
+      expect(
+        getOptions(mockBoards, { expansions: ['Jagged Earth'] })
+      ).toStrictEqual([
+        { name: 'D', thematicName: 'West' },
+        { name: 'E', thematicName: 'Southeast', expansion: 'Jagged Earth' },
+      ]);
+    });
   });
 });
