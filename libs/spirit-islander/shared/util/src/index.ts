39b586d03b4b7b045a1aaaba27367998181a057e
John Coppola <jdcoppola@gmail.com>
2023-11-29 15:55:38 -0500

    FEATURE: implements getPlayers

--- libs/spirit-islander/shared/util/src/index.ts
+++ libs/spirit-islander/shared/util/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/game';
 export * from './lib/functions';
 export * from './lib/types';
--- libs/spirit-islander/shared/util/src/lib/game.ts
+++
@@ -1,4 +0,0 @@
-import { PLAYERS as PLAYERS_DATA } from './data';
-import { Players } from './types';
-
-export const PLAYERS: readonly Players[] = PLAYERS_DATA;
