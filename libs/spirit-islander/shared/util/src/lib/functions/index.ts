b643b238ce8fde61034b40f1edf20f8aef9a552d
John Coppola <jdcoppola@gmail.com>
2023-11-29 08:26:06 -0500

    FEATURE: exports getMaps

--- libs/spirit-islander/shared/util/src/lib/functions/index.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/index.ts
@@ -1,6 +1,7 @@
 export * from './get-adversary-level-ids';
 export * from './get-boards';
 export * from './get-difficulty';
+export * from './get-maps';
 export * from './get-names';
 export * from './get-options';
 export * from './get-options-by-name';
