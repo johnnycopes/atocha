9d900ee4567fff5e67c8157d7cd889a8636c9424
John Coppola <jdcoppola@gmail.com>
2023-11-29 08:11:32 -0500

    OOPS: makes expansions argument readonly

--- libs/spirit-islander/shared/util/src/lib/functions/get-boards.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-boards.ts
@@ -2,6 +2,6 @@ import { BOARDS } from '../data';
 import { Board, Expansion } from '../types';
 import { getOptions } from './get-options';
 
-export function getBoards(expansions?: Expansion[]): readonly Board[] {
+export function getBoards(expansions?: readonly Expansion[]): readonly Board[] {
   return getOptions(BOARDS, expansions);
 }
