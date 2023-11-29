707432cef85b23bb13b3dc378356d66c210685c5
John Coppola <jdcoppola@gmail.com>
2023-11-29 08:03:29 -0500

    FEATURE: deletes SPIRITS export from game.ts

--- libs/spirit-islander/shared/util/src/lib/game.ts
+++ libs/spirit-islander/shared/util/src/lib/game.ts
@@ -2,7 +2,6 @@ import {
   EXPANSIONS as EXPANSIONS_DATA,
   DIFFICULTIES as DIFFICULTIES_DATA,
   PLAYERS as PLAYERS_DATA,
-  SPIRITS as SPIRITS_DATA,
   BOARDS as BOARDS_DATA,
   MAPS as MAPS_DATA,
   SCENARIOS as SCENARIOS_DATA,
@@ -12,7 +11,6 @@ import {
   Expansion,
   Difficulty,
   Players,
-  Spirit,
   Map,
   Board,
   Scenario,
@@ -22,7 +20,6 @@ import {
 export const EXPANSIONS: readonly Expansion[] = EXPANSIONS_DATA;
 export const DIFFICULTIES: readonly Difficulty[] = DIFFICULTIES_DATA;
 export const PLAYERS: readonly Players[] = PLAYERS_DATA;
-export const SPIRITS: readonly Spirit[] = SPIRITS_DATA;
 export const BOARDS: readonly Board[] = BOARDS_DATA;
 export const MAPS: readonly Map[] = MAPS_DATA;
 export const SCENARIOS: readonly Scenario[] = SCENARIOS_DATA;
