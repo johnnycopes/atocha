8342ec8050918a912ad551c19e858aeaee39982d
John Coppola <jdcoppola@gmail.com>
2023-11-30 06:07:18 -0500

    FEATURE: fleshes out getOptionsFactory body

--- libs/spirit-islander/shared/util/src/lib/functions/get-options-factory.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-options-factory.ts
@@ -1,38 +1,34 @@
-import { ExpansionOption, Filters } from '../types';
+import { Expansion, ExpansionOption } from '../types';
 
 export type GetOptions<
   TName extends string,
   TOption extends ExpansionOption<TName>
-> = (
-  options: readonly TOption[],
-  { expansions, names }: Filters<TName>
-) => readonly TOption[];
+> = ({ expansions, names }: Filters<TName>) => readonly TOption[];
+
+interface Filters<TName extends string> {
+  names?: readonly TName[];
+  expansions?: readonly Expansion[];
+}
 
 export function getOptionsFactory<
   TName extends string,
   TOption extends ExpansionOption<TName>
->(): GetOptions<TName, TOption> {
-  return function getOptions(
-    options: readonly TOption[],
-    { expansions, names }: Filters<TName> = {}
-  ): readonly TOption[] {
-    if (expansions && names) {
-      throw new Error(
-        'Options can only be filtered by expansions OR names (not both at once)'
-      );
-    }
-
-    if (expansions && !names) {
-      return options.filter((item) => {
-        if (item.expansion) {
-          return expansions.includes(item.expansion);
-        } else {
-          return true;
-        }
-      });
-    }
-
-    if (names && !expansions) {
+>(
+  options: readonly TOption[],
+  {
+    filterNames,
+    filterExpansions,
+  }: {
+    filterNames: (
+      options: readonly TOption[],
+      names: readonly TName[]
+    ) => readonly TOption[];
+    filterExpansions: (
+      options: readonly TOption[],
+      expansions: readonly Expansion[]
+    ) => readonly TOption[];
+  } = {
+    filterNames: (options, names) => {
       const filteredOptions: TOption[] = [];
 
       for (const name of names) {
@@ -43,6 +39,34 @@ export function getOptionsFactory<
       }
 
       return filteredOptions;
+    },
+    filterExpansions: (options, expansions) => {
+      return options.filter((item) => {
+        if (item.expansion) {
+          return expansions.includes(item.expansion);
+        } else {
+          return true;
+        }
+      });
+    },
+  }
+): GetOptions<TName, TOption> {
+  return function getOptions({
+    expansions,
+    names,
+  }: Filters<TName> = {}): readonly TOption[] {
+    if (expansions && names) {
+      throw new Error(
+        'Options can only be filtered by expansions OR names (not both at once)'
+      );
+    }
+
+    if (expansions && !names) {
+      return filterExpansions(options, expansions);
+    }
+
+    if (names && !expansions) {
+      return filterNames(options, names);
     }
 
     return options;
