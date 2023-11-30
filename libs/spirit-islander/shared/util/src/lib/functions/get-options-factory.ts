590da3de715ba2f54480a23e8edddbd9632a6bac
John Coppola <jdcoppola@gmail.com>
2023-11-30 05:51:43 -0500

    FEATURE: inlines existing getOptions function definition

--- libs/spirit-islander/shared/util/src/lib/functions/get-options-factory.ts
+++ libs/spirit-islander/shared/util/src/lib/functions/get-options-factory.ts
@@ -1,5 +1,4 @@
 import { ExpansionOption, Filters } from '../types';
-import { getOptions } from './get-options';
 
 export type GetOptions<
   TName extends string,
@@ -13,5 +12,39 @@ export function getOptionsFactory<
   TName extends string,
   TOption extends ExpansionOption<TName>
 >(): GetOptions<TName, TOption> {
-  return getOptions;
+  return function getOptions(
+    options: readonly TOption[],
+    { expansions, names }: Filters<TName> = {}
+  ): readonly TOption[] {
+    if (expansions && names) {
+      throw new Error(
+        'Options can only be filtered by expansions OR names (not both at once)'
+      );
+    }
+
+    if (expansions && !names) {
+      return options.filter((item) => {
+        if (item.expansion) {
+          return expansions.includes(item.expansion);
+        } else {
+          return true;
+        }
+      });
+    }
+
+    if (names && !expansions) {
+      const filteredOptions: TOption[] = [];
+
+      for (const name of names) {
+        const foundOption = options.find((option) => option.name === name);
+        if (foundOption) {
+          filteredOptions.push(foundOption);
+        }
+      }
+
+      return filteredOptions;
+    }
+
+    return options;
+  };
 }
