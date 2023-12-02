81b91e9bf64c2aa5eb2ef676021b661b919ef590
John Coppola <jdcoppola@gmail.com>
2023-12-02 10:18:45 -0500

    FEATURE: deletes boilerplate component and writes basic SettingsComponent

--- libs/spirit-islander/settings/feature/src/index.ts
+++ libs/spirit-islander/settings/feature/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/spirit-islander-settings-feature/spirit-islander-settings-feature.component';
+export * from './settings.component';
---
+++ libs/spirit-islander/settings/feature/src/settings.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-settings',
+  imports: [],
+  template: ` <p>This page under construction</p> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SettingsComponent {}
