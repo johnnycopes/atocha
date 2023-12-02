292a3f5c0bd8632b532bb627b379825a87e376f0
John Coppola <jdcoppola@gmail.com>
2023-12-02 10:26:38 -0500

    FEATURE: fleshes out settings page contents

--- libs/spirit-islander/settings/feature/src/settings.component.ts
+++ libs/spirit-islander/settings/feature/src/settings.component.ts
@@ -1,10 +1,35 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ButtonComponent } from '@atocha/core/ui';
+import { AppFacadeService } from '@atocha/spirit-islander/shared/data-access';
 
 @Component({
   standalone: true,
   selector: 'app-settings',
-  imports: [],
-  template: ` <p>This page under construction</p> `,
+  imports: [ButtonComponent],
+  template: `
+    <p>The /settings page is currently under construction 🚧</p>
+    <button core-button (click)="goToConfigPage()">Home</button>
+  `,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      }
+
+      button {
+        margin-top: 32px;
+      }
+    `,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SettingsComponent {}
+export class SettingsComponent {
+  constructor(private _appFacadeService: AppFacadeService) {}
+
+  goToConfigPage() {
+    this._appFacadeService.navigateToConfig();
+  }
+}
