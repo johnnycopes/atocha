ba2f600f7f30f2b664ab10e548cd5c2720beab62
John Coppola <jdcoppola@gmail.com>
2023-12-02 23:28:25 -0500

    FEATURE: nav bar with links

--- libs/spirit-islander/shell/feature/src/lib/header/header.component.ts
+++ libs/spirit-islander/shell/feature/src/lib/header/header.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { ExternalLinkDirective } from '@atocha/core/ui';
+import { Route } from '@atocha/spirit-islander/shared/data-access';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'header[app-header]',
   standalone: true,
-  imports: [CommonModule, ExternalLinkDirective],
+  imports: [CommonModule, ExternalLinkDirective, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  readonly Route: typeof Route = Route;
+}
