b002931349d087c1ab8f734189e61484ef016404
John Coppola <jdcoppola@gmail.com>
2023-12-03 00:12:24 -0500

    FEATURE: forcibly routes to Game Setup page

--- libs/spirit-islander/shell/feature/src/lib/header/header.component.ts
+++ libs/spirit-islander/shell/feature/src/lib/header/header.component.ts
@@ -3,7 +3,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ExternalLinkDirective } from '@atocha/core/ui';
-import { Route } from '@atocha/spirit-islander/shared/data-access';
+import {
+  AppFacadeService,
+  Route,
+} from '@atocha/spirit-islander/shared/data-access';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { mapConfigToParams } from 'libs/spirit-islander/shared/data-access/src/lib/internal/url-mappers';
+import { map, tap } from 'rxjs';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -16,4 +22,10 @@ import { Route } from '@atocha/spirit-islander/shared/data-access';
 })
 export class HeaderComponent {
   readonly Route: typeof Route = Route;
+  config$ = this._appFacadeService.state$.pipe(
+    map(({ config }) => mapConfigToParams(config)),
+    tap(console.log)
+  );
+
+  constructor(private _appFacadeService: AppFacadeService) {}
 }
