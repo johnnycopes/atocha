6301b0b847b60e46245390b928bf9ef19b9d9e97
John Coppola <jdcoppola@gmail.com>
2023-12-05 06:28:51 -0500

    FEATURE: removes unreferenced functionality from AppFacadeService

--- libs/spirit-islander/shared/data-access/src/lib/app-facade.service.ts
+++ libs/spirit-islander/shared/data-access/src/lib/app-facade.service.ts
@@ -1,56 +1,15 @@
 import { Injectable } from '@angular/core';
-import { ParamMap, Router } from '@angular/router';
-import { map } from 'rxjs';
 
-import { Config } from '@atocha/spirit-islander/config/util';
 import { Settings } from '@atocha/spirit-islander/settings/util';
-import { Route } from './route.enum';
 import { AppStateService } from './internal/app-state.service';
-import { mapConfigToParams, mapParamsToConfig } from './internal/url-mappers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AppFacadeService {
   state$ = this._appStateService.state$;
-  configParams$ = this._appStateService.state$.pipe(
-    map(({ config }) => mapConfigToParams(config))
-  );
 
-  constructor(
-    private _appStateService: AppStateService,
-    private _router: Router
-  ) {}
-
-  async navigateToHome(): Promise<void> {
-    await this._router.navigate([Route.home]);
-  }
-
-  async navigateToGameSetup(config: Config): Promise<void> {
-    await this._router.navigate([Route.gameSetup], {
-      queryParams: mapConfigToParams(config),
-    });
-  }
-
-  async navigateToConfig(): Promise<void> {
-    await this._router.navigate([Route.config]);
-  }
-
-  async navigateToError(): Promise<void> {
-    await this._router.navigate([Route.error], {
-      skipLocationChange: true,
-      queryParamsHandling: 'preserve',
-    });
-  }
-
-  async processParams(params: ParamMap): Promise<void> {
-    try {
-      const config = mapParamsToConfig(params);
-      this._appStateService.updateConfig(config);
-    } catch {
-      await this.navigateToError();
-    }
-  }
+  constructor(private _appStateService: AppStateService) {}
 
   updateSettings(changes: Partial<Settings>): void {
     this._appStateService.updateSettings(changes);
