0c56319c5b34d5c60316366c4a95e7b1385dfd9f
John Coppola <jdcoppola@gmail.com>
2023-12-05 06:21:21 -0500

    MANUAL: copies routing functionality from AppFacadeService to RouterService

--- libs/spirit-islander/shared/data-access/src/lib/router.service.ts
+++ libs/spirit-islander/shared/data-access/src/lib/router.service.ts
@@ -1,6 +1,52 @@
 import { Injectable } from '@angular/core';
+import { ParamMap, Router } from '@angular/router';
+import { map } from 'rxjs';
+
+import { Config } from '@atocha/spirit-islander/config/util';
+import { AppStateService } from './internal/app-state.service';
+import { mapConfigToParams, mapParamsToConfig } from './internal/url-mappers';
+import { Route } from './route.enum';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RouterService {}
+export class RouterService {
+  configParams$ = this._appStateService.state$.pipe(
+    map(({ config }) => mapConfigToParams(config))
+  );
+
+  constructor(
+    private _appStateService: AppStateService,
+    private _router: Router
+  ) {}
+
+  async navigateToHome(): Promise<void> {
+    await this._router.navigate([Route.home]);
+  }
+
+  async navigateToGameSetup(config: Config): Promise<void> {
+    await this._router.navigate([Route.gameSetup], {
+      queryParams: mapConfigToParams(config),
+    });
+  }
+
+  async navigateToConfig(): Promise<void> {
+    await this._router.navigate([Route.config]);
+  }
+
+  async navigateToError(): Promise<void> {
+    await this._router.navigate([Route.error], {
+      skipLocationChange: true,
+      queryParamsHandling: 'preserve',
+    });
+  }
+
+  async processParams(params: ParamMap): Promise<void> {
+    try {
+      const config = mapParamsToConfig(params);
+      this._appStateService.updateConfig(config);
+    } catch {
+      await this.navigateToError();
+    }
+  }
+}
