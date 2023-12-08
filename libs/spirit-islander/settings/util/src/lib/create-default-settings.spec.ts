b140a2a17e8e7b35361e205da7b36a2eb60500b8
John Coppola <jdcoppola@gmail.com>
2023-12-08 09:02:55 -0500

    TEST: consistent property order

--- libs/spirit-islander/settings/util/src/lib/create-default-settings.spec.ts
+++ libs/spirit-islander/settings/util/src/lib/create-default-settings.spec.ts
@@ -3,8 +3,8 @@ import { createDefaultSettings } from './create-default-settings';
 describe('createDefaultSettings', () => {
   it('returns settings object with correct default values', () => {
     expect(createDefaultSettings()).toEqual({
-      allowBEAndDFBoards: true,
       randomThematicBoards: false,
+      allowBEAndDFBoards: true,
     });
   });
 });
