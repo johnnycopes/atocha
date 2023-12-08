034a90f4f5fefd187ea8f3aaed12f0868977fab8
John Coppola <jdcoppola@gmail.com>
2023-12-08 07:50:58 -0500

    FEATURE: adds readonly modifiers to incoming and outgoing state objects

--- libs/spirit-islander/shared/data-access/src/lib/internal/update-settings.ts
+++ libs/spirit-islander/shared/data-access/src/lib/internal/update-settings.ts
@@ -3,8 +3,8 @@ import { AppState } from './app-state.interface';
 
 export function updateSettings(
   changes: Partial<Settings>,
-  state: AppState
-): AppState {
+  state: Readonly<AppState>
+): Readonly<AppState> {
   return {
     ...state,
     settings: {
