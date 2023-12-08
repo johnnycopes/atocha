a75307cd6d72010ab5b24a8922f002350e2ade07
John Coppola <jdcoppola@gmail.com>
2023-12-08 08:40:44 -0500

    fFEATURE: updates function to use boolean rather than StateService

--- libs/spirit-islander/config/feature/src/lib/config-form/validators.ts
+++ libs/spirit-islander/config/feature/src/lib/config-form/validators.ts
@@ -95,27 +95,21 @@ export function restrictedBoardPairings(
   allowBEAndDFBoards: Settings['allowBEAndDFBoards']
 ): ValidatorFn {
   return (control: AbstractControl<Config>): ValidationErrors | null => {
-    let isError = false;
+    if (allowBEAndDFBoards || control.value.players !== 2) return null;
 
-    stateService.settings$.pipe(first()).subscribe(({ allowBEAndDFBoards }) => {
-      if (allowBEAndDFBoards || control.value.players !== 2) return;
+    const boards = control.value.boardNames;
+    if (boards.length !== 2) return null;
 
-      const boards = control.value.boardNames;
-      if (boards.length !== 2) return;
+    if (
+      (boards.includes('B') && boards.includes('E')) ||
+      (boards.includes('D') && boards.includes('F'))
+    ) {
+      return {
+        restrictedBoardPairings:
+          'Boards B / E and boards D / F not allowed in a 2 player game',
+      };
+    }
 
-      if (
-        (boards.includes('B') && boards.includes('E')) ||
-        (boards.includes('D') && boards.includes('F'))
-      ) {
-        isError = true;
-      }
-    });
-
-    return isError
-      ? {
-          restrictedBoardPairings:
-            'Boards B / E and boards D / F not allowed in a 2 player game',
-        }
-      : null;
+    return null;
   };
 }
