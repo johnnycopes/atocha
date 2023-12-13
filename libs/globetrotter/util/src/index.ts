59fc4d0cc468bb83b13f8a7dc2f4e20853a77aae
John Coppola <jdcoppola@gmail.com>
2023-12-13 06:38:43 -0500

    FILE: deletes Duration enum

--- libs/globetrotter/util/src/index.ts
+++ libs/globetrotter/util/src/index.ts
@@ -9,7 +9,6 @@ export * from './lib/domain/region.interface';
 export * from './lib/domain/subregion.interface';
 export * from './lib/domain/summary.interface';
 
-export * from './lib/duration.enum';
 export * from './lib/duration';
 export * from './lib/quiz-type.enum';
 export * from './lib/route.enum';
