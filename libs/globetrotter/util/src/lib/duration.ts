7d6306a48357644f57ddcf87ee9b984bb1613b52
John Coppola <jdcoppola@gmail.com>
2023-12-13 06:37:12 -0500

    FEATURE: updates DURATION to use 'as const'

--- libs/globetrotter/util/src/lib/duration.ts
+++ libs/globetrotter/util/src/lib/duration.ts
@@ -1,8 +1,8 @@
-export const DURATION: Readonly<Record<string, number>> = {
+export const DURATION = {
   cardAnimation: 300,
   cardFlipDisplay: 1000,
   quizUpdateDelay: 100,
   fadeIn: 300,
   position: 500,
   stagger: 100,
-};
+} as const;
