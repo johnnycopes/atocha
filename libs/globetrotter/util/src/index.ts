3ddafa6c5abf27a82e941c87541015be77120271
John Coppola <jdcoppola@gmail.com>
2023-12-13 06:35:16 -0500

    FILE: creates new duration const and exports it

--- libs/globetrotter/util/src/index.ts
+++ libs/globetrotter/util/src/index.ts
@@ -10,6 +10,7 @@ export * from './lib/domain/subregion.interface';
 export * from './lib/domain/summary.interface';
 
 export * from './lib/duration.enum';
+export * from './lib/duration';
 export * from './lib/quiz-type.enum';
 export * from './lib/route.enum';
 export * from './lib/selection.interface';
---
+++ libs/globetrotter/util/src/lib/duration.ts
@@ -0,0 +1,8 @@
+export const DURATION: Readonly<Record<string, number>> = {
+  cardAnimation: 300,
+  cardFlipDisplay: 1000,
+  quizUpdateDelay: 100,
+  fadeIn: 300,
+  position: 500,
+  stagger: 100,
+};
