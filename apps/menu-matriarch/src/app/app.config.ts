b248c28568f3ce6a0d506aeb383b761c5a023c95
John Coppola <jdcoppola@gmail.com>
2023-12-16 08:22:45 -0800

    MANUAL: import order

--- apps/menu-matriarch/src/app/app.config.ts
+++ apps/menu-matriarch/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
