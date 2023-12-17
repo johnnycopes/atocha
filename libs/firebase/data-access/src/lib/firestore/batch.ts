03a4f9af92698abd0a1324634c3233f69b1291f9
John Coppola <jdcoppola@gmail.com>
2023-12-17 10:36:44 -0800

    Update Firebase packages (#493)
    * TOOL: updates @angular/fire, firebase, and firebase-tools packages
    
    * FEATURE: updates typing in Batch class and FirestoreService after package updates
    
    * TOOL: updates rxfire to 6.0.5
    
    * TOOL: updates firebase-admin package
    
    * TEST: adds TextDecoder fix from Github issue thread to fix breaking tests
    
    * TEST: missed adding fix to one test

--- libs/firebase/data-access/src/lib/firestore/batch.ts
+++ libs/firebase/data-access/src/lib/firestore/batch.ts
@@ -1,4 +1,8 @@
-import { DocumentReference, WriteBatch } from '@angular/fire/firestore';
+import {
+  DocumentReference,
+  FieldValue,
+  WriteBatch,
+} from '@angular/fire/firestore';
 
 export interface BatchSet<T> {
   endpoint: string;
@@ -6,7 +10,9 @@ export interface BatchSet<T> {
   data: T;
 }
 
-export type BatchUpdate = BatchSet<{ [fieldPath: string]: unknown }>;
+export type BatchUpdate = BatchSet<{
+  [x: string]: FieldValue | Partial<unknown> | undefined;
+}>;
 
 export class Batch {
   constructor(
@@ -19,7 +25,7 @@ export class Batch {
 
   set<T>({ endpoint, id, data }: BatchSet<T>): Batch {
     const docRef = this._getDocRef<T>(endpoint, id);
-    this._batch.set<T>(docRef, data);
+    this._batch.set(docRef, data);
     return this;
   }
 
@@ -28,19 +34,19 @@ export class Batch {
     return this;
   }
 
-  update({ endpoint, id, data }: BatchUpdate): Batch {
-    const docRef = this._getDocRef(endpoint, id);
+  update<T>({ endpoint, id, data }: BatchUpdate): Batch {
+    const docRef = this._getDocRef<T>(endpoint, id);
     this._batch.update(docRef, data);
     return this;
   }
 
-  updateMultiple(updates: BatchUpdate[]): Batch {
-    updates.forEach((update) => this.update(update));
+  updateMultiple<T>(updates: BatchUpdate[]): Batch {
+    updates.forEach((update) => this.update<T>(update));
     return this;
   }
 
-  delete(endpoint: string, id: string): Batch {
-    const docRef = this._getDocRef(endpoint, id);
+  delete<T>(endpoint: string, id: string): Batch {
+    const docRef = this._getDocRef<T>(endpoint, id);
     this._batch.delete(docRef);
     return this;
   }
