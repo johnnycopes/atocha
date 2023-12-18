1c51e8b78734869685afc23f76d985b5e31ba4ba
John Coppola <jdcoppola@gmail.com>
2023-12-17 19:23:59 -0800

    FEATURE: writes new logic for groupBy

--- libs/globetrotter/data-access/src/lib/services/internal/group-by.ts
+++ libs/globetrotter/data-access/src/lib/services/internal/group-by.ts
@@ -1,3 +1,18 @@
-import { groupBy as libGroupBy } from 'lodash-es';
+export function groupBy<T extends object, K extends keyof T>(
+  collection: T[],
+  key: K
+): Record<string, T[]> {
+  const record: Record<string, T[]> = {};
 
-export const groupBy = libGroupBy;
+  for (const item of collection) {
+    const result = item[key];
+    if (typeof result !== 'string') {
+      throw new Error('Key must be associated with value of type string');
+    }
+
+    if (!(result in record)) record[result] = [];
+    record[result].push(item);
+  }
+
+  return record;
+}
