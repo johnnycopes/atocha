d2a1a73a20d07d3e5a4da2c7e5616cc7cde40992
John Coppola <jdcoppola@gmail.com>
2023-12-18 15:21:56 -0800

    FILE: defines shuffle function that uses Durstenfeld's algorithm

--- libs/globetrotter/util/src/lib/shuffle.ts
+++ libs/globetrotter/util/src/lib/shuffle.ts
@@ -1,3 +1,9 @@
-export function shuffle<T>(items: T[]): T[] {
-  return items.slice();
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+export function shuffle<T>(arr: T[]): void {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 }
