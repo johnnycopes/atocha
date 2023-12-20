4c41384f94a5bcb6c347cb039471c84f081fef76
John Coppola <jdcoppola@gmail.com>
2023-12-20 12:09:38 -0800

    FEATURE: rewrites (unused) getOne method of ingredientTypeService to only fetch its associated ingredients rather than all ingredients

--- libs/menu-matriarch/ingredients/data-access/src/lib/ingredient-type.service.ts
+++ libs/menu-matriarch/ingredients/data-access/src/lib/ingredient-type.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, combineLatest, of } from 'rxjs';
-import { concatMap, first, map } from 'rxjs/operators';
+import { concatMap, first, map, switchMap } from 'rxjs/operators';
 
 import { AuthService } from '@atocha/firebase/data-access';
 import { IEntityService } from '@atocha/menu-matriarch/shared/data-access-api';
@@ -10,6 +10,7 @@ import {
   IngredientTypeDtoService,
 } from './internal/ingredient-type-dto.service';
 import { IngredientService } from './ingredient.service';
+import { IngredientDtoService } from './internal/ingredient-dto.service';
 import { mapIngredientTypeDtoToIngredientType } from './internal/map-ingredient-type-dto-to-ingredient-type';
 
 @Injectable({
@@ -21,14 +22,21 @@ export class IngredientTypeService
   constructor(
     private _authService: AuthService,
     private _ingredientTypeDtoService: IngredientTypeDtoService,
+    private _ingredientDtoService: IngredientDtoService,
     private _ingredientService: IngredientService
-  ) {}
+  ) {
+    this.getOne('LeUlBOssJlvdiNVGskSk').subscribe();
+  }
 
   getOne(id: string): Observable<IngredientType | undefined> {
-    return combineLatest([
-      this._ingredientTypeDtoService.getOne(id),
-      this._ingredientService.getAll(),
-    ]).pipe(
+    const ingredientTypeDto$ = this._ingredientTypeDtoService.getOne(id);
+    const ingredients$ = ingredientTypeDto$.pipe(
+      switchMap((dto) =>
+        this._ingredientDtoService.getMany(dto?.ingredientIds ?? [])
+      )
+    );
+
+    return combineLatest([ingredientTypeDto$, ingredients$]).pipe(
       map(([ingredientTypeDto, ingredients]) => {
         if (!ingredientTypeDto) {
           return undefined;
@@ -50,11 +58,12 @@ export class IngredientTypeService
             this._ingredientTypeDtoService.getAll(uid),
             this._ingredientService.getAll(),
           ]).pipe(
-            map(([ingredientDtos, ingredients]) =>
-              ingredientDtos.map((ingredientDto) =>
-                mapIngredientTypeDtoToIngredientType(ingredientDto, ingredients)
-              )
-            )
+            map(([ingredientDtos, ingredients]) => {
+              // console.log(ingredientDtos, ingredients);
+              return ingredientDtos.map((dishDto) =>
+                mapIngredientTypeDtoToIngredientType(dishDto, ingredients)
+              );
+            })
           );
         }
         return of([]);
