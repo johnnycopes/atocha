d938204ba02a829eb270cf904ff805c946a36074
John Coppola <jdcoppola@gmail.com>
2023-12-24 10:18:05 -0800

    FEATURE: implements new Places class in PlaceService

--- libs/globetrotter/data-access/src/lib/place.service.ts
+++ libs/globetrotter/data-access/src/lib/place.service.ts
@@ -4,9 +4,7 @@ import { Observable } from 'rxjs';
 import { State } from '@atocha/core/data-access';
 import { Country, Region } from '@atocha/globetrotter/util';
 import { ApiService } from './api.service';
-import { mapCountryDtoToCountry } from './internal/map-country-dto-to-country';
-import { groupBy } from './internal/group-by';
-import { sort } from './internal/sort';
+import { Places } from './internal/places';
 
 // Overrides to API data
 const COUNTRY_SUMMARY_NAMES: Readonly<Record<string, string>> = {
@@ -21,74 +19,17 @@ export class PlaceService {
     countries: Country[];
     countriesBySubregion: Record<string, Country[]>;
     regions: Region[];
-  }>({
-    countries: [],
-    countriesBySubregion: {},
-    regions: [],
-  });
+  }>(new Places());
   places$ = this._places.get();
 
   constructor(private _apiService: ApiService) {
-    this._apiService.fetchCountries().subscribe((countryDtos) => {
-      const countries = sort(
-        countryDtos
-          .filter(({ unMember }) => unMember)
-          .map(mapCountryDtoToCountry),
-        ({ name }) => name
-      );
-      const countriesBySubregion = groupBy(countries, 'subregion');
-      const subregionsByRegion =
-        this._groupSubregionsByRegion(countriesBySubregion);
-      const regions = this._formatRegions(
-        countriesBySubregion,
-        subregionsByRegion
-      );
-      this._places.update({
-        countries,
-        countriesBySubregion,
-        regions,
-      });
-    });
+    this._apiService
+      .fetchCountries()
+      .subscribe((countryDtos) => this._places.update(new Places(countryDtos)));
   }
 
   getSummary(countryName: string): Observable<string> {
     const searchTerm = COUNTRY_SUMMARY_NAMES[countryName] || countryName;
     return this._apiService.fetchSummary(searchTerm);
-  }
-
-  private _groupSubregionsByRegion(
-    countriesBySubregion: Record<string, Country[]>
-  ): Record<string, string[]> {
-    const subregions: Record<string, string[]> = {};
-
-    for (const [subregion, countries] of Object.entries(countriesBySubregion)) {
-      const region = countries?.[0]?.region ?? 'MISSING_REGION';
-      subregions[region] = !subregions[region]
-        ? [subregion]
-        : [...subregions[region], subregion];
-    }
-
-    return subregions;
-  }
-
-  private _formatRegions(
-    countriesBySubregion: Record<string, Country[]>,
-    subregionsByRegion: Record<string, string[]>
-  ): Region[] {
-    const regions: Region[] = [];
-
-    for (const [regionName, subregionNames] of Object.entries(
-      subregionsByRegion
-    )) {
-      regions.push({
-        name: regionName,
-        subregions: subregionNames.map((subregionName) => ({
-          name: subregionName,
-          countries: countriesBySubregion[subregionName],
-        })),
-      });
-    }
-
-    return regions;
   }
 }
