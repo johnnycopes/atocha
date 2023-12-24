c9a0b9799bb18ddf4b47f54339dbd93855151a4f
John Coppola <jdcoppola@gmail.com>
2023-12-24 09:07:25 -0800

    FEATURE: removes selection.interface.ts file export from lib

--- libs/globetrotter/data-access/src/index.ts
+++ libs/globetrotter/data-access/src/index.ts
@@ -5,5 +5,4 @@ export * from './lib/place.service';
 export * from './lib/quiz.service';
 export * from './lib/router.service';
 export * from './lib/routes';
-export * from './lib/selection.interface';
 export * from './lib/select.service';
