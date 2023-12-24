081054770bfef6c25dec7de40721279c1b773ddc
John Coppola <jdcoppola@gmail.com>
2023-12-24 08:15:43 -0800

    FEATURE: converts enum into readonly object

--- libs/menu-matriarch/shared/data-access-routing/src/lib/routes.ts
+++ libs/menu-matriarch/shared/data-access-routing/src/lib/routes.ts
@@ -1,10 +1,10 @@
-export enum Route {
-  planner = 'planner',
-  dishes = 'dishes',
-  ingredients = 'ingredients',
-  meals = 'meals',
-  menus = 'menus',
-  tags = 'tags',
-  settings = 'settings',
-  welcome = 'welcome',
-}
+export const Route = {
+  planner: 'planner',
+  dishes: 'dishes',
+  ingredients: 'ingredients',
+  meals: 'meals',
+  menus: 'menus',
+  tags: 'tags',
+  settings: 'settings',
+  welcome: 'welcome',
+} as const;
