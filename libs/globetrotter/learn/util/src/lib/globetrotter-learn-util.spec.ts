12adcd3198c4e10c0be7b8076afaefed700a18e1
John Coppola <jdcoppola@gmail.com>
2023-12-25 09:50:34 -0800

    TOOL: scaffolds globetrotter-learn-util lib

---
+++ libs/globetrotter/learn/util/src/lib/globetrotter-learn-util.spec.ts
@@ -0,0 +1,7 @@
+import { globetrotterLearnUtil } from './globetrotter-learn-util';
+
+describe('globetrotterLearnUtil', () => {
+  it('should work', () => {
+    expect(globetrotterLearnUtil()).toEqual('globetrotter-learn-util');
+  });
+});
---
+++ libs/globetrotter/learn/util/src/lib/globetrotter-learn-util.ts
@@ -0,0 +1,3 @@
+export function globetrotterLearnUtil(): string {
+  return 'globetrotter-learn-util';
+}
