b44143ae3be4d97fa766ecd6e5bf3cdbb2d72621
John Coppola <jdcoppola@gmail.com>
2023-12-25 10:47:39 -0800

    FILE: deletes original Summary interface

--- libs/globetrotter/shared/util/src/index.ts
+++ libs/globetrotter/shared/util/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/country.interface';
 export * from './lib/place.type';
 export * from './lib/region.interface';
 export * from './lib/subregion.interface';
-export * from './lib/summary.interface';
