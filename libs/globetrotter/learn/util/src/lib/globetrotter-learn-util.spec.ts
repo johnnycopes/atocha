dda3bd95d065b96c301c89b6e8126b86f7190637
John Coppola <jdcoppola@gmail.com>
2023-12-25 11:04:50 -0800

    FILE: deletes boilerplate function

--- libs/globetrotter/learn/util/src/lib/globetrotter-learn-util.spec.ts
+++
@@ -1,7 +0,0 @@
-import { globetrotterLearnUtil } from './globetrotter-learn-util';
-
-describe('globetrotterLearnUtil', () => {
-  it('should work', () => {
-    expect(globetrotterLearnUtil()).toEqual('globetrotter-learn-util');
-  });
-});
--- libs/globetrotter/learn/util/src/lib/globetrotter-learn-util.ts
+++
@@ -1,3 +0,0 @@
-export function globetrotterLearnUtil(): string {
-  return 'globetrotter-learn-util';
-}
