cfb1ee9e678ea7b6eea0fb148ae89d721a6b8530
John Coppola <jdcoppola@gmail.com>
2023-12-25 08:36:34 -0800

    FILE: deletes obsolete content from shared/data-acess

--- libs/globetrotter/shared/data-access/src/index.ts
+++ libs/globetrotter/shared/data-access/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/error.service';
 export * from './lib/loader.service';
 export * from './lib/place.service';
-export * from './lib/quiz.service';
 export * from './lib/router.service';
 export * from './lib/routes';
