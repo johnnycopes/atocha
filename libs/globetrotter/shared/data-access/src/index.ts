6e1959eb84f250174b7aa57cef5d6e6a07718d11
John Coppola <jdcoppola@gmail.com>
2023-12-26 09:37:27 -0800

    FILE: deletes shared/ version of PlaceService

--- libs/globetrotter/shared/data-access/src/index.ts
+++ libs/globetrotter/shared/data-access/src/index.ts
@@ -2,6 +2,5 @@ export * from './lib/country.service';
 export * from './lib/error.service';
 export * from './lib/loader.service';
 export * from './lib/places';
-export * from './lib/place.service';
 export * from './lib/router.service';
 export * from './lib/routes';
