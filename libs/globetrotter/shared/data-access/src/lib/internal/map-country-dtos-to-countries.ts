93f61079fae162e838b56657aef7c2fa81a8fd21
John Coppola <jdcoppola@gmail.com>
2023-12-26 11:37:10 -0800

    FEATURE: inlines sort function

--- libs/globetrotter/shared/data-access/src/lib/internal/map-country-dtos-to-countries.ts
+++ libs/globetrotter/shared/data-access/src/lib/internal/map-country-dtos-to-countries.ts
@@ -1,13 +1,12 @@
 import { Country } from '@atocha/globetrotter/shared/util';
 import { CountryDto } from './country-dto.interface';
 import { mapCountryDtoToCountry } from './map-country-dto-to-country';
-import { sort } from './sort';
 
 export function mapCountryDtosToCountries(
   countryDtos: CountryDto[]
 ): Country[] {
-  return sort(
-    countryDtos.filter(({ unMember }) => unMember).map(mapCountryDtoToCountry),
-    ({ name }) => name
-  );
+  return countryDtos
+    .filter(({ unMember }) => unMember)
+    .map(mapCountryDtoToCountry)
+    .sort((a, b) => (a.name > b.name ? 1 : -1));
 }
