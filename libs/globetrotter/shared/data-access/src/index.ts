f6c34f295a8fa63d0385ca2e0a9447166330242d
John Coppola <jdcoppola@gmail.com>
2023-12-26 09:38:13 -0800

    FILE: deletes shared/ version of Places class

--- libs/globetrotter/shared/data-access/src/index.ts
+++ libs/globetrotter/shared/data-access/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/country.service';
 export * from './lib/error.service';
 export * from './lib/loader.service';
-export * from './lib/places';
 export * from './lib/router.service';
 export * from './lib/routes';
