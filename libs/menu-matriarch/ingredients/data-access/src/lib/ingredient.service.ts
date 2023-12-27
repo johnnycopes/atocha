57db9ce9803bde15e76aa78e7cfd8230bd33625d
John Coppola <jdcoppola@gmail.com>
2023-12-27 08:52:47 -0800

    FEATURE: sets up State instancea in IngredientService

--- libs/menu-matriarch/ingredients/data-access/src/lib/ingredient.service.ts
+++ libs/menu-matriarch/ingredients/data-access/src/lib/ingredient.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { concatMap, first } from 'rxjs/operators';
 
+import { State } from '@atocha/core/data-access';
 import { AuthService } from '@atocha/firebase/data-access';
 import { IEntityService } from '@atocha/menu-matriarch/shared/data-access-api';
 import { Ingredient } from '@atocha/menu-matriarch/shared/util';
@@ -16,10 +17,21 @@ import {
 export class IngredientService
   implements IEntityService<Ingredient, EditableIngredientData>
 {
+  _state = new State<{ ingredients: Ingredient[] }>({ ingredients: [] });
+  ingredients$ = this._state.getProp('ingredients');
+
   constructor(
     private _authService: AuthService,
     private _ingredientDtoService: IngredientDtoService
-  ) {}
+  ) {
+    this._authService.uid$.pipe(first()).subscribe((uid) => {
+      if (uid) {
+        this._ingredientDtoService.getAll(uid).subscribe((ingredients) => {
+          this._state.updateProp('ingredients', ingredients);
+        });
+      }
+    });
+  }
 
   getOne(id: string): Observable<Ingredient | undefined> {
     return this._ingredientDtoService.getOne(id);
