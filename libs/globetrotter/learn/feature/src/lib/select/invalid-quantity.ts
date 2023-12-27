78f36e52e374aa6218839ee5e33b41d5a009456b
John Coppola <jdcoppola@gmail.com>
2023-12-27 09:23:17 -0800

    FILE: scaffolds invalid-quantity validator

---
+++ libs/globetrotter/learn/feature/src/lib/select/invalid-quantity.ts
@@ -0,0 +1,13 @@
+import { ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+
+import { Selection } from '@atocha/globetrotter/learn/util';
+
+export const playersOutnumberSelectedBoards: ValidatorFn = (
+  control: AbstractControl<Selection>
+): ValidationErrors | null => {
+  const quantity = control.value.quantity;
+  const numberOfPlaces = control.value.places.length;
+  numberOfPlaces;
+
+  return quantity < 2 ? { invalidQuantity: 'Invalid quantity' } : null;
+};
