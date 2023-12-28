6154efdb8c75c28f93f0bd009af6beb33bc2896f
John Coppola <jdcoppola@gmail.com>
2023-12-28 12:11:44 -0800

    FEATURE: fleshes out xplore-countries and implements it in explore instead of list-details component

--- libs/globetrotter/explore/feature/src/lib/explore-countries/explore-countries.component.ts
+++ libs/globetrotter/explore/feature/src/lib/explore-countries/explore-countries.component.ts
@@ -1,12 +1,93 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+
+import { SearchInputComponent, trackByFactory } from '@atocha/core/ui';
+import { InputComponent } from '@atocha/globetrotter/shared/ui';
+import { Country } from '@atocha/globetrotter/shared/util';
 
 @Component({
   standalone: true,
-  selector: 'app-explore-countries',
-  imports: [CommonModule],
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'nav[app-explore-countries]',
+  imports: [CommonModule, InputComponent, SearchInputComponent],
   templateUrl: './explore-countries.component.html',
   styleUrls: ['./explore-countries.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ExploreCountriesComponent {}
+export class ExploreCountriesComponent implements OnChanges {
+  @Input() countries: Country[] = [];
+  @Input() selectedCountry: Country | undefined;
+  @Input() searchTerm = '';
+  @Output() selectedCountryChange = new EventEmitter<Country>();
+  @Output() searchTermChange = new EventEmitter<string>();
+
+  @ViewChild('list', { static: true })
+  list!: ElementRef<HTMLElement>;
+
+  readonly trackByFn = trackByFactory<Country>(({ id }) => id);
+  private _selectedCountryIndex = 0;
+  private readonly _countryHeight = 60;
+
+  @HostListener('window:keydown.arrowUp', ['$event'])
+  onArrowUp(event: KeyboardEvent): void {
+    event.preventDefault();
+    this._moveUpList(1);
+  }
+
+  @HostListener('window:keydown.shift.arrowUp', ['$event'])
+  onShiftArrowUp(event: KeyboardEvent): void {
+    event.preventDefault();
+    this._moveUpList(10);
+  }
+
+  @HostListener('window:keydown.arrowDown', ['$event'])
+  onArrowDown(event: KeyboardEvent): void {
+    event.preventDefault();
+    this._moveDownList(1);
+  }
+
+  @HostListener('window:keydown.shift.arrowDown', ['$event'])
+  onShiftArrowDown(event: KeyboardEvent): void {
+    event.preventDefault();
+    this._moveDownList(10);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes?.['selectedCountry'] || changes?.['countries']) {
+      if (!this.selectedCountry) {
+        return;
+      }
+
+      this._selectedCountryIndex = this.countries.indexOf(this.selectedCountry);
+      if (this._selectedCountryIndex >= 0) {
+        this.list.nativeElement.scrollTop =
+          this._selectedCountryIndex * this._countryHeight;
+      }
+    }
+  }
+
+  private _moveUpList(incrementValue: number): void {
+    const newIndex = this._selectedCountryIndex - incrementValue;
+    if (newIndex >= 0) {
+      this.selectedCountryChange.emit(this.countries[newIndex]);
+    }
+  }
+
+  private _moveDownList(incrementValue: number): void {
+    const newIndex = this._selectedCountryIndex + incrementValue;
+    if (newIndex < this.countries.length) {
+      this.selectedCountryChange.emit(this.countries[newIndex]);
+    }
+  }
+}
--- libs/globetrotter/explore/feature/src/lib/explore.component.ts
+++ libs/globetrotter/explore/feature/src/lib/explore.component.ts
@@ -4,13 +4,19 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { fadeInAnimation } from '@atocha/globetrotter/shared/ui';
 import { Country } from '@atocha/globetrotter/shared/util';
 import { ExploreService } from './explore.service';
+import { ExploreCountriesComponent } from './explore-countries/explore-countries.component';
 import { ExploreCountryComponent } from './explore-country/explore-country.component';
 import { ListDetailsComponent } from './list-details/list-details.component';
 
 @Component({
   standalone: true,
   selector: 'app-explore',
-  imports: [CommonModule, ExploreCountryComponent, ListDetailsComponent],
+  imports: [
+    CommonModule,
+    ExploreCountriesComponent,
+    ExploreCountryComponent,
+    ListDetailsComponent,
+  ],
   providers: [ExploreService],
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss'],
