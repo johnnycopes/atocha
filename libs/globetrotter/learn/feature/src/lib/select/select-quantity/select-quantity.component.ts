06e9f26a4575bffe2632069faca4ca9ce4bee440
John Coppola <jdcoppola@gmail.com>
2023-12-27 17:21:17 -0800

    FEATURE: simplifies select-quantity component

--- libs/globetrotter/learn/feature/src/lib/select/select-quantity/select-quantity.component.ts
+++ libs/globetrotter/learn/feature/src/lib/select/select-quantity/select-quantity.component.ts
@@ -1,11 +1,6 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  Input,
-  Output,
-  EventEmitter,
-} from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { InputComponent } from '@atocha/globetrotter/shared/ui';
 import { SelectForm } from '../select-form';
@@ -13,14 +8,12 @@ import { SelectForm } from '../select-form';
 @Component({
   standalone: true,
   selector: 'app-select-quantity',
-  imports: [FormsModule, InputComponent, ReactiveFormsModule],
+  imports: [CommonModule, InputComponent, ReactiveFormsModule],
   templateUrl: './select-quantity.component.html',
   styleUrls: ['./select-quantity.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectQuantityComponent {
   @Input({ required: true }) form!: SelectForm;
-  @Input() invalid = false;
-  @Input() quantity = 0;
-  @Output() quantityChange = new EventEmitter<number>();
+  @Input({ required: true }) error = '';
 }
