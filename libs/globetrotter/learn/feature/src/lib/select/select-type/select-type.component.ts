133e720ce226d1e246a5ec3250f49a44a15c7f69
John Coppola <jdcoppola@gmail.com>
2023-12-27 17:32:05 -0800

    FEATURE: simplifies select-type component

--- libs/globetrotter/learn/feature/src/lib/select/select-type/select-type.component.ts
+++ libs/globetrotter/learn/feature/src/lib/select/select-type/select-type.component.ts
@@ -1,64 +1,34 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  ChangeDetectionStrategy,
-  Input,
-  Output,
-  EventEmitter,
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import {
-  RadioButtonsComponent,
-  RadioButtonsOption,
-  SmallCapsComponent,
-} from '@atocha/globetrotter/shared/ui';
+import { SmallCapsComponent } from '@atocha/globetrotter/shared/ui';
 import { QuizType } from '@atocha/globetrotter/learn/util';
 import { SelectForm } from '../select-form';
 
 @Component({
   standalone: true,
   selector: 'app-select-type',
-  imports: [
-    CommonModule,
-    RadioButtonsComponent,
-    ReactiveFormsModule,
-    SmallCapsComponent,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, SmallCapsComponent],
   templateUrl: './select-type.component.html',
   styleUrls: ['./select-type.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectTypeComponent {
-  options: RadioButtonsOption<QuizType>[] = [
-    QuizType.flagsCountries,
-    QuizType.capitalsCountries,
-    QuizType.countriesCapitals,
-  ].map((quizType) => ({
-    display: this._getDisplayText(quizType),
-    value: quizType,
-  }));
-
   @Input({ required: true }) form!: SelectForm;
-  @Input()
-  set type(value: QuizType) {
-    const option = this.options.find((type) => type.value === value);
-    if (option) {
-      this.selectedOption = { ...option };
-    }
-  }
-  selectedOption = this.options[0];
 
-  @Output() typeChange = new EventEmitter<QuizType>();
-
-  private _getDisplayText(quizType: QuizType): string {
-    switch (quizType) {
-      case QuizType.flagsCountries:
-        return 'Flags / Countries';
-      case QuizType.capitalsCountries:
-        return 'Capitals / Countries';
-      case QuizType.countriesCapitals:
-        return 'Countries / Capitals';
-    }
-  }
+  readonly options = [
+    {
+      display: 'Flags / Countries',
+      value: QuizType.flagsCountries,
+    },
+    {
+      display: 'Capitals / Countries',
+      value: QuizType.capitalsCountries,
+    },
+    {
+      display: 'Countries / Capitals',
+      value: QuizType.countriesCapitals,
+    },
+  ];
 }
