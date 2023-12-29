f036c4c12906b139d7460d5261758495c2e8171d
John Coppola <jdcoppola@gmail.com>
2023-12-29 06:26:54 -0800

    TEST: begins writing spec for ExploreService

--- libs/globetrotter/explore/feature/src/lib/explore.service.spec.ts
+++ libs/globetrotter/explore/feature/src/lib/explore.service.spec.ts
@@ -7,13 +7,58 @@ import { ExploreService } from './explore.service';
 describe('ExploreService', () => {
   let service: ExploreService;
 
+  const DJIBOUTI = {
+    name: 'Djibouti',
+    capital: 'Djibouti',
+    region: 'Africa',
+    subregion: 'Eastern Africa',
+  } as Country;
+
+  const MONTENEGRO = {
+    name: 'Montenegro',
+    capital: 'Podgorica',
+    region: 'Europe',
+    subregion: 'Southeast Europe',
+  } as Country;
+
+  const PHILIPPINES = {
+    name: 'Philippines',
+    capital: 'Manila',
+    region: 'Asia',
+    subregion: 'South-Eastern Asia',
+  } as Country;
+
+  const SEYCHELLES = {
+    name: 'Seychelles',
+    capital: 'Victoria',
+    region: 'Africa',
+    subregion: 'Eastern Africa',
+  } as Country;
+
   beforeEach(() => {
     service = new ExploreService({
-      countries$: of<Country[]>([]),
+      countries$: of<Country[]>([
+        DJIBOUTI,
+        MONTENEGRO,
+        PHILIPPINES,
+        SEYCHELLES,
+      ]),
+      getCountries() {},
+      getSummary(_: string) {
+        return of('');
+      },
     } as CountryService);
   });
 
-  it('renders', () => {
-    expect(service).toBeTruthy();
+  it('initializes with default state', (done) => {
+    service.vm$.subscribe((state) => {
+      expect(state).toStrictEqual({
+        filteredCountries: [DJIBOUTI, MONTENEGRO, PHILIPPINES, SEYCHELLES],
+        selectedCountry: DJIBOUTI,
+        searchTerm: '',
+        summary: '',
+      });
+      done();
+    });
   });
 });
