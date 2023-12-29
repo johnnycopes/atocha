4063e46cc820740c2458e58862fd906ef1682234
John Coppola <jdcoppola@gmail.com>
2023-12-29 07:04:43 -0800

    FILE: writes MockCountryService logic

--- libs/globetrotter/shared/data-access/src/lib/testing/mock-country.service.ts
+++ libs/globetrotter/shared/data-access/src/lib/testing/mock-country.service.ts
@@ -1,1 +1,11 @@
-export class MockCountryService {}
+import { Observable, of } from 'rxjs';
+
+import { DJIBOUTI, MONTENEGRO, PHILIPPINES, SEYCHELLES } from './countries';
+
+export class MockCountryService {
+  countries$ = of([DJIBOUTI, MONTENEGRO, PHILIPPINES, SEYCHELLES]);
+
+  getSummary(countryName: string): Observable<string> {
+    return of(`Summary text about ${countryName}`);
+  }
+}
