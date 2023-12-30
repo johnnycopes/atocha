313b1cb76bc4dfba14bca4c9f06059f726df2e3a
John Coppola <jdcoppola@gmail.com>
2023-12-30 08:27:56 -0800

    FILE: duplicates FlipCardComponent into globetrotter/learn/ui

--- libs/globetrotter/learn/ui/src/index.ts
+++ libs/globetrotter/learn/ui/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/fixed-slideable-panel/fixed-slideable-panel.component';
+export * from './lib/flip-card/flip-card.component';
 export * from './lib/globetrotter-learn-ui/globetrotter-learn-ui.component';
