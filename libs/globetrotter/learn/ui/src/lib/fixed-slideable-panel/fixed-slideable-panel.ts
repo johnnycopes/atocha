55cbee9aa27c3cff002a54648aa27fd9cdc771c4
John Coppola <jdcoppola@gmail.com>
2023-12-30 09:04:28 -0800

    FILE: duplicates positionAnimation to consumers

---
+++ libs/globetrotter/learn/ui/src/lib/fixed-slideable-panel/fixed-slideable-panel.ts
@@ -0,0 +1,37 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+
+import { DURATION } from '@atocha/globetrotter/shared/ui';
+
+export const positionAnimation = trigger('position', [
+  state(
+    'offscreen',
+    style({
+      transform: 'translateY(-100%)',
+    })
+  ),
+  state(
+    'header',
+    style({
+      transform: 'translateY(calc(-100% + 64px))', // this value must kept in sync with the header height in SCSS
+    })
+  ),
+  state(
+    'fullscreen',
+    style({
+      transform: 'translateY(0)',
+    })
+  ),
+  state(
+    'navigation',
+    style({
+      transform: 'translateX(100%)',
+    })
+  ),
+  transition('* => *', animate(`${DURATION.position}ms ease-in-out`)),
+]);
