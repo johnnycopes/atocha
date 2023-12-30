e076800026cc6e32bfa95498d0aa12a81743c097
John Coppola <jdcoppola@gmail.com>
2023-12-30 09:44:07 -0800

    FEATURE: implements FlagComponent in ExploreCountryComponent

--- libs/globetrotter/explore/feature/src/lib/explore-country/explore-country.component.ts
+++ libs/globetrotter/explore/feature/src/lib/explore-country/explore-country.component.ts
@@ -12,7 +12,10 @@ import {
 
 import { trackByFactory, trackBySelf } from '@atocha/core/ui';
 import { pluralize } from '@atocha/core/util';
-import { SmallCapsComponent } from '@atocha/globetrotter/shared/ui';
+import {
+  FlagComponent,
+  SmallCapsComponent,
+} from '@atocha/globetrotter/shared/ui';
 import { Country } from '@atocha/globetrotter/shared/util';
 import { MeasurementPipe } from './measurement.pipe';
 
@@ -25,7 +28,7 @@ interface TableData {
 @Component({
   standalone: true,
   selector: 'app-explore-country',
-  imports: [CommonModule, MeasurementPipe, SmallCapsComponent],
+  imports: [CommonModule, FlagComponent, MeasurementPipe, SmallCapsComponent],
   templateUrl: './explore-country.component.html',
   styleUrls: ['./explore-country.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
