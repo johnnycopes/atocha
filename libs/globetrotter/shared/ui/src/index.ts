0541e3d6a0fd822b4c8597c77faf4aab6eef8c66
John Coppola <jdcoppola@gmail.com>
2023-12-30 08:35:38 -0800

    FILE: deletes unused MeasurementPipe

--- libs/globetrotter/shared/ui/src/index.ts
+++ libs/globetrotter/shared/ui/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib/error/error.component';
 export * from './lib/icon/icon.component';
 export * from './lib/input/input.component';
 export * from './lib/loader/loader.component';
-export * from './lib/measurement/measurement.pipe';
 export * from './lib/small-caps/small-caps.component';
 
 export * from './lib/animations';
