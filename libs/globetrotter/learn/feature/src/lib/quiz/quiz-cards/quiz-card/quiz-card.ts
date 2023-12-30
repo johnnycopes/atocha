0029b13a74afa88c1e296b4ef539e1fdfa71ae5f
John Coppola <jdcoppola@gmail.com>
2023-12-30 08:39:45 -0800

    FILE: duplicates quiz-card animations into quiz-card.ts

---
+++ libs/globetrotter/learn/feature/src/lib/quiz/quiz-cards/quiz-card/quiz-card.ts
@@ -0,0 +1,61 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+
+import { DURATION } from '@atocha/globetrotter/shared/ui';
+
+export const flipAnimation = trigger('flip', [
+  state(
+    'front',
+    style({
+      transform: 'rotateY(0)',
+    })
+  ),
+  state(
+    'back',
+    style({
+      transform: 'rotateY(180deg)',
+    })
+  ),
+  transition('front => back', animate(`${DURATION.cardAnimation}ms ease-in`)),
+  transition('back => front', animate(`${DURATION.cardAnimation}ms ease-out`)),
+]);
+
+export const disabledAnimation = trigger('disabled', [
+  state(
+    'disabled',
+    style({
+      filter: 'grayscale(100%)',
+    })
+  ),
+  transition('* => disabled', animate(`${DURATION.cardAnimation}ms ease-in`)),
+]);
+
+export const guessAnimation = trigger('guess', [
+  state(
+    'none',
+    style({
+      border: 'none',
+      padding: '20px',
+    })
+  ),
+  state(
+    'correct',
+    style({
+      border: '20px solid limegreen',
+      padding: '0',
+    })
+  ),
+  state(
+    'incorrect',
+    style({
+      border: '20px solid crimson',
+      padding: '0',
+    })
+  ),
+  transition('* => *', animate(`${DURATION.cardAnimation}ms ease-in`)),
+]);
