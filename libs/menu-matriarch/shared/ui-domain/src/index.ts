5e138b326878481c9c486d8ca4c577cd46d2faeb
John Coppola <jdcoppola@gmail.com>
2023-12-31 14:41:47 -0800

    FILE: deletes original dish-summary

--- libs/menu-matriarch/shared/ui-domain/src/index.ts
+++ libs/menu-matriarch/shared/ui-domain/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/track-by-functions';
 
 export * from './lib/count/count.component';
-export * from './lib/dish-summary/dish-summary.component';
 export * from './lib/inline-day-select/inline-day-select.component';
 export * from './lib/inline-name-edit/inline-name-edit.component';
