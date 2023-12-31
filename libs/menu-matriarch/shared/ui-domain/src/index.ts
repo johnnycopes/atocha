ed32d91689c812fc6746b632cd6ba18ebfc149b7
John Coppola <jdcoppola@gmail.com>
2023-12-31 12:58:23 -0800

    FILE: deletes filterable-list component from menu-matriarch shared/ui-domain

--- libs/menu-matriarch/shared/ui-domain/src/index.ts
+++ libs/menu-matriarch/shared/ui-domain/src/index.ts
@@ -2,7 +2,6 @@ export * from './lib/track-by-functions';
 
 export * from './lib/count/count.component';
 export * from './lib/dish-summary/dish-summary.component';
-export * from './lib/filterable-list/filterable-list.component';
 export * from './lib/inline-day-select/inline-day-select.component';
 export * from './lib/inline-name-edit/inline-name-edit.component';
 export * from './lib/meal-summary/meal-summary.component';
