2605fee369e3feaeb93cae25d69fa6cc31d8a9e8
John Coppola <jdcoppola@gmail.com>
2023-12-31 14:36:26 -0800

    FILE: deletes original meal-summary

--- libs/menu-matriarch/shared/ui-domain/src/index.ts
+++ libs/menu-matriarch/shared/ui-domain/src/index.ts
@@ -4,4 +4,3 @@ export * from './lib/count/count.component';
 export * from './lib/dish-summary/dish-summary.component';
 export * from './lib/inline-day-select/inline-day-select.component';
 export * from './lib/inline-name-edit/inline-name-edit.component';
-export * from './lib/meal-summary/meal-summary.component';
