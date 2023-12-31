62507d7a1e1cc8a8fea1aafd2ec3575589da5032
John Coppola <jdcoppola@gmail.com>
2023-12-31 12:50:41 -0800

    FEATURE: removes export of filters and filters button components

--- libs/menu-matriarch/shared/ui-domain/src/index.ts
+++ libs/menu-matriarch/shared/ui-domain/src/index.ts
@@ -3,8 +3,6 @@ export * from './lib/track-by-functions';
 export * from './lib/count/count.component';
 export * from './lib/dish-summary/dish-summary.component';
 export * from './lib/filterable-list/filterable-list.component';
-export * from './lib/filters/filters.component';
-export * from './lib/filters-button/filters-button.component';
 export * from './lib/inline-day-select/inline-day-select.component';
 export * from './lib/inline-name-edit/inline-name-edit.component';
 export * from './lib/meal-summary/meal-summary.component';
