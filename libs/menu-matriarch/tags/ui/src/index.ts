15a9d47928028ce9642236835b56310a810affb6
John Coppola <jdcoppola@gmail.com>
2023-12-31 14:12:12 -0800

    FILE: copies tag, tags-list, and tag def to new lib

--- libs/menu-matriarch/tags/ui/src/index.ts
+++ libs/menu-matriarch/tags/ui/src/index.ts
@@ -1,1 +1,4 @@
 export * from './lib/menu-matriarch-tags-ui/menu-matriarch-tags-ui.component';
+export * from './lib/tag/tag.component';
+export * from './lib/tags-list/tag-def.directive';
+export * from './lib/tags-list/tags-list.component';
---
+++ libs/menu-matriarch/tags/ui/src/lib/tags-list/tags-list.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  Input,
+  TemplateRef,
+  ContentChild,
+} from '@angular/core';
+
+import { trackByFactory } from '@atocha/core/ui';
+import { Tag } from '@atocha/menu-matriarch/shared/util';
+import { TagDefContext, TagDefDirective } from './tag-def.directive';
+
+@Component({
+  standalone: true,
+  selector: 'ui-tags-list',
+  imports: [CommonModule],
+  templateUrl: './tags-list.component.html',
+  styleUrls: ['./tags-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TagsListComponent<T extends Tag> {
+  @Input() tags: T[] = [];
+  @Input() interactive = false;
+  readonly trackByFn = trackByFactory<Tag>(({ id }) => id);
+
+  @ContentChild(TagDefDirective)
+  tagDef: TagDefDirective<T> | undefined;
+
+  get tagTemplate(): TemplateRef<TagDefContext<T>> | null {
+    return this.tagDef?.template ?? null;
+  }
+}
