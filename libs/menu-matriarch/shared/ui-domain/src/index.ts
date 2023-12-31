eb21bbe3a0e41e2098c99ad8371ad751cf017c09
John Coppola <jdcoppola@gmail.com>
2023-12-31 14:16:46 -0800

    FILE: deletes original components from shared/ui

--- libs/menu-matriarch/shared/ui-domain/src/index.ts
+++ libs/menu-matriarch/shared/ui-domain/src/index.ts
@@ -5,6 +5,3 @@ export * from './lib/dish-summary/dish-summary.component';
 export * from './lib/inline-day-select/inline-day-select.component';
 export * from './lib/inline-name-edit/inline-name-edit.component';
 export * from './lib/meal-summary/meal-summary.component';
-export * from './lib/tags/tag/tag.component';
-export * from './lib/tags/tags-list/tag-def.directive';
-export * from './lib/tags/tags-list/tags-list.component';
