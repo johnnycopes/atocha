1d1c897fc61a23ee1a6333db12e90780d2a0f2a7
John Coppola <jdcoppola@gmail.com>
2023-12-31 14:20:14 -0800

    FILE: deletes boilerplate component

--- libs/menu-matriarch/tags/ui/src/index.ts
+++ libs/menu-matriarch/tags/ui/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/menu-matriarch-tags-ui/menu-matriarch-tags-ui.component';
 export * from './lib/tag/tag.component';
 export * from './lib/tags-list/tag-def.directive';
 export * from './lib/tags-list/tags-list.component';
