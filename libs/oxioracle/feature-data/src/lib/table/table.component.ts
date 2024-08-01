609367596a189000f5d7ad6f3e501bb9957217a3
John Coppola <jdcoppola@gmail.com>
2024-08-01 08:16:23 -0400

    FEATURE: pipes todos into table

--- libs/oxioracle/feature-data/src/lib/table/table.component.ts
+++ libs/oxioracle/feature-data/src/lib/table/table.component.ts
@@ -8,27 +8,10 @@ import {
 } from '@angular/core';
 import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { map } from 'rxjs';
 
 import { TodoService } from '@atocha/oxioracle/data-access';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
+import { Todo } from '@atocha/oxioracle/util';
 
 @Component({
   standalone: true,
@@ -39,9 +22,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TableComponent implements AfterViewInit {
-  todos$ = this._todoService.todos$;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  displayedColumns: string[] = ['id', 'userId', 'title', 'completed'];
+
+  private _dataSource = new MatTableDataSource<Todo>();
+  dataSource$ = this._todoService.todos$.pipe(
+    map((todos) => {
+      const dataSource = this._dataSource;
+      dataSource.data = todos;
+      return dataSource;
+    })
+  );
 
   constructor(
     private _liveAnnouncer: LiveAnnouncer,
@@ -51,15 +41,10 @@ export class TableComponent implements AfterViewInit {
   @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
+    this._dataSource.sort = this.sort;
   }
 
-  /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
