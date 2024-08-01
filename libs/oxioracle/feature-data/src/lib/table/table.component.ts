dd613ef615266c7e4a42d87c5c20fb7a09b7ab69
John Coppola <jdcoppola@gmail.com>
2024-08-01 08:56:17 -0400

    Table view (#543)
    * CONFIG: installs Angular Material
    
    * TOOL: adds Material to oxioracle project
    
    * FEATURE: brings in boilerplate sortable table
    
    * FEATURE: pipes todos into table
    
    * FEATURE: styles table typography

--- libs/oxioracle/feature-data/src/lib/table/table.component.ts
+++ libs/oxioracle/feature-data/src/lib/table/table.component.ts
@@ -1,18 +1,54 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { map } from 'rxjs';
 
 import { TodoService } from '@atocha/oxioracle/data-access';
+import { Todo } from '@atocha/oxioracle/util';
 
 @Component({
   standalone: true,
   selector: 'app-table',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatSortModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableComponent {
-  todos$ = this._todoService.todos$;
+export class TableComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'userId', 'title', 'completed'];
 
-  constructor(private _todoService: TodoService) {}
+  private _dataSource = new MatTableDataSource<Todo>();
+  dataSource$ = this._todoService.todos$.pipe(
+    map((todos) => {
+      const dataSource = this._dataSource;
+      dataSource.data = todos;
+      return dataSource;
+    })
+  );
+
+  constructor(
+    private _liveAnnouncer: LiveAnnouncer,
+    private _todoService: TodoService
+  ) {}
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this._dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
