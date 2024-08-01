9edde642bc252d6573f16c16b3b1b7a4bde9b8b3
John Coppola <jdcoppola@gmail.com>
2024-08-01 07:05:15 -0400

    FEATURE: loads todos in both feature components

--- libs/oxioracle/feature-data/src/lib/table/table.component.ts
+++ libs/oxioracle/feature-data/src/lib/table/table.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { TodoService } from '@atocha/oxioracle/data-access';
 
 @Component({
   standalone: true,
   selector: 'app-table',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableComponent {}
+export class TableComponent {
+  todos$ = this._todoService.todos$;
+
+  constructor(private _todoService: TodoService) {}
+}
