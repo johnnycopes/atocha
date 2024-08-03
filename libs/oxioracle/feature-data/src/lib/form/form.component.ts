b8d1dadb5131fa5967dbb8ba627f4d39243972a7
John Coppola <jdcoppola@gmail.com>
2024-08-03 17:27:14 -0400

    FEATURE: adds success toast

--- libs/oxioracle/feature-data/src/lib/form/form.component.ts
+++ libs/oxioracle/feature-data/src/lib/form/form.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 import { TodoForm } from './form';
+import { wait } from '@atocha/core/util';
 import { TodoService } from '@atocha/oxioracle/data-access';
 
 @Component({
@@ -15,15 +17,19 @@ import { TodoService } from '@atocha/oxioracle/data-access';
 })
 export class FormComponent {
   readonly form = new TodoForm();
+  feedback$ = new BehaviorSubject(false);
 
   constructor(private _todoService: TodoService) {}
 
-  onSubmit() {
+  async onSubmit() {
     if (!this.form.valid) {
       this.form.markAllAsTouched();
       return;
     }
     this._todoService.addTodo(this.form.getRawValue());
     this.form.reset();
+    this.feedback$.next(true);
+    await wait(2000);
+    this.feedback$.next(false);
   }
 }
