5e361be3590cfa38f0b09d0fc52a7ad2ab7cdf65
John Coppola <jdcoppola@gmail.com>
2024-08-03 13:38:52 -0400

    FEATURE: sets up completed/not completed pie chart

--- libs/oxioracle/feature-data/src/lib/charts/charts.component.ts
+++ libs/oxioracle/feature-data/src/lib/charts/charts.component.ts
@@ -1,18 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartOptions } from 'chart.js';
 
 import { TodoService } from '@atocha/oxioracle/data-access';
+
+interface PieChart {
+  datasets: [{ data: number[] }];
+  labels: string[];
+  options: ChartOptions<'pie'>;
+}
 
 @Component({
   standalone: true,
   selector: 'app-charts',
-  imports: [CommonModule],
+  imports: [BaseChartDirective, CommonModule],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartsComponent {
   todos$ = this._todoService.sortedTodos$;
+
+  pieChart$: Observable<PieChart> = this._todoService.todos$.pipe(
+    map((todos) => {
+      const completed = todos.filter((todo) => todo.completed).length;
+      const data = [completed, todos.length - completed];
+      return {
+        datasets: [{ data }],
+        labels: ['Completed', 'Not completed'],
+        options: { responsive: true },
+      };
+    })
+  );
 
   constructor(private _todoService: TodoService) {}
 }
