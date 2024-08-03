145bf033c05b8504c17326930fc6d42705861e21
John Coppola <jdcoppola@gmail.com>
2024-08-03 15:26:10 -0400

    Charts view (#546)
    * TOOL: nx add ng2-charts project=oxioracle
    
    * FEATURE: sets up completed/not completed pie chart
    
    * FILE: adds groupBy function
    
    * FEATURE: creates bar chart
    
    * FEATURE: erases bar chart boilerplate code
    
    * FEATURE: adds completed/incomplete todos to chart
    
    * FEATURE: makes shape of Chart interface more similar
    
    * FEATURE: extracts generic Chart interface
    
    * FILE: extracts component logic to local ChartsService
    
    * FEATURE: moves type into interface
    
    * FEATURE: lays out charts horizontally
    
    * FEATURE: adds titles to charts
    
    * FEATURE: correct font
    
    * FEATURE: chart colors
    
    * FEATURE: nicer titles
    
    * FILE: copies groupBy to core-util lib
    
    * FEATURE: exports fn
    
    * FEATURE: uses core-util groupBy across app
    
    * FILE: deletes original groupBy
    
    * FILE: deletes copied groupBy from oxioracle-util
    
    * MANUAL: import order
    
    * TOOL: nx format:write
    
    * TOOL: npm install to generate new package-lock.json
    
    * FILE: copies group-by to be local function
    
    * Revert "FILE: deletes original groupBy"
    
    This reverts commit 9e6135393558eb5d2f609e2890cd463ef6f17c53.
    
    * FILE: reverts groupBy to core-util

--- libs/oxioracle/feature-data/src/lib/charts/charts.component.ts
+++ libs/oxioracle/feature-data/src/lib/charts/charts.component.ts
@@ -1,18 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
 
-import { TodoService } from '@atocha/oxioracle/data-access';
+import { ChartsService } from './charts.service';
 
 @Component({
   standalone: true,
   selector: 'app-charts',
-  imports: [CommonModule],
+  imports: [BaseChartDirective, CommonModule],
+  providers: [ChartsService],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartsComponent {
-  todos$ = this._todoService.sortedTodos$;
+  barChart$ = this._chartsService.barChart$;
+  pieChart$ = this._chartsService.pieChart$;
 
-  constructor(private _todoService: TodoService) {}
+  constructor(private _chartsService: ChartsService) {}
 }
---
+++ libs/oxioracle/feature-data/src/lib/charts/charts.service.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+import { TodoService } from '@atocha/oxioracle/data-access';
+import { map, Observable } from 'rxjs';
+import {
+  ChartConfiguration,
+  ChartOptions,
+  ChartType,
+  ChartTypeRegistry,
+} from 'chart.js';
+
+import { groupBy } from './group-by';
+
+export interface Chart<T extends ChartType = keyof ChartTypeRegistry> {
+  data: ChartConfiguration<T>['data'];
+  options: ChartOptions<T>;
+  type: T;
+}
+
+type BarChart = Chart<'bar'>;
+type PieChart = Chart<'pie'>;
+
+@Injectable()
+export class ChartsService {
+  private readonly _todoColor = '#ffcd56';
+  private readonly _completedTodoColor = '#36a2eb';
+  private readonly _incompleteTodoColor = '#ff6384';
+
+  barChart$: Observable<BarChart> = this._todoService.todos$.pipe(
+    map((todos) => {
+      const todosByUser = groupBy(todos, 'userId');
+      return {
+        data: {
+          datasets: [
+            {
+              data: Object.values(todosByUser).map((todos) => todos.length),
+              label: 'All todos',
+              backgroundColor: this._todoColor,
+              borderColor: this._todoColor,
+            },
+            {
+              data: Object.values(todosByUser).map(
+                (todos) => todos.filter((todo) => todo.completed).length
+              ),
+              label: 'Completed todos',
+              backgroundColor: this._completedTodoColor,
+              borderColor: this._completedTodoColor,
+            },
+            {
+              data: Object.values(todosByUser).map(
+                (todos) => todos.filter((todo) => !todo.completed).length
+              ),
+              label: 'Incomplete todos',
+              backgroundColor: this._incompleteTodoColor,
+              borderColor: this._incompleteTodoColor,
+            },
+          ],
+          labels: Object.keys(todosByUser),
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: `Todos by User ID (${todos.length} total)`,
+              font: { size: 20, weight: 400, family: 'Raleway, sans-serif' },
+            },
+            legend: { labels: { font: { family: 'Raleway, sans-serif' } } },
+          },
+        },
+        type: 'bar',
+      };
+    })
+  );
+
+  pieChart$: Observable<PieChart> = this._todoService.todos$.pipe(
+    map((todos) => {
+      const completed = todos.filter((todo) => todo.completed).length;
+      const data = [completed, todos.length - completed];
+      return {
+        data: {
+          datasets: [
+            {
+              data,
+              backgroundColor: [
+                this._completedTodoColor,
+                this._incompleteTodoColor,
+              ],
+            },
+          ],
+          labels: ['Completed', 'Not completed'],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            title: {
+              display: true,
+              text: `All todos (${todos.length} total)`,
+              font: { size: 20, weight: 400, family: 'Raleway, sans-serif' },
+            },
+            legend: { labels: { font: { family: 'Raleway, sans-serif' } } },
+          },
+        },
+        type: 'pie',
+      };
+    })
+  );
+
+  constructor(private _todoService: TodoService) {}
+}
