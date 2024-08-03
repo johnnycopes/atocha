31ab876d4238f30da7eeaa4a7969764e71f13d36
John Coppola <jdcoppola@gmail.com>
2024-08-03 14:08:25 -0400

    FEATURE: erases bar chart boilerplate code

--- libs/oxioracle/feature-data/src/lib/charts/charts.component.ts
+++ libs/oxioracle/feature-data/src/lib/charts/charts.component.ts
@@ -29,21 +29,6 @@ interface BarChart {
 export class ChartsComponent {
   todos$ = this._todoService.sortedTodos$;
 
-  public barChartLegend = true;
-  public barChartPlugins = [];
-
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    ],
-  };
-
-  public barChartOptions: ChartConfiguration<'bar'>['options'] = {
-    responsive: false,
-  };
-
   barChart$: Observable<BarChart> = this._todoService.todos$.pipe(
     map((todos) => {
       const todosByUser = groupBy<Todo>(todos, 'userId');
