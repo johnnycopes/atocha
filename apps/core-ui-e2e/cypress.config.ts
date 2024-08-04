f9a552bff94136b4b59fb1dbc70bb14412e8d1a2
John Coppola <jdcoppola@gmail.com>
2024-08-04 08:04:02 -0400

    CONFIG: moves core-ui to 4200

--- apps/core-ui-e2e/cypress.config.ts
+++ apps/core-ui-e2e/cypress.config.ts
@@ -8,7 +8,7 @@ const cypressJsonConfig = {
   videosFolder: '../../dist/cypress/apps/core-ui-e2e/videos',
   screenshotsFolder: '../../dist/cypress/apps/core-ui-e2e/screenshots',
   chromeWebSecurity: false,
-  baseUrl: 'http://localhost:4600',
+  baseUrl: 'http://localhost:4200',
   specPattern: 'src/e2e/**/*.cy.{js,jsx,ts,tsx}',
   supportFile: 'src/support/e2e.ts',
 };
