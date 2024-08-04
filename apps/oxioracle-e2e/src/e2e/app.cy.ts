f353051b771c8f27daad1e432ca007b9cf814331
John Coppola <jdcoppola@gmail.com>
2024-08-04 09:46:40 -0400

    TEST: writes e2e tests for routes

--- apps/oxioracle-e2e/src/e2e/app.cy.ts
+++ apps/oxioracle-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,30 @@
-import { getGreeting } from '../support/app.po';
-
 describe('oxioracle-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('has README link', () => {
+    cy.get('[data-test="app-name"')
+      .should('have.attr', 'href')
+      .and('include', 'README');
+    // TODO: add after README file is created
+    // .then((href) => cy.visit(String(href)));
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Welcome/);
+  it('loads table route', () => {
+    cy.get('[data-test="table-link"]').click();
+
+    cy.get('[data-test="table"]').should('exist');
+  });
+
+  it('loads charts route', () => {
+    cy.get('[data-test="charts-link"]').click();
+
+    cy.get('[data-test="bar-chart"]').should('exist');
+    cy.get('[data-test="pie-chart"]').should('exist');
+  });
+
+  it('loads form route', () => {
+    cy.get('[data-test="form-link"]').click();
+
+    cy.get('[data-test="form"]').should('exist');
   });
 });
