ed4f255bcbe1b9ccac0223bc2ca291d239ee481f
John Coppola <jdcoppola@gmail.com>
2024-08-04 09:57:00 -0400

    TEST: adds test for page not found and improves e2e suite

--- apps/oxioracle-e2e/src/e2e/app.cy.ts
+++ apps/oxioracle-e2e/src/e2e/app.cy.ts
@@ -9,22 +9,57 @@ describe('oxioracle-e2e', () => {
     // .then((href) => cy.visit(String(href)));
   });
 
-  it('loads table route', () => {
-    cy.get('[data-test="table-link"]').click();
+  describe('loads table route', () => {
+    it('by default', () => {
+      cy.get('[data-test="table"]').should('exist');
+    });
 
-    cy.get('[data-test="table"]').should('exist');
+    it('via route', () => {
+      cy.visit('/');
+
+      cy.get('[data-test="table"]').should('exist');
+    });
+
+    it('via nav', () => {
+      cy.get('[data-test="table-link"]').click();
+
+      cy.get('[data-test="table"]').should('exist');
+    });
   });
 
-  it('loads charts route', () => {
-    cy.get('[data-test="charts-link"]').click();
+  describe('loads charts route', () => {
+    it('by URL', () => {
+      cy.visit('/charts');
 
-    cy.get('[data-test="bar-chart"]').should('exist');
-    cy.get('[data-test="pie-chart"]').should('exist');
+      cy.get('[data-test="bar-chart"]').should('exist');
+      cy.get('[data-test="pie-chart"]').should('exist');
+    });
+
+    it('via nav', () => {
+      cy.get('[data-test="charts-link"]').click();
+
+      cy.get('[data-test="bar-chart"]').should('exist');
+      cy.get('[data-test="pie-chart"]').should('exist');
+    });
   });
 
-  it('loads form route', () => {
-    cy.get('[data-test="form-link"]').click();
+  describe('loads form route', () => {
+    it('by URL', () => {
+      cy.visit('/form');
 
-    cy.get('[data-test="form"]').should('exist');
+      cy.get('[data-test="form"]').should('exist');
+    });
+
+    it('via nav', () => {
+      cy.get('[data-test="form-link"]').click();
+
+      cy.get('[data-test="form"]').should('exist');
+    });
+  });
+
+  it('loads "page not found" route for invalid URLs', () => {
+    cy.visit('/bogus');
+
+    cy.get('[data-test="page-not-found"]').should('exist');
   });
 });
