d8b44956cde7108691f6ba8b2ec2eeda4dcf6167
John Coppola <jdcoppola@gmail.com>
2025-04-10 13:50:52 -0400

    MANUAL: erases unused imports (MM)

--- libs/menu-matriarch/shared/ui/src/lib/select-day/select-day.component.ts
+++ libs/menu-matriarch/shared/ui/src/lib/select-day/select-day.component.ts
@@ -4,17 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { trackBySelf } from '@atocha/core/ui';
 import { getDays } from '@atocha/menu-matriarch/shared/util';
-import { InputComponent } from '../input/input.component';
 
 @Component({
-    // eslint-disable-next-line @angular-eslint/component-selector
-    selector: 'select[ui-select-day]',
-    imports: [CommonModule, FormsModule, InputComponent],
-    template: `
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'select[ui-select-day]',
+  imports: [CommonModule, FormsModule],
+  template: `
     <option *ngFor="let day of days; trackBy: trackByFn" [ngValue]="day">
       {{ day }}
     </option>
-  `
+  `,
 })
 export class SelectDayComponent {
   readonly days = getDays();
