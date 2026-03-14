75d692c2ac832883cf40014b59380ba70b9a2d98
John Coppola <jdcoppola@gmail.com>
2026-03-14 10:29:27 -0400

    RISKY: updates MM jest configs

--- libs/menu-matriarch/shell/data-access/jest.config.ts
+++ libs/menu-matriarch/shell/data-access/jest.config.ts
@@ -2,7 +2,7 @@
 export default {
   displayName: 'menu-matriarch-shell-data-access',
   preset: '../../../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
   coverageDirectory:
     '../../../../coverage/libs/menu-matriarch/shell/data-access',
   transform: {
