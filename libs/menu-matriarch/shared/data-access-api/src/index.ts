f0cd3209b462df7f6af5685e6e75f9d6508f325d
John Coppola <jdcoppola@gmail.com>
2026-04-14 11:13:46 -0400

    FILE: deletes obsolete updater services

--- libs/menu-matriarch/shared/data-access-api/src/index.ts
+++ libs/menu-matriarch/shared/data-access-api/src/index.ts
@@ -1,11 +1,3 @@
 export * from './lib/types/endpoint.enum';
 export * from './lib/types/dto-service.interface';
 export * from './lib/types/entity-service.interface';
-
-export * from './lib/updaters/dish-update.service';
-export * from './lib/updaters/ingredient-type-update.service';
-export * from './lib/updaters/ingredient-update.service';
-export * from './lib/updaters/meal-update.service';
-export * from './lib/updaters/menu-update.service';
-export * from './lib/updaters/tag-update.service';
-export * from './lib/updaters/user-update.service';
--- libs/menu-matriarch/shared/data-access-api/src/lib/updaters/internal/key-to-update.type.ts
+++
@@ -1,1 +0,0 @@
-export type KeyToUpdate = 'mealIds' | 'dishIds' | 'ingredientIds' | 'tagIds';
